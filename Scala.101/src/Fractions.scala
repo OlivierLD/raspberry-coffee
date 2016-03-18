59714591c582025bf2b94f6a217278e247313830
Olivier LeDiouris <olivier.lediouris@oracle.com>
2016-03-18 13:56:49 -0700

    Int -> Long.

--- Scala.101/src/Fractions.scala
+++ Scala.101/src/Fractions.scala
@@ -1,17 +1,17 @@
 object Fractions {
 
   def main(args:Array[String]):Unit = {
-    val num = 3.1415926
+    val num = Math.PI  // 3.1415926
     val dec = num.toString.length - num.toString.indexOf('.')
-    val den = Math.pow(10, dec).asInstanceOf[Int]
-    val frac = reduce((num * den).asInstanceOf[Int], den)
+    val den = Math.pow(10, dec).asInstanceOf[Long]
+    val frac = reduce((num * den).asInstanceOf[Long], den)
     println(s"${num} = ${frac._1}/${frac._2}")
     println(s"Proof:${frac._1}/${frac._2} = ${ frac._1.asInstanceOf[Double] / frac._2.asInstanceOf[Double] }" )
   }
 
-  def reduce(a:Int, b:Int):(Int, Int) = {
-    var n1 = a
-    var n2 = b
+  def reduce(fr:(Long, Long)):(Long, Long) = {
+    var n1 = fr._1
+    var n2 = fr._2
     val t1 = n1
     val t2 = n2
 
