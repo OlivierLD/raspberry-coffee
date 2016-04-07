990786bfee9c1f160a34f33a7c02d2fb92e182b1
Olivier LeDiouris <olivier.lediouris@oracle.com>
2016-04-06 18:09:35 -0700

    Added key prm (gradle and groovy)

--- Groovy.101/src/JAXRSClient.groovy
+++ Groovy.101/src/JAXRSClient.groovy
@@ -3,10 +3,15 @@ import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 // import javax.xml.ws.Response
 
+def key = "54c2767878ca793f2e3cae1c45d62aa7ae9f8056"
+for (a in this.args) {
+    println("Received arg: " + a)
+    key = a
+}
 Client cl = ClientBuilder.newClient()
 WebTarget target = cl.target("https://io.adafruit.com/api/feeds/onoff")
 def resp = target.request()
-                 .header("X-AIO-Key", "54c2767878ca793f2e3cae1c45d62aa7ae9f8056")
+                 .header("X-AIO-Key", key)
                  .header("Accept",    "application/json")
                  .get(String.class) // Response.class)
 println "resp is a ${resp.getClass().getName()}:"
