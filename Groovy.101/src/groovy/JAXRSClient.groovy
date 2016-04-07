98ff612947a28fa150459b95e892ebfc7932ef5e
Olivier LeDiouris <olivier.lediouris@oracle.com>
2016-04-07 15:40:44 -0700

    Refactoring of the Groovy project

--- Groovy.101/src/groovy/JAXRSClient.groovy
+++ Groovy.101/src/groovy/JAXRSClient.groovy
@@ -3,10 +3,10 @@ import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 // import javax.xml.ws.Response
 
-def key = "54c2767878ca793f2e3cae1c45d62aa7ae9f8056"
+def key = "54c2767878ca793f2e3cae1c45d62aa7ae9f8056" // Default
 for (a in this.args) {
     println("Received script arg: " + a)
-    if (a.startsWith("key:")) { // Reveived a prm like key:XXXX, extract the XXXX
+    if (a.startsWith("key:")) { // Received a prm like key:XXXX, extract the XXXX
         key = a.substring("key:".length())
     }
 }
