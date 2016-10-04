907d456d821e3e77aeeb8a3b7c6091ac4e52a24c
Olivier LeDiouris <olediouris@medallia.com>
2016-10-03 17:46:27 -0700

    Merge

--- RMI.sample/src/java/client/ComputePi.java
+++ RMI.sample/src/java/client/ComputePi.java
@@ -11,14 +11,14 @@ public class ComputePi {
 
 	public static void main(String args[]) {
 		if (args.length != 3) {
-			System.out.println("Arguments: [RMI Server Name] [RMI Name] [PI precision]");
+			System.out.println("Arguments: [RMI Server Name] [Port] [PI precision]");
 			System.exit(1);
 		}
 
 		System.out.println("Looking up [" + bindingName + " on " + args[0] + ":" + args[1] + "]");
 		try {
 			Registry registry = LocateRegistry.getRegistry(args[0], new Integer(args[1])); // Server name, port
-			Compute comp = (Compute) registry.lookup(bindingName);       // RMI Name
+			Compute comp = (Compute) registry.lookup(bindingName);   // RMI Name
 
 			Pi task = new Pi(Integer.parseInt(args[2]));             // Precision
 			BigDecimal pi = comp.executeTask(task);
