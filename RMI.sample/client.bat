d2b36fabbf787adfe3f8b79d102fd07363250b13
Olivier LeDiouris <olediouris@medallia.com>
2016-10-05 08:11:54 -0700

    Added security policies

--- RMI.sample/client.bat
+++ RMI.sample/client.bat
@@ -5,6 +5,8 @@ set CP=.
 set CP=%CP%;.\build\libs\compute.jar
 set CP=%CP%;.\build\classes
 ::
-java -cp %CP% client.ComputePi %*
+set JAVA_OPTS="-Djava.security.policy=client.policy"
+::
+java -cp %CP% %JAVA_OPTS% client.ComputePi %*
 ::
 @endlocal
