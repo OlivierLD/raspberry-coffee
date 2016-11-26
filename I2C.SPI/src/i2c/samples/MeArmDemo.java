79b3566801720a08b09697dfd488cf34e460f963
Olivier LeDiouris <olediouris@medallia.com>
2016-11-26 12:40:48 -0800

    More moves

--- I2C.SPI/src/i2c/samples/MeArmDemo.java
+++ I2C.SPI/src/i2c/samples/MeArmDemo.java
@@ -5,7 +5,6 @@ import i2c.servo.pwm.PCA9685;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import client.SpeechTools;
 
 /*
  * Standard, all the way, clockwise, counterclockwise
@@ -55,7 +54,7 @@ public class MeArmDemo
     final int LEFT_SERVO_CHANNEL   = 0; // Range 350 (all the way up) 135 (all the way down)
     final int CLAW_SERVO_CHANNEL   = 1; // Range 130 (open) 400 (closed)
     final int BOTTOM_SERVO_CHANNEL = 2; // 130 (all the way right) 675 (all the way left). Center at ~410
-    final int RIGHT_SERVO_CHANNEL  = 4; // 130 (too far back, limit to 300) 675 (all the way ahead)
+    final int RIGHT_SERVO_CHANNEL  = 4; // 130 (too far back, limit to 300) 675 (all the way ahead), right at ~430
 
     // Test the 4 servos.
     try {
@@ -69,19 +68,26 @@ public class MeArmDemo
       // Center the arm
       servoBoard.setPWM(BOTTOM_SERVO_CHANNEL, 0, 410);
       servoBoard.setPWM(BOTTOM_SERVO_CHANNEL, 0, 0);
+      // Stand up
+      servoBoard.setPWM(RIGHT_SERVO_CHANNEL, 0, 430);
+      servoBoard.setPWM(RIGHT_SERVO_CHANNEL, 0, 0);
+
       // Open and close the claw
       // 130 Open, 400 closed
       move(servoBoard, CLAW_SERVO_CHANNEL, 400, 130, 10, 50); // Open it
       System.out.println("Give ne something to grab.");
-      SpeechTools.speak("Give me something to grab, hit return when I can catch it.");
+      client.SpeechTools.speak("Give me something to grab, hit return when I can catch it.");
       userInput("Hit return when I can catch it.");
       move(servoBoard, CLAW_SERVO_CHANNEL, 130, 400, 10, 50); // Close it
       System.out.println("Thank you!");
-      SpeechTools.speak("Thank you!");
+      client.SpeechTools.speak("Thank you!");
 
       // Turn left and drop it.
       move(servoBoard, BOTTOM_SERVO_CHANNEL, 410, 670, 10, 50); // Turn left
+      move(servoBoard, BOTTOM_SERVO_CHANNEL, 410, 670, 10, 50); // Turn left
+      move(servoBoard, RIGHT_SERVO_CHANNEL, 430, 600, 10, 50); // Move ahead
       move(servoBoard, CLAW_SERVO_CHANNEL, 400, 130, 10, 50); // Drop it
+      move(servoBoard, RIGHT_SERVO_CHANNEL, 600, 430, 10, 50); // Move back
       move(servoBoard, BOTTOM_SERVO_CHANNEL, 670, 410, 10, 50); // Come back
       move(servoBoard, CLAW_SERVO_CHANNEL, 130, 400, 10, 50); // Close it
     } finally {
