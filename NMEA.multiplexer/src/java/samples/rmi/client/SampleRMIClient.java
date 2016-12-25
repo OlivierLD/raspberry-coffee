f4897a4fdb74a5703089e46ca37d421e09e618e9
Olivier LeDiouris <olediouris@medallia.com>
2016-12-25 10:08:43 -0800

    New structure for the code. Added Scala samples.

--- NMEA.multiplexer/src/java/samples/rmi/client/SampleRMIClient.java
+++ NMEA.multiplexer/src/java/samples/rmi/client/SampleRMIClient.java
@@ -30,7 +30,7 @@ public class SampleRMIClient {
 
 		if (args.length != 2) {
 			System.out.println("Arguments: [RMI Server Name] [Port]");
-			name = "olediouris-mbp"; // "10.10.226.181";
+			name = "olediouris-mbp.att.net"; // "raspberrypi3.att.net";
 			port = "1099";
 		} else {
 			name = args[0];
---
+++ NMEA.multiplexer/src/scala/scalarmi/client/SampleOne.scala
@@ -0,0 +1,103 @@
+package scalarmi.client
+
+import context.NMEADataCache
+import nmea.forwarders.rmi.ServerInterface
+import nmea.forwarders.rmi.clientoperations.BoatPosition
+import nmea.forwarders.rmi.clientoperations.CalculatedCurrent
+import nmea.forwarders.rmi.clientoperations.InstantCurrent
+import nmea.forwarders.rmi.clientoperations.LastString
+import nmea.forwarders.rmi.clientoperations.NMEACache
+import nmea.forwarders.rmi.clientoperations.TrueWind
+import nmea.parser.GeoPos
+import java.rmi.registry.LocateRegistry
+import java.text.NumberFormat
+
+object SampleOne {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala!")
+
+    val bindingName = "RMI-NMEA"
+    val name = "raspberrypi3.att.net"
+    // "olediouris-mbp" // "10.10.226.181";
+    val port = "1099"
+    println(s"Looking up $bindingName on $name:$port ...")
+
+    try {
+      var before = System.currentTimeMillis
+      val registry = LocateRegistry.getRegistry(name, new Integer(port))
+      // Server name, port
+      val remote = registry.lookup(bindingName)
+      println(s"Remote is a ${remote.getClass.getName}")
+      val comp = registry.lookup(bindingName).asInstanceOf[ServerInterface]
+      // RMI Name
+      var after = System.currentTimeMillis
+      println(s"Lookup took ${NumberFormat.getInstance.format(after - before)} ms.")
+      val task = new LastString
+      before = System.currentTimeMillis
+      val last = comp.executeTask(task)
+      after = System.currentTimeMillis
+      println(s"LastString execution took ${NumberFormat.getInstance.format(after - before)} ms.")
+      println(last)
+      try {
+        Thread.sleep(1000L)
+      } catch {
+        case ie: InterruptedException => {
+        }
+      }
+      val cacheTask = new NMEACache
+      before = System.currentTimeMillis
+      val cache = comp.executeTask(cacheTask)
+      after = System.currentTimeMillis
+      println(s"NMEACache execution took ${NumberFormat.getInstance.format(after - before)} ms.")
+      val position = cache.get(NMEADataCache.POSITION)
+      println(s"Position is a ${position.getClass.getName}")
+      if (position.isInstanceOf[GeoPos]) println(s"Position is ${position.asInstanceOf[GeoPos].toString} (Grid Square ${position.asInstanceOf[GeoPos].gridSquare})")
+      try {
+        Thread.sleep(1000L)
+      } catch {
+        case ie: InterruptedException => {
+        }
+      }
+      val boatPositionTask = new BoatPosition
+      before = System.currentTimeMillis
+      val boatGeoPos = comp.executeTask(boatPositionTask)
+      after = System.currentTimeMillis
+      println(s"BoatPosition execution took ${NumberFormat.getInstance.format(after - before)} ms.")
+      println(s"Position is ${boatGeoPos.toString}")
+      try {
+        Thread.sleep(1000L)
+      } catch {
+        case ie: InterruptedException => {
+        }
+      }
+      val trueWind = new TrueWind
+      val calculatedCurrent = new CalculatedCurrent
+      val instantCurrent = new InstantCurrent
+      // Instant: CSP & CDR
+      (1 to 50).foreach((idx: Int) => {
+        before = System.currentTimeMillis
+        val tw = comp.executeTask(trueWind)
+        after = System.currentTimeMillis
+        println(s"TrueWind execution took ${NumberFormat.getInstance.format(after - before)} ms")
+        println(s"TW is ${tw.speed} knots, from ${tw.angle}")
+        val calc = comp.executeTask(calculatedCurrent)
+        val inst = comp.executeTask(instantCurrent)
+        println(s"Instant Current    ${inst.speed} knots, dir ${inst.angle.round.toInt}")
+        println(s"Calculated Current ${calc.speed} knots, dir ${calc.angle.round.toInt}")
+        try {
+          Thread.sleep(1000L)
+        } catch {
+          case ie: InterruptedException => {
+          }
+        }
+      })
+    } catch {
+      case e: Exception => {
+        System.err.println("Compute exception:")
+        e.printStackTrace()
+      }
+    }
+  }
+}
+
