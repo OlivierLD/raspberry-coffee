7a15d33c0e9af871feea0de3b1cc1b21cfb8b3aa
Olivier LeDiouris <olediouris@medallia.com>
2017-01-07 11:28:53 -0800

    Dynamic start of readers.

--- NMEA.multiplexer/src/java/nmea/consumers/reader/BME280Reader.java
+++ NMEA.multiplexer/src/java/nmea/consumers/reader/BME280Reader.java
@@ -41,8 +41,8 @@ public class BME280Reader extends NMEAReader {
 
 	@Override
 	public void startReader() {
-		System.out.println(String.format(">> Starting reader [%s]", this.getClass().getName()));
 		super.enableReading();
+		System.out.println(String.format(">> Starting reader [%s]. Enabled:%s", this.getClass().getName(), this.canRead()));
 		while (this.canRead()) {
 			// Read data every 1 second
 			try {
