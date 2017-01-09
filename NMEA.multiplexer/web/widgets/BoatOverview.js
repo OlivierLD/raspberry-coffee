c0ad37ef9f47c25f2065cbf374086652be4cbf79
Olivier LeDiouris <olediouris@medallia.com>
2017-01-08 17:26:19 -0800

    Cleaning.

--- NMEA.multiplexer/web/widgets/BoatOverview.js
+++ NMEA.multiplexer/web/widgets/BoatOverview.js
@@ -1,8 +1,7 @@
 /*
  * @author Olivier Le Diouris
  */
-function BoatOverview(cName)     // Canvas name
-{
+function BoatOverview(cName) {   // Canvas name
   var instance = this;
   var cWidth, cHeight;
   
@@ -10,7 +9,7 @@ function BoatOverview(cName)     // Canvas name
   var context;
   
   // NMEA Data
-  var  bsp = 0, hdg = 0, tws = 0, twa = 0, twd = 0, aws = 0, awa = 0, perf = 0, 
+  var  bsp = 0, hdg = 0, tws = 0, twa = 0, twd = 0, aws = 0, awa = 0,
        leeway = 0, vmg = 0, cog = 0, sog = 0, cmg = 0, csp = 0, cdr = 0, b2wp = 0;
   
   var VMW_ON_WIND     = 0;
@@ -19,127 +18,99 @@ function BoatOverview(cName)     // Canvas name
   var vmgOption = VMW_ON_WIND;
   var toWayPoint = "";
       
-  this.setVMGonWind = function()
-  {
+  this.setVMGonWind = function() {
     vmgOption = VMW_ON_WIND;
   };
   
-  this.setVMGto = function(waypoint)
-  {
+  this.setVMGto = function(waypoint) {
     vmgOption = VMW_ON_WAYPOINT;
     toWayPoint = waypoint;
   };
   
-  this.setB2WP = function(d)
-  {
+  this.setB2WP = function(d) {
     b2wp = d;
     instance.drawGraph();
   };
-  this.setBSP = function(d)
-  {
+  this.setBSP = function(d) {
     bsp = d;
     instance.drawGraph();
   };
-  this.setVMG = function(d)
-  {
+  this.setVMG = function(d) {
     vmg = d;
     instance.drawGraph();
   };
-  this.setTWA = function(d)
-  {
+  this.setTWA = function(d) {
     twa = d;
     instance.drawGraph();
   };
-  this.setTWS = function(d)
-  {
+  this.setTWS = function(d) {
     tws = d;
     instance.drawGraph();
   };
-  this.setTWD = function(d)
-  {
+  this.setTWD = function(d) {
     twd = d;
     instance.drawGraph();
   };
-  this.setHDG = function(d)
-  {
+  this.setHDG = function(d) {
     hdg = d;
     instance.drawGraph();
   };
-  this.setAWA = function(d)
-  {
+  this.setAWA = function(d) {
     awa = d;
     instance.drawGraph();
   };
-  this.setAWS = function(d)
-  {
+  this.setAWS = function(d) {
     aws = d;
     instance.drawGraph();
   };
-  this.setCOG = function(d)
-  {
+  this.setCOG = function(d) {
     cog = d;
     instance.drawGraph();
   };
-  this.setSOG = function(d)
-  {
+  this.setSOG = function(d) {
     sog = d;
     instance.drawGraph();
   };
-  this.setCSP = function(d)
-  {
+  this.setCSP = function(d) {
     csp = d;
     instance.drawGraph();
   };
-  this.setCDR = function(d)
-  {
+  this.setCDR = function(d) {
     cdr = d;
     instance.drawGraph();
   };
-  this.setCMG = function(d)
-  {
+  this.setCMG = function(d) {
     cmg = d;
     instance.drawGraph();
   };
-  this.setLeeway = function(d)
-  {
+  this.setLeeway = function(d) {
     leeway = d;
     instance.drawGraph();
   };
-  this.setPerf = function(d)
-  {
-    perf = d;
-    instance.drawGraph();
-  };
 
-  var rotate = function(p, angle)
-  {
+  var rotate = function(p, angle) {
     var r = new Point(Math.round((p.x * Math.cos(toRadians(angle))) + (p.y * Math.sin(toRadians(angle)))), 
                       Math.round((p.x * -Math.sin(toRadians(angle))) + (p.y * Math.cos(toRadians(angle)))));
     return r;
   };
     
-  var getDir = function(x, y)
-  {
+  var getDir = function(x, y) {
     var dir = 0.0;
     if (y != 0)
       dir = toDegrees(Math.atan(x / y));
-    if (x <= 0 || y <= 0)
-    {
+    if (x <= 0 || y <= 0) {
       if (x > 0 && y < 0)
         dir += 180;
       else if (x < 0 && y > 0)
         dir += 360;
       else if (x < 0 && y < 0)
         dir += 180;
-      else if (x == 0)
-      {
+      else if (x == 0) {
         if (y > 0)
           dir = 0.0;
         else
           dir = 180;
-      }
-      else if (y == 0)
-      {
+      } else if (y == 0) {
         if (x > 0)
           dir = 90;
         else
@@ -153,20 +124,17 @@ function BoatOverview(cName)     // Canvas name
   };
     
   // Line with arrow head
-  function Line(x1, y1, x2, y2)
-  {
+  function Line(x1, y1, x2, y2) {
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
   };
-  Line.prototype.drawWithArrowhead = function(ctx)
-  {
+  Line.prototype.drawWithArrowhead = function(ctx) {
     this.drawWithArrowheads(ctx, false);
   };
   
-  Line.prototype.drawWithArrowheads = function(ctx, both)
-  {
+  Line.prototype.drawWithArrowheads = function(ctx, both) {
     if (both === undefined)
       both = true;
     // arbitrary styling
@@ -180,8 +148,7 @@ function BoatOverview(cName)     // Canvas name
     ctx.lineTo(this.x2, this.y2);
     ctx.stroke();
   
-    if (both)
-    {
+    if (both) {
       // draw the starting arrowhead
       var startRadians = Math.atan((this.y2 - this.y1) / (this.x2 - this.x1));
       startRadians += ((this.x2>this.x1)?-90:90) * Math.PI/180;
@@ -193,8 +160,7 @@ function BoatOverview(cName)     // Canvas name
     this.drawArrowhead(ctx, this.x2, this.y2, endRadians);
   };
   
-  Line.prototype.drawWithAnemoArrowheads = function(ctx)
-  {
+  Line.prototype.drawWithAnemoArrowheads = function(ctx) {
     // draw the line
     ctx.beginPath();
     ctx.moveTo(this.x1, this.y1);
@@ -206,8 +172,7 @@ function BoatOverview(cName)     // Canvas name
     this.drawArrowhead(ctx, this.x2 - (this.x2 - this.x1) / 2, this.y2 - (this.y2 - this.y1) / 2, endRadians);
   };
   
-  Line.prototype.drawHollowArrow = function(ctx)
-  {
+  Line.prototype.drawHollowArrow = function(ctx) {
     var headLength = 30;
     var arrowWidth = 10; // Ondulation width
     var headWidth  = 20;
@@ -262,8 +227,7 @@ function BoatOverview(cName)     // Canvas name
 
     context.beginPath();
     context.moveTo(x[0], y[0]);
-    for (var i=1; i<x.length; i++)   
-    {
+    for (var i=1; i<x.length; i++) {
       context.lineTo(x[i], y[i]);
     }
     context.closePath();
@@ -272,8 +236,7 @@ function BoatOverview(cName)     // Canvas name
   
   var HEAD_LENGTH = 20;
   var HEAD_WIDTH  = 6;
-  Line.prototype.drawArrowhead = function(ctx, x, y, radians)
-  {
+  Line.prototype.drawArrowhead = function(ctx, x, y, radians) {
     ctx.save();
     ctx.beginPath();
     ctx.translate(x, y);
@@ -286,20 +249,17 @@ function BoatOverview(cName)     // Canvas name
     ctx.fill();
   };
   
-  var relativeMouseCoords = function (event, element)
-  {
+  var relativeMouseCoords = function (event, element) {
     var totalOffsetX = 0;
     var totalOffsetY = 0;
     var canvasX = 0;
     var canvasY = 0;
     var currentElement = element;
 
-    do
-    {
+    do {
       totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
       totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-    }
-    while (currentElement = currentElement.offsetParent)
+    } while (currentElement = currentElement.offsetParent)
 
     canvasX = event.pageX - totalOffsetX;
     canvasY = event.pageY - totalOffsetY;
@@ -309,8 +269,7 @@ function BoatOverview(cName)     // Canvas name
   
   var speedScale = 10;
   
-  var getCanvasCenter = function()
-  {
+  var getCanvasCenter = function() {
     var cw = document.getElementById(cName).width;
     var ch = document.getElementById(cName).height;
     var distFromRight = Math.min(cw, ch) / 2;
@@ -318,8 +277,7 @@ function BoatOverview(cName)     // Canvas name
     return { x: cw - distFromRight, y: ch / 2};
   };
   
-  this.drawGraph = function()
-  {
+  this.drawGraph = function() {
     var maxSpeed = 5;
     maxSpeed = Math.max(maxSpeed, sog);
     maxSpeed = Math.max(maxSpeed, bsp);
@@ -351,8 +309,7 @@ function BoatOverview(cName)     // Canvas name
     var y = center.y;
     
     context.strokeStyle = 'gray';
-    for (var circ=1; circ<=speedScale; circ++)
-    {
+    for (var circ=1; circ<=speedScale; circ++) {
       var radius = Math.round(circ * ((Math.min(cHeight, cWidth) / 2) / speedScale));
       context.beginPath();
       if (circ % 5 == 0)
@@ -387,56 +344,39 @@ function BoatOverview(cName)     // Canvas name
     context.fillText(bsp + " kts", col2, txtY);
     txtY += space;    
     context.fillText("HDG", col1, txtY);
-    context.fillText(hdg.toFixed(0) + "" /* "º" */, col2, txtY);
+    context.fillText(hdg.toFixed(0) + "" /* "ï¿½" */, col2, txtY);
     txtY += space;    
     context.fillText("AWS", col1, txtY);
     context.fillText(aws + " kts", col2, txtY);
     txtY += space;    
     context.fillText("AWA", col1, txtY);
-    context.fillText(awa + "" /* "º" */, col2, txtY);
+    context.fillText(awa + "" /* "ï¿½" */, col2, txtY);
     context.fillStyle = 'blue';
     txtY += space;    
     context.fillText("TWS", col1, txtY);
     context.fillText(tws.toFixed(2) + " kts", col2, txtY);
     txtY += space;    
     context.fillText("TWA", col1, txtY);
-    context.fillText(twa + "" /* "º" */, col2, txtY);
+    context.fillText(twa + "" /* "ï¿½" */, col2, txtY);
     txtY += space;    
     context.fillText("TWD", col1, txtY);
-    context.fillText(twd + "" /* "º" */, col2, txtY);
+    context.fillText(twd + "" /* "ï¿½" */, col2, txtY);
     txtY += space;    
     context.fillText("CDR", col1, txtY);
-    context.fillText(cdr.toFixed(0) + "" /* "º" */, col2, txtY);
+    context.fillText(cdr.toFixed(0) + "" /* "ï¿½" */, col2, txtY);
     txtY += space;    
     context.fillText("CSP", col1, txtY);
     context.fillText(csp.toFixed(2) + " kts", col2, txtY);
 
     txtY += space;    
     context.fillText("leeway", col1, txtY);
-    context.fillText(leeway.toFixed(2) + "" /* "º" */, col2, txtY);
+    context.fillText(leeway.toFixed(2) + "" /* "ï¿½" */, col2, txtY);
     txtY += space;    
     context.fillText("CMG", col1, txtY);
-    context.fillText(cmg.toFixed(0) + "" /* "º" */, col2, txtY);
-    
-    if (document.getElementById("display.perf").checked)
-    {
-      context.font="30px Arial";
-      context.fillStyle = 'white';
-      context.strokeStyle = 'red';
-      context.lineWidth = 1;
-      space = 32;
-      txtY += space;    
-      
-      context.fillText("Perf", col1, txtY);
-      context.strokeText("Perf", col1, txtY);
-      context.fillText(Math.round(perf) + "%", col2, txtY);
-      context.strokeText(Math.round(perf) + "%", col2, txtY);
-      context.font= "bold 12px Arial";
-    }
+    context.fillText(cmg.toFixed(0) + "" /* "ï¿½" */, col2, txtY);
   };
   
-  this.drawTrueWind = function()
-  {    
+  this.drawTrueWind = function() {
     cWidth  = document.getElementById(cName).width;
     cHeight = document.getElementById(cName).height;
 
@@ -461,12 +401,11 @@ function BoatOverview(cName)     // Canvas name
     {
       context.font= "bold 12px Arial";
       context.fillStyle = "black";
-      context.fillText("TWS:" + tws.toFixed(2) + " kts, TWA:" + twa + "" /* "º" */, x + dX, y + dY);
+      context.fillText("TWS:" + tws.toFixed(2) + " kts, TWA:" + twa + "" /* "ï¿½" */, x + dX, y + dY);
     }
   };
   
-  this.drawAppWind = function()
-  {    
+  this.drawAppWind = function() {
     cWidth  = document.getElementById(cName).width;
     cHeight = document.getElementById(cName).height;
 
@@ -490,16 +429,14 @@ function BoatOverview(cName)     // Canvas name
     context.lineWidth = 5;
     line.drawWithAnemoArrowheads(context);
     context.closePath();
-    if (document.getElementById("display.labels").checked)
-    {
+    if (document.getElementById("display.labels").checked) {
       context.font= "bold 12px Arial";
       context.fillStyle = "blue";
-      context.fillText("AWS:" + aws + " kts, AWA:" + awa + "" /* "º" */, x + dX, y + dY);
+      context.fillText("AWS:" + aws + " kts, AWA:" + awa + "" /* "ï¿½" */, x + dX, y + dY);
     }
   };
   
-  this.drawBSP = function()
-  {    
+  this.drawBSP = function() {
     if (bsp === 0) return;
 
     cWidth  = document.getElementById(cName).width;
@@ -523,16 +460,14 @@ function BoatOverview(cName)     // Canvas name
     context.closePath();
 //    var metrics = context.measureText(valueToDisplay);
 //    len = metrics.width;
-    if (document.getElementById("display.labels").checked)
-    {
+    if (document.getElementById("display.labels").checked) {
       context.font= "bold 12px Arial";
       context.fillStyle = "red";
-      context.fillText("BSP:" + bsp.toFixed(2) + " kts, HDG:" + hdg.toFixed(0) + "" /* "º" */, x + dX, y + dY);
+      context.fillText("BSP:" + bsp.toFixed(2) + " kts, HDG:" + hdg.toFixed(0) + "" /* "ï¿½" */, x + dX, y + dY);
     }
   };
   
-  this.drawCMG = function()
-  {    
+  this.drawCMG = function() {
     if (bsp === 0 || leeway === 0) return;
 
     cWidth  = document.getElementById(cName).width;
@@ -555,16 +490,14 @@ function BoatOverview(cName)     // Canvas name
     context.lineWidth = 5;
     line.drawWithArrowhead(context);
     context.closePath();
-    if (document.getElementById("display.labels").checked)
-    {
+    if (document.getElementById("display.labels").checked) {
       context.font= "bold 12px Arial";
       context.fillStyle = "cyan";
-      context.fillText("CMG:" + cmg.toFixed(0) + "" /* "º" */, x + dX, y + dY);
+      context.fillText("CMG:" + cmg.toFixed(0) + "" /* "ï¿½" */, x + dX, y + dY);
     }
   };
 
-  this.drawSOG = function()
-  {    
+  this.drawSOG = function() {
     if (sog === 0) return;
 
     cWidth  = document.getElementById(cName).width;
@@ -587,19 +520,17 @@ function BoatOverview(cName)     // Canvas name
     context.lineWidth = 5;
     line.drawWithArrowhead(context);
     context.closePath();
-    if (document.getElementById("display.labels").checked)
-    {
+    if (document.getElementById("display.labels").checked) {
       context.font= "bold 12px Arial";
       context.fillStyle = "pink";
-      context.fillText("SOG:" + sog + " kts, COG:" + cog + "" /* "º" */, x + dX, y + dY);
+      context.fillText("SOG:" + sog + " kts, COG:" + cog + "" /* "ï¿½" */, x + dX, y + dY);
       context.strokeStyle = "black";
       context.lineWidth = 1;
-//    context.strokeText("SOG:" + sog.toFixed(2) + " kts, COG:" + cog + "" /* "º" */, x + dX, y + dY);
+//    context.strokeText("SOG:" + sog.toFixed(2) + " kts, COG:" + cog + "" /* "ï¿½" */, x + dX, y + dY);
     }
   };
   
-  this.drawVMG = function()
-  { 
+  this.drawVMG = function() {
     cWidth  = document.getElementById(cName).width;
     cHeight = document.getElementById(cName).height;
     
@@ -610,10 +541,9 @@ function BoatOverview(cName)     // Canvas name
     var y = center.y;
     
     var wpName = document.getElementById("display.vmg.waypoint").value;
-    if (document.getElementById("display.vmg.wind").checked)
-      _hdg = toRadians(twd); 
-    else
-    {
+    if (document.getElementById("display.vmg.wind").checked) {
+        _hdg = toRadians(twd);
+    } else {
       _hdg = toRadians(b2wp);       
       // Display WP direction
       context.strokeStyle = "orange";
@@ -638,17 +568,15 @@ function BoatOverview(cName)     // Canvas name
     context.lineWidth = 5;
     line.drawWithArrowhead(context);
     context.closePath();
-    if (false && document.getElementById("display.labels").checked)
-    {
+    if (false && document.getElementById("display.labels").checked) {
       context.font= "bold 12px Arial";
       context.fillStyle = "yellow";
       context.fillText("VMG:" + vmg.toFixed(2) + " kts", x + dX, y + dY);
       context.strokeStyle = "black";
       context.lineWidth = 1;
-//    context.strokeText("SOG:" + sog.toFixed(2) + " kts, COG:" + cog + "" /* "º" */, x + dX, y + dY);
+//    context.strokeText("SOG:" + sog.toFixed(2) + " kts, COG:" + cog + "" /* "ï¿½" */, x + dX, y + dY);
     }
-    if (context.setLineDash !== undefined)
-    {
+    if (context.setLineDash !== undefined) {
       context.setLineDash([5]);
       context.moveTo(x + dX, y + dY);
       var _cog = toRadians(cog); 
@@ -663,8 +591,7 @@ function BoatOverview(cName)     // Canvas name
     }
   };
   
-  this.drawCurrent = function()
-  {    
+  this.drawCurrent = function() {
     if (csp === 0) return;
     
     cWidth  = document.getElementById(cName).width;
@@ -693,16 +620,14 @@ function BoatOverview(cName)     // Canvas name
     context.lineWidth = 5;
     line.drawWithArrowhead(context);
     context.closePath();
-    if (document.getElementById("display.labels").checked)
-    {
+    if (document.getElementById("display.labels").checked) {
       context.font= "bold 12px Arial";
       context.fillStyle = "LightGreen";
-      context.fillText("CSP:" + csp.toFixed(2) + " kts, CDR:" + cdr.toFixed(0) + "" /* "º" */, x + dXcog, y + dYcog + 14); // + 14 not to overlap the SOG/COG
+      context.fillText("CSP:" + csp.toFixed(2) + " kts, CDR:" + cdr.toFixed(0) + "" /* "ï¿½" */, x + dXcog, y + dYcog + 14); // + 14 not to overlap the SOG/COG
     }
   };
   
-  this.drawVW = function()
-  {    
+  this.drawVW = function() {
     if (sog === 0) return;
 
     cWidth  = document.getElementById(cName).width;
@@ -739,8 +664,7 @@ function BoatOverview(cName)     // Canvas name
   
   var WL_RATIO_COEFF = 0.75; // Ratio to apply to (3.5 * Width / Length)
   var BOAT_LENGTH = 50;
-  this.drawBoat = function(trueHeading)
-  {
+  this.drawBoat = function(trueHeading) {
     cWidth  = document.getElementById(cName).width;
     cHeight = document.getElementById(cName).height;
 
@@ -776,8 +700,7 @@ function BoatOverview(cName)     // Canvas name
     var ptX = center.x;
     var ptY = center.y;
 
-    for (var i=0; i<x.length; i++)
-    {
+    for (var i=0; i<x.length; i++) {
       var dx = x[i] * Math.cos(toRadians(trueHeading)) + (y[i] * (-Math.sin(toRadians(trueHeading))));
       var dy = x[i] * Math.sin(toRadians(trueHeading)) + (y[i] * Math.cos(toRadians(trueHeading)));
       xpoints.push(Math.round(ptX + dx));
@@ -786,8 +709,7 @@ function BoatOverview(cName)     // Canvas name
     context.fillStyle = 'gray';
     context.beginPath();
     context.moveTo(xpoints[0], ypoints[0]);
-    for (var i=1; i<xpoints.length; i++)   
-    {
+    for (var i=1; i<xpoints.length; i++) {
       context.lineTo(xpoints[i], ypoints[i]);
     }
     context.closePath();
@@ -797,25 +719,21 @@ function BoatOverview(cName)     // Canvas name
     context.stroke();
   };
 
-  var toRadians = function(deg)
-  {
+  var toRadians = function(deg) {
     return deg * (Math.PI / 180);
   };
 
-  function toDegrees(rad)
-  {
+  function toDegrees(rad) {
     return rad * (180 / Math.PI);
   };
   
-  (function()
-   { 
+  (function() {
      canvas = document.getElementById(cName);
      instance.drawGraph();
    })(); // Invoked automatically when new is invoked.  
 };
 
-function Point(_x, _y)
-{
+function Point(_x, _y) {
   this.x = _x;
   this.y = _y;
 };
