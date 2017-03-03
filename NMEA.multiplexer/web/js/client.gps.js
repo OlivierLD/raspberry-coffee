e999c4b07ea0580fc41fe109fc8a6ef18546e6bc
Olivier LeDiouris <olediouris@medallia.com>
2017-03-03 08:54:33 -0800

    JS Refactoring and Digital GPS demo

--- NMEA.multiplexer/web/js/client.gps.js
+++ NMEA.multiplexer/web/js/client.gps.js
@@ -1,58 +1,5 @@
 "use strict";
 
-var getNMEAData = function() {
-    var deferred = $.Deferred(),  // a jQuery deferred
-        url = '/cache',
-        xhr = new XMLHttpRequest(),
-        TIMEOUT = 10000;
-
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.send();
-
-    var requestTimer = setTimeout(function() {
-        xhr.abort();
-        deferred.reject(408, { message: 'Timeout'});
-    }, TIMEOUT);
-
-    xhr.onload = function() {
-        clearTimeout(requestTimer);
-        if (xhr.status === 200) {
-            deferred.resolve(xhr.response);
-        } else {
-            deferred.reject(xhr.status, xhr.response);
-        }
-    };
-    return deferred.promise();
-};
-
-// Executed at startup
-(function () {
-  // Long poll
-  setInterval(function() {
-    fetch();
-  }, 1000);
-})();
-
-var fetch = function() {
-    var getData = getNMEAData();
-    getData.done(function(value) {
-    //  console.log("Done:", value);
-        var json = JSON.parse(value);
-        onMessage(json);
-    });
-    getData.fail(function(error, errmess) {
-        var message;
-        if (errmess !== undefined) {
-            var mess = JSON.parse(errmess);
-            if (mess.message !== undefined) {
-                message = mess.message;
-            }
-        }
-        alert("Failed to get nmea data..." + (error !== undefined ? error : ' - ') + ', ' + (message !== undefined ? message : ' - '));
-    });
-};
-
 var onMessage = function (json) {
     $("#raw-json").text(JSON.stringify(json, null, 2));
 
@@ -192,3 +139,5 @@ var setConnectionStatus = function(ok) {
     title.style.color = (ok === true ? 'green' : 'red');
   }
 };
+
+var cacheClient = new cacheClient(onMessage);
