a5c38692711c71fd71d8c62b6da34bfe5c5c7188
OlivierLD <olivier.lediouris@gmail.com>
2017-03-19 12:34:37 -0700

    Added properties file

--- NMEA.multiplexer/mux.sh
+++ NMEA.multiplexer/mux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
 JAVA_OPTIONS=
-JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=./libs"       # for Mac
-# JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=/usr/lib/jni" # for Raspberry PI
+# JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=./libs"       # for Mac
+JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=/usr/lib/jni" # for Raspberry PI
 #
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dserial.data.verbose=false"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dtcp.data.verbose=false"
@@ -16,12 +16,12 @@ JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=./libs"       # for Mac
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dverbose=false"
 #
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.rpi.demo.properties"
-JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.5.properties"
+JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.7.properties"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dhttp.verbose=true"
 #
 CP=./build/libs/NMEA.multiplexer-1.0-all.jar
-CP=$CP:./libs/RXTXcomm.jar          # for Mac
-# CP=$CP:/usr/share/java/RXTXcomm.jar # For Raspberry PI
+# CP=$CP:./libs/RXTXcomm.jar          # for Mac
+CP=$CP:/usr/share/java/RXTXcomm.jar # For Raspberry PI
 #
 # For JFR
 JFR_FLAGS=
@@ -33,5 +33,5 @@ REMOTE_DEBUG_FLAGS=
 LOGGING_FLAG=
 LOGGING_FLAG=-Djava.util.logging.config.file=./logging.properties
 # use sudo on Raspberry PI
-java $JAVA_OPTIONS $LOGGING_FLAG $JFR_FLAGS $REMOTE_DEBUG_FLAGS -cp $CP nmea.mux.GenericNMEAMultiplexer
+sudo java $JAVA_OPTIONS $LOGGING_FLAG $JFR_FLAGS $REMOTE_DEBUG_FLAGS -cp $CP nmea.mux.GenericNMEAMultiplexer
 #
