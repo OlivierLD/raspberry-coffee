c4e1bacff6a211a501a01e8f83c0cf4dd7c00ba7
Olivier LeDiouris <olediouris@medallia.com>
2017-04-10 17:51:23 -0700

    Properties file as prm of mux.sh

--- NMEA.multiplexer/mux.sh
+++ NMEA.multiplexer/mux.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 #
+MUX_PROP_FILE=nmea.mux.rpi.demo.properties
+if [ $# -gt 0 ]
+then
+  MUX_PROP_FILE=$1
+fi
+#
+echo Using properties file $MUX_PROP_FILE
+#
 JAVA_OPTIONS=
 JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=./libs"       # for Mac
 # JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=/usr/lib/jni" # for Raspberry PI
@@ -11,16 +19,13 @@ JAVA_OPTIONS="$JAVA_OPTIONS -Djava.library.path=./libs"       # for Mac
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dhtu21df.data.verbose=false"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dbme280.data.verbose=false"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Drnd.data.verbose=false"
-#
+# JAVA_OPTIONS="$JAVA_OPTIONS -Dhttp.verbose=true"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.data.verbose=false"
-JAVA_OPTIONS="$JAVA_OPTIONS -Dhttp.verbose=true"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dverbose=false"
 #
-# JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.rpi.demo.properties"
-JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.8.properties"
-# JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.5.properties"
-# JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=nmea.mux.in.out.properties"
-# JAVA_OPTIONS="$JAVA_OPTIONS -Dhttp.verbose=true"
+JAVA_OPTIONS="$JAVA_OPTIONS -Dhttp.verbose=true"
+#
+JAVA_OPTIONS="$JAVA_OPTIONS -Dmux.properties=$MUX_PROP_FILE"
 #
 CP=./build/libs/NMEA.multiplexer-1.0-all.jar
 CP=$CP:./libs/RXTXcomm.jar          # for Mac
