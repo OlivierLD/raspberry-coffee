81d8b38d2110819ec51a2ef416a204f3db80a5d3
Olivier LeDiouris <olediouris@medallia.com>
2017-05-15 07:21:51 -0700

    ForMeArm start

---
+++ RasPISamples/src/java/paddle/ForMeArm.java
@@ -0,0 +1,20 @@
+package paddle;
+
+/**
+ * A joystick, and 4 buttons.
+ *
+ * Joystick:
+ * - Move forward
+ * - Move backward
+ * - Turn right
+ * - Turn left
+ *
+ * 4 Buttons:
+ * - Up
+ * - Down
+ * - Open claw
+ * - Close claw
+ */
+public class ForMeArm {
+	// TODO do it
+}
