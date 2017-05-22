846a7104af5c747f7bd8463216f1cd81f983885a
Olivier LeDiouris <olediouris@medallia.com>
2017-05-22 16:28:13 -0700

    Version 0.1

--- RasPISamples/src/java/paddle/MeArmPilotImplementation.java
+++ RasPISamples/src/java/paddle/MeArmPilotImplementation.java
@@ -56,10 +56,11 @@ public class MeArmPilotImplementation {
 	 */
 
 	enum ServoRange {
+		//     Name      from to   middle
 		BOTTOM("BOTTOM", 310, 510, 410), // Left, Right, Middle
-		CLAW("CLAW",     130, 400),      // Open, Closed
-		LEFT("LEFT",     230, 350, 290), // Low, High, Middle
-		RIGHT("RIGHT",   430, 550, 490); // Backward, Forward, Middle
+		CLAW  ("CLAW",   130, 400),      // Open, Closed
+		LEFT  ("LEFT",   230, 350, 290), // Low, High, Middle
+		RIGHT ("RIGHT",  430, 550, 490); // Backward, Forward, Middle
 
 		private final String servoName;
 		private final int from;
@@ -82,10 +83,10 @@ public class MeArmPilotImplementation {
 	}
 
 	// Original settings.
-	private static int currentClawPos         = ServoRange.CLAW.to(); // Closed
+	private static int currentClawPos         = ServoRange.CLAW.to();       // Closed
 	private static int currentLeftRightPos    = ServoRange.BOTTOM.middle(); // Center
-	private static int currentUpDownPos       = ServoRange.LEFT.middle(); // Center
-	private static int currentBackAndForthPos = ServoRange.RIGHT.middle(); // Middle
+	private static int currentUpDownPos       = ServoRange.LEFT.middle();   // Center
+	private static int currentBackAndForthPos = ServoRange.RIGHT.middle();  // Middle
 
 	private static void executeCommandList(String[] commands) {
 		Arrays.stream(commands).forEach(cmd -> {
@@ -139,104 +140,128 @@ public class MeArmPilotImplementation {
 	}
 
 	public int moveForward() {
-		int backForthPos = currentBackAndForthPos + 10;
+		return moveForward(10, 1);
+	}
+	public int moveForward(int inc, int steps) {
+		int backForthPos = currentBackAndForthPos + inc;
 		if (backForthPos > ServoRange.RIGHT.to()) {
 			return -1;
 		}
 		String[] moveForward = {
 						"PRINT: \"Moving forward\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.RIGHT.servoName(), currentBackAndForthPos, backForthPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.RIGHT.servoName(), currentBackAndForthPos, backForthPos, steps)
 		};
 		executeCommandList(moveForward);
 		currentBackAndForthPos = backForthPos;
 		return 0;
 	}
 	public int moveBackward() {
-		int backForthPos = currentBackAndForthPos - 10;
+		return moveBackward(10, 1);
+	}
+	public int moveBackward(int inc, int steps) {
+		int backForthPos = currentBackAndForthPos - inc;
 		if (backForthPos < ServoRange.RIGHT.from()) {
 			return -1;
 		}
 		String[] moveBackward = {
 						"PRINT: \"Moving backward\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.RIGHT.servoName(), currentBackAndForthPos, backForthPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.RIGHT.servoName(), currentBackAndForthPos, backForthPos, steps)
 		};
 		executeCommandList(moveBackward);
 		currentBackAndForthPos = backForthPos;
 		return 0;
 	}
 	public int moveUp() {
-		int upDownPos = currentUpDownPos + 10;
+		return moveUp(10, 1);
+	}
+	public int moveUp(int inc, int steps) {
+		int upDownPos = currentUpDownPos + inc;
 		if (upDownPos > ServoRange.LEFT.to()) {
 			return -1;
 		}
 		String[] moveUp = {
 						"PRINT: \"Moving up\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.LEFT.servoName(), currentUpDownPos, upDownPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.LEFT.servoName(), currentUpDownPos, upDownPos, steps)
 		};
 		executeCommandList(moveUp);
 		currentUpDownPos = upDownPos;
 		return 0;
 	}
 	public int moveDown() {
-		int upDownPos = currentUpDownPos - 10;
+		return moveDown(10, 1);
+	}
+	public int moveDown(int inc, int steps) {
+		int upDownPos = currentUpDownPos - inc;
 		if (upDownPos < ServoRange.LEFT.from()) {
 			return -1;
 		}
 		String[] moveDown = {
 						"PRINT: \"Moving down\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.LEFT.servoName(), currentUpDownPos, upDownPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.LEFT.servoName(), currentUpDownPos, upDownPos, steps)
 		};
 		executeCommandList(moveDown);
 		currentUpDownPos = upDownPos;
 		return 0;
 	}
 	public int turnRight() {
-		int leftRightPos = currentLeftRightPos + 10;
+		return turnRight(10, 1);
+	}
+	public int turnRight(int inc, int steps) {
+		int leftRightPos = currentLeftRightPos + inc;
 		if (leftRightPos > ServoRange.BOTTOM.to()) {
 			return -1;
 		}
 		String[] turnRight = {
 						"PRINT: \"Moving right\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.BOTTOM.servoName(), currentLeftRightPos, leftRightPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.BOTTOM.servoName(), currentLeftRightPos, leftRightPos, steps)
 		};
 		executeCommandList(turnRight);
 		currentLeftRightPos = leftRightPos;
 		return 0;
 	}
 	public int turnLeft() {
-		int leftRightPos = currentLeftRightPos - 10;
+		return turnLeft(10, 1);
+	}
+	public int turnLeft(int inc, int steps) {
+		int leftRightPos = currentLeftRightPos - inc;
 		if (leftRightPos < ServoRange.BOTTOM.from()) {
 			return -1;
 		}
 		String[] turnLeft = {
 						"PRINT: \"Moving left\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.BOTTOM.servoName(), currentLeftRightPos, leftRightPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.BOTTOM.servoName(), currentLeftRightPos, leftRightPos, steps)
 		};
 		executeCommandList(turnLeft);
 		currentLeftRightPos = leftRightPos;
 		return 0;
 	}
-	public int openClaw() { // TODO Speed factor, += ?
-		int clawPos = currentClawPos - 10;
+	public int openClaw() {
+		return openClaw(10, 1);
+	}
+	public int openClaw(int inc, int steps) {
+		int clawPos = currentClawPos - inc;
 		if (clawPos < ServoRange.CLAW.from()) {
 			return -1;
 		}
 		String[] openClaw = {
 						"PRINT: \"Opening the claw\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.CLAW.servoName(), currentClawPos, clawPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.CLAW.servoName(), currentClawPos, clawPos, steps)
 		};
 		executeCommandList(openClaw);
 		currentClawPos = clawPos;
 		return 0;
 	}
 	public int closeClaw() {
-		int clawPos = currentClawPos + 10;
+		return closeClaw(10, 1);
+	}
+	public int closeClaw(int inc, int steps) {
+		int clawPos = currentClawPos + inc;
 		if (clawPos > ServoRange.CLAW.to()) {
 			return -1;
 		}
 		String[] closeClaw = {
 						"PRINT: \"Closing the claw\"",
-						String.format("MOVE: %s, %d, %d, 1, 25", ServoRange.CLAW.servoName(), currentClawPos, clawPos)
+						String.format("MOVE: %s, %d, %d, %d, 25", ServoRange.CLAW.servoName(), currentClawPos, clawPos, steps)
 		};
 		executeCommandList(closeClaw);
 		currentClawPos = clawPos;
@@ -257,14 +282,14 @@ public class MeArmPilotImplementation {
 		// Open and close the claw
 		boolean ok = true;
 		while (ok) {
-			while (meArm.openClaw() == 0) {
+			while (meArm.openClaw(10, 10) == 0) {
 				try { Thread.sleep(10); } catch (Exception whatever) {}
 			}
 			ok = false;
 		}
 		ok = true;
 		while (ok) {
-			while (meArm.closeClaw() == 0) {
+			while (meArm.closeClaw(10, 10) == 0) {
 				try { Thread.sleep(10); } catch (Exception whatever) {}
 			}
 			ok = false;
@@ -273,14 +298,14 @@ public class MeArmPilotImplementation {
 		// Move right and left
 		ok = true;
 		while (ok) {
-			while (meArm.turnRight() == 0) {
+			while (meArm.turnRight(10, 10) == 0) {
 				try { Thread.sleep(10); } catch (Exception whatever) {}
 			}
 			ok = false;
 		}
 		ok = true;
 		while (ok) {
-			while (meArm.turnLeft() == 0) {
+			while (meArm.turnLeft(10, 10) == 0) {
 				try { Thread.sleep(10); } catch (Exception whatever) {}
 			}
 			ok = false;
@@ -288,7 +313,55 @@ public class MeArmPilotImplementation {
 		// Back in the middle
 		ok = true;
 		while (ok && currentLeftRightPos != ServoRange.BOTTOM.middle()) {
-			while (meArm.turnRight() == 0) {
+			while (meArm.turnRight(10, 10) == 0) {
+				try { Thread.sleep(10); } catch (Exception whatever) {}
+			}
+			ok = false;
+		}
+
+		// Move up & down
+		ok = true;
+		while (ok) {
+			while (meArm.moveUp(10, 10) == 0) {
+				try { Thread.sleep(10); } catch (Exception whatever) {}
+			}
+			ok = false;
+		}
+		ok = true;
+		while (ok) {
+			while (meArm.moveDown(10, 10) == 0) {
+				try { Thread.sleep(10); } catch (Exception whatever) {}
+			}
+			ok = false;
+		}
+		// Back in the middle
+		ok = true;
+		while (ok && currentUpDownPos != ServoRange.LEFT.middle()) {
+			while (meArm.moveUp(10, 10) == 0) {
+				try { Thread.sleep(10); } catch (Exception whatever) {}
+			}
+			ok = false;
+		}
+
+		// Move back & forth
+		ok = true;
+		while (ok) {
+			while (meArm.moveBackward(10, 10) == 0) {
+				try { Thread.sleep(10); } catch (Exception whatever) {}
+			}
+			ok = false;
+		}
+		ok = true;
+		while (ok) {
+			while (meArm.moveForward(10, 10) == 0) {
+				try { Thread.sleep(10); } catch (Exception whatever) {}
+			}
+			ok = false;
+		}
+		// Back in the middle
+		ok = true;
+		while (ok && currentBackAndForthPos != ServoRange.RIGHT.middle()) {
+			while (meArm.moveBackward(10, 10) == 0) {
 				try { Thread.sleep(10); } catch (Exception whatever) {}
 			}
 			ok = false;
