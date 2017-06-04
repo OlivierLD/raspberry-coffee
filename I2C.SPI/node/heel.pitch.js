9d1fc49170a9f79e1c369b7b82be90cc88328052
Olivier LeDiouris <olediouris@medallia.com>
2017-06-04 13:33:00 -0700

    WebGL demo for heel and pitch

--- I2C.SPI/node/heel.pitch.js
+++ I2C.SPI/node/heel.pitch.js
@@ -36,7 +36,8 @@ var connection;
     document.getElementById('Y').innerHTML = payload.pitch.toFixed(0);
     document.getElementById('Z').innerHTML = payload.roll.toFixed(0);
 
-    sendToCube(payload.heading.toFixed(0) - h, payload.pitch.toFixed(0) - p, payload.roll.toFixed(0) - r);
+    sendToCube(payload.heading.toFixed(0), payload.pitch.toFixed(0), payload.roll.toFixed(0));
+//  sendToCube(payload.heading.toFixed(0) - h, payload.pitch.toFixed(0) - p, payload.roll.toFixed(0) - r);
 
     h = payload.heading.toFixed(0);
     p = payload.pitch.toFixed(0);
