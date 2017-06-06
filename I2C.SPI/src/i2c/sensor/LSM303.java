91fbb0ea92e3e3c93a125de2a019e01fd3f7dbbd
Olivier LeDiouris <olediouris@medallia.com>
2017-06-06 15:21:55 -0700

    read 6 bytes

--- I2C.SPI/src/i2c/sensor/LSM303.java
+++ I2C.SPI/src/i2c/sensor/LSM303.java
@@ -312,9 +312,17 @@ public class LSM303 {
 	private static void dumpBytes(byte[] ba, int len) {
 		String str = String.format("%d bytes: ", len);
 		for (int i=0; i<len; i++) {
-			str += (Integer.toHexString(ba[i] & 0xFF) + " ").toUpperCase();
+			str += lpad((Integer.toHexString(ba[i] & 0xFF) + " ").toUpperCase(), 2, "0");
 		}
 		System.out.println(str);
+	}
+
+	private static String lpad(String str, int len, String with) {
+		String s = str;
+		while (s.length() < len) {
+			s = with + s;
+		}
+		return s;
 	}
 	/**
 	 * This is for tests.
