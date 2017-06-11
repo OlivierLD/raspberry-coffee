e0fa885a87b20b02e41128151e05801fcf7fd5b6
Olivier LeDiouris <olediouris@medallia.com>
2017-06-11 10:44:35 -0700

    Added java sources

--- PurePWM/src/java/pwm/Pwm01.java
+++ PurePWM/src/java/pwm/Pwm01.java
@@ -3,30 +3,34 @@ package pwm;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
-import com.pi4j.io.gpio.PinMode;
 import com.pi4j.io.gpio.RaspiPin;
 
+/**
+ * See: http://wiringpi.com/reference/software-pwm-library/
+ */
 public class Pwm01 {
 	public static void main(String[] args)
 					throws InterruptedException
 	{
 
-		System.out.println("GPIO Control - pin 01 ... started.");
+		System.out.println("PWM Control - pin 01 ... started.");
 
 		// create gpio controller
 		final GpioController gpio = GpioFactory.getInstance();
 
-		// provision gpio pin #01 as an output pin and turn on
-		final GpioPinPwmOutput pin = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_01, "Standard-Servo");
-		pin.setMode(PinMode.PWM_OUTPUT);
-		pin.setPwmRange(2000);
-		pin.setPwm(150);
+		GpioPinPwmOutput pin = gpio.provisionSoftPwmOutputPin(RaspiPin.GPIO_01); // , "Standard-Servo");
+//	pin.setMode(PinMode.PWM_OUTPUT);
+		pin.setPwmRange(100);
+		pin.setPwm(100);
 		delay(1000);
-		pin.setPwm(200);
+		pin.setPwm(50);
+		delay(1000);
+		pin.setPwm(0);
 
 		// stop all GPIO activity/threads by shutting down the GPIO controller
 		// (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
 		gpio.shutdown();
+		System.out.println("Bye");
 	}
 
 	private static void delay(long ms) {
