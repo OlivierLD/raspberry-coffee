718a4317963b15e4c1efd30611b56ce1e202c2ed
Olivier LeDiouris <olediouris@medallia.com>
2017-06-11 10:22:12 -0700

    V.1

--- PurePWM/C/pwm01.c
+++ PurePWM/C/pwm01.c
@@ -11,14 +11,15 @@
 #include <wiringPi.h>
 
 int main (void) {
-   printf ("Raspberry Pi PWM wiringPi test program\n");
+   fprintf (stdout, "Raspberry Pi PWM wiringPi test program\n");
    wiringPiSetupGpio();
-   pinMode (18, PWM_OUTPUT);
+   pinMode (18, PWM_OUTPUT); // Pin #12
    pwmSetMode (PWM_MODE_MS);
    pwmSetRange (2000);
    pwmSetClock (192);
    pwmWrite(18,150);
    delay(1000);
    pwmWrite(18,200);
+   fprintf(stdout, "Done");
    return 0;
 }
---
+++ PurePWM/pwm01
@@ -0,0 +1,7 @@
+#!/bin/bash
+CP=./build/libs/PurePWM-1.0-all.jar
+#
+JAVA_OPTIONS=
+# JAVA_OPTIONS="$JAVA_OPTIONS -Dwhatever=true"
+#
+sudo java $JAVA_OPTIONS -cp $CP pwm.Pwm01
