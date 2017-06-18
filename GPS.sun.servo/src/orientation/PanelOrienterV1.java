659cbbf9bae9dd09bd884145c2164a6de30332ee
Olivier LeDiouris <olediouris@medallia.com>
2017-06-18 08:54:06 -0700

    V1

--- GPS.sun.servo/src/orientation/PanelOrienterV1.java
+++ GPS.sun.servo/src/orientation/PanelOrienterV1.java
@@ -1,8 +1,12 @@
 package orientation;
 
+import calculation.AstroComputer;
+import calculation.SightReductionUtil;
 import i2c.sensor.LSM303;
 import i2c.sensor.listener.LSM303Listener;
 import i2c.servo.pwm.PCA9685;
+import java.util.Calendar;
+import java.util.TimeZone;
 import user.util.GeomUtil;
 
 /**
@@ -10,6 +14,8 @@ import user.util.GeomUtil;
  * You have an LSM303 attached to a standard servo.
  * Start at Zero, point the x -> arrow drawn on the LSM303 as close to the north as you can, and
  * start the program.
+ *
+ * In addition, this one <b>calculates</b> the Sun elevation and orients a second servo accordingly.
  *
  * Then move the servo (to which the board is attached).
  * The servo should orient the board, so it keeps pointing in the same (North, aka 0) direction.
@@ -23,7 +29,7 @@ import user.util.GeomUtil;
  *
  * or GPS... (later).
  */
-public class Orientation101 {
+public class PanelOrienterV1 {
 
 	private static double declination = 14D; // E+, W-
 	private static int targetWindow = 1;
@@ -31,11 +37,34 @@ public class Orientation101 {
 	private static double latitude = 0D;
 	private static double longitude = 0D;
 
-	private static double z = 0D;
+	private static boolean keepWorking = true;
+
+	private static double he = 0D, z = 0D;
+	private static boolean dayTime = true;
 
 	private static boolean orientationVerbose = false;
+	private static boolean astroVerbose = false;
 
-	private int servo = -1;
+	private static void getSunData(double lat, double lng) {
+		Calendar current = Calendar.getInstance(TimeZone.getTimeZone("etc/UTC"));
+		AstroComputer.setDateTime(current.get(Calendar.YEAR),
+						current.get(Calendar.MONTH) + 1,
+						current.get(Calendar.DAY_OF_MONTH),
+						current.get(Calendar.HOUR_OF_DAY),
+						current.get(Calendar.MINUTE),
+						current.get(Calendar.SECOND));
+		AstroComputer.calculate();
+		SightReductionUtil sru = new SightReductionUtil(AstroComputer.getSunGHA(),
+						AstroComputer.getSunDecl(),
+						lat,
+						lng);
+		sru.calculate();
+		he = sru.getHe().doubleValue();
+		z = sru.getZ().doubleValue();
+	}
+
+	private static int servoHeading = 14;
+	private static int servoTilt    = 15;
 	private static int currentServoAngle = 0;
 
 	private final static int DEFAULT_SERVO_MIN = 122; // Value for Min position (-90, unit is [0..1023])
@@ -48,10 +77,9 @@ public class Orientation101 {
 
 	private PCA9685 servoBoard = null;
 
-	public Orientation101(int channel) {
+	public PanelOrienterV1() {
 		try {
-			System.out.println("Driving Servo on Channel " + channel);
-			this.servo = channel;
+			System.out.println("Driving Servos on Channels " + servoHeading + " and " + servoTilt);
 			this.servoBoard = new PCA9685();
 			this.servoBoard.setPWMFreq(freq); // Set frequency in Hz
 
@@ -59,16 +87,15 @@ public class Orientation101 {
 			ex.printStackTrace();
 			System.exit(1);
 		}
-
 	}
 
-	public void setAngle(float f) {
+	public void setAngle(int servo, float f) {
 		int pwm = degreeToPWM(servoMin, servoMax, f);
 		// System.out.println(f + " degrees (" + pwm + ")");
 		servoBoard.setPWM(servo, 0, pwm);
 	}
 
-	public void stop() { // Set to 0
+	public void stop(int servo) { // Set to 0
 		servoBoard.setPWM(servo, 0, 0);
 	}
 
@@ -83,15 +110,18 @@ public class Orientation101 {
 
 	public static void main(String... args) {
 
-		int channel = 14;
-		if (args.length > 0) {
-			try {
-				channel = Integer.parseInt(args[0]);
-			} catch (Exception e) {
-				throw e;
-			}
-		}
+		servoHeading = 14;
+		servoTilt = 15;
+		// TODO PRM all that
+//		if (args.length > 0) {
+//			try {
+//				servoHeading = Integer.parseInt(args[0]);
+//			} catch (Exception e) {
+//				throw e;
+//			}
+//		}
 
+		// Read System Properties
 		orientationVerbose = "true".equals(System.getProperty("orient.verbose", "false"));
 
 		String strLat = System.getProperty("latitude");
@@ -145,9 +175,10 @@ public class Orientation101 {
 		final LSM303 sensor;
 		final LSM303Listener orientationListener;
 
-		final Orientation101 instance = new Orientation101(channel);
+		final PanelOrienterV1 instance = new PanelOrienterV1();
 		// Set to 0
-		instance.setAngle((float)currentServoAngle);
+		instance.setAngle(servoHeading, (float)currentServoAngle);
+		instance.setAngle(servoTilt, 0f);
 
 		try {
 			sensor = new LSM303();
@@ -162,10 +193,10 @@ public class Orientation101 {
 					String headingMessage = "Heading OK";
 					int delta = 0;
 					if (headingDiff > targetWindow) {
-						headingMessage = "Turn right";
+						headingMessage = "Sun is on the right";
 						delta = -1; 
 					} else if (headingDiff < -targetWindow) {
-						headingMessage = "Turn left";
+						headingMessage = "Sun is on the left";
 						delta = 1;
 					}
 					if (orientationVerbose) {
@@ -175,7 +206,7 @@ public class Orientation101 {
 					if (delta != 0) {						
 						currentServoAngle += delta;
 //					System.out.println("Pointing to " + currentServoAngle);
-						instance.setAngle((float)currentServoAngle);
+						instance.setAngle(servoHeading, (float)currentServoAngle);
 					}
 				}
 
@@ -186,12 +217,48 @@ public class Orientation101 {
 			};
 			sensor.setDataListener(orientationListener);
 
+			Thread timeThread = new Thread(() -> {
+				int previous = 0;
+				while (keepWorking) {
+					// Sun position calculation geos here
+					getSunData(latitude, longitude);
+					if (he > 0) {
+						dayTime = true;
+						if (astroVerbose) {
+							System.out.println(String.format("From %s / %s, He:%.02f\272, Z:%.02f\272 (true)",
+											GeomUtil.decToSex(latitude, GeomUtil.SWING, GeomUtil.NS),
+											GeomUtil.decToSex(longitude, GeomUtil.SWING, GeomUtil.EW),
+											he,
+											z));
+						}
+						int angle = (int)Math.round(90 - he);
+						if (angle != previous) {
+							instance.setAngle(servoTilt, (float) angle);
+							previous = angle;
+						}
+					} else {
+						dayTime = false;
+						System.out.println("Fait nuit, parked...");
+						int angle = 0;
+						if (angle != previous) {
+							instance.setAngle(servoTilt, (float) angle);
+							previous = angle;
+						}
+					}
+					try { Thread.sleep(1_000L); } catch (Exception ex) {}
+				}
+				System.out.println("Timer done.");
+			});
+			System.out.println("Starting the timer loop");
+			timeThread.start();
+
 			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 				System.out.println("\nBye.");
 				synchronized (sensor) {
 					sensor.setKeepReading(false);
 					orientationListener.close();
-					instance.setAngle(0f);
+					instance.stop(servoHeading);
+					instance.stop(servoTilt);
 					try {
 						Thread.sleep(1_500L);
 					} catch (InterruptedException ie) {
