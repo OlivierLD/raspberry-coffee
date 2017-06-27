12f2a15f1a0246b4c5d8e9ea28926c67f505ccea
Olivier LeDiouris <olediouris@medallia.com>
2017-06-26 17:07:44 -0700

    Advancing on PWM

--- RasPISamples/src/java/raspisamples/wp/WiringPiSoftPWMExample.java
+++ RasPISamples/src/java/raspisamples/wp/WiringPiSoftPWMExample.java
@@ -10,6 +10,10 @@ public class WiringPiSoftPWMExample {
 
 	private static boolean go = true;
 
+	private final static int DEFAULT_SERVO_MIN = 122; // Value for Min position (-90, unit is [0..1023])
+	private final static int DEFAULT_SERVO_MAX = 615; // Value for Max position (+90, unit is [0..1023])
+
+
 	public static void main(String[] args)
 					throws InterruptedException {
 		// initialize wiringPi library
@@ -17,7 +21,7 @@ public class WiringPiSoftPWMExample {
 		int pinAddress = RaspiPin.GPIO_01.getAddress();
 		// create soft-pwm pins (min=0 ; max=100)
 //  SoftPwm.softPwmCreate(1, 0, 100); 
-		SoftPwm.softPwmCreate(pinAddress, 0, 100);
+		SoftPwm.softPwmCreate(pinAddress, DEFAULT_SERVO_MIN, DEFAULT_SERVO_MAX); // was 0, 100
 
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 			go = false;
@@ -28,13 +32,13 @@ public class WiringPiSoftPWMExample {
 		while (go) {
 	//	for (int idx = 0; idx < 5; idx++) {
 				// fade LED to fully ON
-				for (int i = 0; i <= 100; i++) {
-					SoftPwm.softPwmWrite(1, i); // TODO Use pinAddress instead of 1 ?
+				for (int i = DEFAULT_SERVO_MIN; i <= DEFAULT_SERVO_MAX; i++) {
+					SoftPwm.softPwmWrite(pinAddress, i);
 					Thread.sleep(10);
 				}
 				// fade LED to fully OFF
-				for (int i = 100; i >= 0; i--) {
-					SoftPwm.softPwmWrite(1, i); // TODO Use pinAddress instead of 1 ?
+				for (int i = DEFAULT_SERVO_MAX; i >= DEFAULT_SERVO_MIN; i--) {
+					SoftPwm.softPwmWrite(pinAddress, i);
 					Thread.sleep(10);
 				}
 	//	}
