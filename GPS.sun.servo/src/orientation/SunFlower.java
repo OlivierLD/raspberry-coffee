11901069a2de28ab2d92f67c93bcdffc1183b0d6
Olivier LeDiouris <olediouris@medallia.com>
2017-06-28 16:48:52 -0700

    Smooth moves tweaks

--- GPS.sun.servo/src/orientation/SunFlower.java
+++ GPS.sun.servo/src/orientation/SunFlower.java
@@ -246,7 +246,7 @@ public class SunFlower {
 		if (smoothMoves && Math.abs(previousHeadingAngle - f) > 5) {
 			// Smooth move for steps > 10
 			if (servoVerbose) {
-				System.out.println(String.format("Start a smooth move from heading %.02f to %.02f", previousHeadingAngle, f));
+				System.out.println(String.format("Start a smooth move from heading %d to %.02f", previousHeadingAngle, f));
 			}
 			Thread smoothy = new Thread(() -> {
 				int sign = (previousHeadingAngle > f) ? -1 : 1;
@@ -274,7 +274,7 @@ public class SunFlower {
 		if (smoothMoves && Math.abs(previousTiltAngle - f) > 5) {
 			// Smooth move for steps > 10
 			if (servoVerbose) {
-				System.out.println(String.format("Start a smooth move from tilt %.02f to %.02f", previousTiltAngle, f));
+				System.out.println(String.format("Start a smooth move from tilt %d to %.02f", previousTiltAngle, f));
 			}
 			Thread smoothy = new Thread(() -> {
 				int sign = (previousTiltAngle > f) ? -1 : 1;
