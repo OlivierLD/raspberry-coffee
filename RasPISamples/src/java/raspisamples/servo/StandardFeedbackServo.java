d4d6c34db8768596132294381b4ceed189950453
Olivier LeDiouris <olediouris@medallia.com>
2017-07-16 12:10:17 -0700

    With ADC

--- RasPISamples/src/java/raspisamples/servo/StandardFeedbackServo.java
+++ RasPISamples/src/java/raspisamples/servo/StandardFeedbackServo.java
@@ -1,5 +1,6 @@
 package raspisamples.servo;
 
+import analogdigitalconverter.mcp3008.MCP3008Reader;
 import com.pi4j.io.i2c.I2CFactory;
 import i2c.servo.pwm.PCA9685;
 
@@ -8,7 +9,13 @@ import i2c.servo.pwm.PCA9685;
  * Feedback comes from an MCP3008
  */
 public class StandardFeedbackServo {
-	public static void waitfor(long howMuch) {
+
+	private static int ADC_CHANNEL =
+					MCP3008Reader.MCP3008_input_channels.CH0.ch(); // Between 0 and 7, 8 channels on the MCP3008
+
+	private static boolean go = true;
+
+	public static void delay(long howMuch) {
 		try {
 			Thread.sleep(howMuch);
 		} catch (InterruptedException ie) {
@@ -76,7 +83,7 @@ public class StandardFeedbackServo {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		int channel = 7;
+		int channel = 7; // For the servo
 		if (args.length > 0) {
 			try {
 				channel = Integer.parseInt(args[0]);
@@ -85,49 +92,82 @@ public class StandardFeedbackServo {
 			}
 		}
 		System.out.println("Servo Channel " + channel);
+
+		MCP3008Reader.initMCP3008();
+
+		// Read the ADC in a thread
+		Thread adcReader = new Thread(() -> {
+			while (go) {
+				int adc = MCP3008Reader.readMCP3008(ADC_CHANNEL);
+				int volume = (int) (adc / 10.23); // [0, 1023] ~ [0x0000, 0x03FF] ~ [0&0, 0&1111111111]
+				System.out.println(">>                                      readAdc:" + Integer.toString(adc) +
+								" (0x" + lpad(Integer.toString(adc, 16).toUpperCase(), "0", 2) +
+								", 0&" + lpad(Integer.toString(adc, 2), "0", 8) + ")");
+				System.out.println("Volume:" + volume + "% (" + adc + ")");
+			}
+			try {
+				synchronized (Thread.currentThread()) {
+					Thread.currentThread().wait(100L);
+				}
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+			System.out.println("Bye, freeing resources.");
+			MCP3008Reader.shutdownMCP3008();
+		});
+		adcReader.start();
+
 		StandardFeedbackServo ss = new StandardFeedbackServo(channel);
 		try {
 			ss.stop();
-			waitfor(2_000);
+			delay(2_000);
 			System.out.println("Let's go, 1 by 1 (" + ss.servoMin + " to " + ss.servoMax + ")");
 			for (int i = ss.servoMin; i <= ss.servoMax; i++) {
 				System.out.println("i=" + i + ", " + (-90f + (((float) (i - ss.servoMin) / (float) ss.diff) * 180f)));
 				ss.setPWM(i);
-				waitfor(10);
+				delay(10);
 			}
 			for (int i = ss.servoMax; i >= ss.servoMin; i--) {
 				System.out.println("i=" + i + ", " + (-90f + (((float) (i - ss.servoMin) / (float) ss.diff) * 180f)));
 				ss.setPWM(i);
-				waitfor(10);
+				delay(10);
 			}
 			ss.stop();
-			waitfor(2_000);
+			delay(2_000);
 			System.out.println("Let's go, 1 deg by 1 deg, forward");
 			for (int i = ss.servoMin; i <= ss.servoMax; i += (ss.diff / 180)) {
 				System.out.println("i=" + i + ", " + Math.round(-90f + (((float) (i - ss.servoMin) / (float) ss.diff) * 180f)));
 				ss.setPWM(i);
-				waitfor(10);
+				delay(10);
 			}
 			System.out.println("... backward");
 			for (int i = ss.servoMax; i >= ss.servoMin; i -= (ss.diff / 180)) {
 				System.out.println("i=" + i + ", " + Math.round(-90f + (((float) (i - ss.servoMin) / (float) ss.diff) * 180f)));
 				ss.setPWM(i);
-				waitfor(10);
+				delay(10);
 			}
 			ss.stop();
-			waitfor(2_000);
+			delay(2_000);
 
 			System.out.println("More randomly:");
 			float[] degValues = {-10, 0, -90, 45, -30, 90, 10, 20, 30, 40, 50, 60, 70, 80, 90, 0};
 			for (float f : degValues) {
 				System.out.println("In degrees:" + f);
 				ss.setAngle(f);
-				waitfor(1_500);
+				delay(1_500);
 			}
 		} finally {
 			ss.stop();
+			go = false;
 		}
 
 		System.out.println("Done.");
+	}
+
+	private static String lpad(String str, String with, int len) {
+		String s = str;
+		while (s.length() < len)
+			s = with + s;
+		return s;
 	}
 }
