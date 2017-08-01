839f99ce71ca940e2cf1f934e702c171830d6c31
Olivier LeDiouris <olivier.lediouris@gmail.com>
2017-08-01 13:32:50 -0700

    Stream from a byte[]

---
+++ oliv.scratch.pad/some.tests/src/main/java/oliv/streams/ByteArrayStream.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Stream from a byte[]
+ * Not 100% obvious
+ */
+public class ByteArrayStream {
+    public static void main(String... args) {
+        byte[] ba = new byte[] {
+                0x00, 0x01, 0x03, 0x67
+        };
+
+        String concat = IntStream.range(0, ba.length)
+                .map(idx -> ba[idx])
+                .boxed()
+                .map(b -> String.format("%02X", (b & 0xFF)))
+                .collect(Collectors.joining(" "));
+
+        System.out.println(concat);
+    }
+}
