995b2aa1f7ef69e8a4e35dbb956b3e197f4335ee
Olivier LeDiouris <olivier.lediouris@oracle.com>
2017-08-28 15:14:05 -0700

    Tweaks on GPIO pin numbers

--- common-utils/src/java/utils/PinUtil.java
+++ common-utils/src/java/utils/PinUtil.java
@@ -6,6 +6,7 @@ import com.pi4j.io.gpio.RaspiPin;
 public class PinUtil {
 	// Disposed as on the board
 	public enum GPIOPin {
+		//      Name            #  IO WiPi                            Name            #  IO WiPi
 		/* 3v3, 1 */                                          /* 5v0, 2 */
 		GPIO_8("SDA1",          3,  2,  8, RaspiPin.GPIO_08), /* 5v0, 4 */
 		GPIO_9("SCL1",          5,  3,  9, RaspiPin.GPIO_09), /* GND, 6 */
@@ -20,16 +21,16 @@ public class PinUtil {
 		GPIO_14("SPI0_CLK",    23, 11, 14, RaspiPin.GPIO_14), GPIO_10("SPI0_CS0_N",  24,  8, 10, RaspiPin.GPIO_10),
 		/* GND, 25 */                                         GPIO_11("SPI0_CS1_N",  26,  7, 11, RaspiPin.GPIO_11),
 		SDA0("SDA0",           27, -1, 30, RaspiPin.GPIO_30), SCL0("SCL0",           28, -1, 31, RaspiPin.GPIO_31),
-		GPIO_21("GPCLK1",      29, -1, 21, RaspiPin.GPIO_21), /* GND, 30 */
-		GPIO_22("GPCLK2",      31, -1, 22, RaspiPin.GPIO_22), GPIO_26("PWM0",        32, -1, 26, RaspiPin.GPIO_26),
-		GPIO_23("PWM1",        33, -1, 23, RaspiPin.GPIO_23), /* GND, 34 */
-		GPIO_24("PCM_FS/PWM1", 35, -1, 24, RaspiPin.GPIO_24), GPIO_27("GPIO_27",     36, -1, 27, RaspiPin.GPIO_27),
-		GPIO_25("GPIO_25",     37, -1, 25, RaspiPin.GPIO_25), GPIO_28("PCM_DIN",     38, -1, 28, RaspiPin.GPIO_28),
-		/* GND, 39 */                                         GPIO_29("PCM_DOUT",    40, -1, 29, RaspiPin.GPIO_29);
+		GPIO_21("GPCLK1",      29,  5, 21, RaspiPin.GPIO_21), /* GND, 30 */
+		GPIO_22("GPCLK2",      31,  6, 22, RaspiPin.GPIO_22), GPIO_26("PWM0",        32, 12, 26, RaspiPin.GPIO_26),
+		GPIO_23("PWM1",        33, 13, 23, RaspiPin.GPIO_23), /* GND, 34 */
+		GPIO_24("PCM_FS/PWM1", 35, 19, 24, RaspiPin.GPIO_24), GPIO_27("GPIO_27",     36, 16, 27, RaspiPin.GPIO_27),
+		GPIO_25("GPIO_25",     37, 26, 25, RaspiPin.GPIO_25), GPIO_28("PCM_DIN",     38, 20, 28, RaspiPin.GPIO_28),
+		/* GND, 39 */                                         GPIO_29("PCM_DOUT",    40, 21, 29, RaspiPin.GPIO_29);
 
 		private String pinName; // Pin name
 		private int pinNumber;  // Physical
-		private int gpio;       // Used by onoff (nodejs)
+		private int gpio;       // Used by onoff (nodejs) anf Javah-io
 		private int wiringPi;   // Also used by PI4J
 		private Pin pin;
 
