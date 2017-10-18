b3bca4e01a999cdd139b8ab15123511310442d32
Olivier LeDiouris <olivier.lediouris@oracle.com>
2017-10-18 10:55:39 -0700

    Messages management

--- LoRa/src/java/arduino/ArduinoLoRaClient.java
+++ LoRa/src/java/arduino/ArduinoLoRaClient.java
@@ -9,29 +9,101 @@ import utils.DumpUtil;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 /**
  * Connect an Arduino Uno with its USB cable.
  * Serial port (/dev/ttyUSB0 below) may vary.
  *
- * Interacticve version.
- * Enter a String from the command line,
- * Send it to the Arduino,
- *
- * Can act as a server or as a client (Tx or Rx).
- * This depends on the Arduino sketch we're talking to.
- *
- * See system properties:
- * "serial.port", default "/dev/ttyUSB0"
- * "baud.rate", default "9600"
+ * Talks to the Arduino where ArdiunoRF95_TX is running
  */
 public class ArduinoLoRaClient implements SerialIOCallbacks {
+
+	private SerialCommunicator sc = null;
+	private boolean verbose = false;
+
+	private Consumer<String> callback;
+
+	public ArduinoLoRaClient(String portName, int baudRate, Consumer<String> onData)
+			throws Exception {
+
+		if (portName.isEmpty()) {
+			throw new RuntimeException("Please provide a serial port name");
+		}
+		if (onData == null) {
+			throw new RuntimeException("Please provide a callback function");
+		}
+		this.verbose = "true".equals(System.getProperty("lora.verbose", "false"));
+		sc = new SerialCommunicator(this);
+		sc.setVerbose("true".equals(System.getProperty("serial.verbose", "false")));
+
+		this.callback = onData;
+
+		Map<String, CommPortIdentifier> pm = sc.getPortList();
+		Set<String> ports = pm.keySet();
+		if (ports.size() == 0) {
+			System.out.println("No serial port found.");
+			System.out.println("Did you run as administrator (sudo) ?");
+		}
+		System.out.println("== Serial Port List ==");
+		for (String port : ports) {
+			System.out.println("-> " + port);
+		}
+		System.out.println("======================");
+
+		String serialPortName = portName;
+		System.out.println(String.format("Opening port %s:%d", serialPortName, baudRate));
+
+		CommPortIdentifier arduinoPort = pm.get(serialPortName);
+		if (arduinoPort == null) {
+			System.out.println(String.format("Port %s not found in the list", serialPortName));
+//		System.exit(1);
+		}
+
+		// Try this if not found in list...
+		if (arduinoPort == null) {
+			if (this.verbose) System.out.println("Trying plan B...");
+			try {
+				arduinoPort = CommPortIdentifier.getPortIdentifier(serialPortName);
+			} catch (NoSuchPortException nspe) {
+				throw nspe;
+			}
+		}
+
+		try {
+			/*
+			 * Serial connection here
+			 */
+			sc.connect(arduinoPort, "Arduino", baudRate);
+			boolean b = sc.initIOStream();
+			System.out.println("IO Streams " + (b ? "" : "NOT ") + "initialized");
+			sc.initListener();
+
+			Thread.sleep(500L);
+			// Wake up! Boom Boom!
+			for (int i = 0; i < 2; i++) {
+				sc.writeData("\n");
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void sendToLora(String str)
+			throws IOException {
+		this.sc.writeData(str);
+	}
+
+	public void close()
+			throws IOException {
+		this.sc.disconnect();
+	}
+
 	@Override
 	public void connected(boolean b) {
 		System.out.println("Arduino connected: " + b);
 	}
 
-	private int lenToRead = 0;
 	private int bufferIdx = 0;
 	private byte[] serialBuffer = new byte[256];
 
@@ -47,13 +119,12 @@ public class ArduinoLoRaClient implements SerialIOCallbacks {
 			}
 			arduinoOutput(mess);
 			// Reset
-			lenToRead = 0;
 			bufferIdx = 0;
 		}
 	}
 
-	public void arduinoOutput(byte[] mess) {
-		if ("true".equals(System.getProperty("serial.verbose", "false"))) { // verbose...
+	private void arduinoOutput(byte[] mess) {
+		if (this.verbose) {
 			try {
 				String[] sa = DumpUtil.dualDump(mess);
 				if (sa != null) {
@@ -66,88 +137,6 @@ public class ArduinoLoRaClient implements SerialIOCallbacks {
 				ex.printStackTrace();
 			}
 		}
-	}
-
-	public static void main(String[] args) {
-		final ArduinoLoRaClient caller = new ArduinoLoRaClient();
-		final SerialCommunicator sc = new SerialCommunicator(caller);
-		sc.setVerbose(false);
-
-		Map<String, CommPortIdentifier> pm = sc.getPortList();
-		Set<String> ports = pm.keySet();
-		if (ports.size() == 0) {
-			System.out.println("No serial port found.");
-			System.out.println("Did you run as administrator (sudo) ?");
-		}
-		System.out.println("== Serial Port List ==");
-		for (String port : ports) {
-			System.out.println("-> " + port);
-		}
-		System.out.println("======================");
-
-		String serialPortName = System.getProperty("serial.port", "/dev/ttyUSB0");
-		String baudRateStr = System.getProperty("baud.rate", "9600");
-		System.out.println(String.format("Opening port %s:%s", serialPortName, baudRateStr));
-
-		CommPortIdentifier arduinoPort = pm.get(serialPortName);
-		if (arduinoPort == null) {
-			System.out.println(String.format("Port %s not found in the list", serialPortName));
-//		System.exit(1);
-		}
-
-		// Try this if not found in list...
-		if (arduinoPort == null) {
-			System.out.println("Trying plan B...");
-			try {
-				arduinoPort = CommPortIdentifier.getPortIdentifier(serialPortName);
-			} catch (NoSuchPortException nspe) {
-				System.err.println(serialPortName + ": No Such Port");
-				nspe.printStackTrace();
-				if ("/dev/ttyACM0".equals(serialPortName)) {
-					System.err.println("Note: There is some bug in libRxTx-java regarding the access to /dev/ttyACM0");
-					System.err.println("If this is your case, try creating a symbolic link on the port, and access it through its link:");
-					System.err.println(" $ sudo ln -s /dev/ttyACM0 /dev/ttyS80");
-					System.err.println("Then try reading or writing on /dev/ttyS80");
-				}
-				System.exit(1);
-			}
-		}
-
-		System.out.println("Enter 'Q' at the prompt to quit.");
-		try {
-			/*
-			 * Serial connection here
-			 */
-			sc.connect(arduinoPort, "Arduino", Integer.parseInt(baudRateStr));
-			boolean b = sc.initIOStream();
-			System.out.println("IO Streams " + (b ? "" : "NOT ") + "initialized");
-			sc.initListener();
-
-			Thread.sleep(500L);
-			// Wake up!
-			for (int i = 0; i < 2; i++) {
-				sc.writeData("\n");
-			}
-
-			boolean keepWorking = true;
-			while (keepWorking) {
-				String str = utils.StaticUtil.userInput("?> ");
-				if ("Q".equalsIgnoreCase(str.trim())) {
-					keepWorking = false;
-				} else {
-					sc.writeData(str + "\n");
-				}
-			}
-			System.out.println("Exiting.");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
-		try {
-			sc.disconnect();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		System.out.println("Done.");
+		this.callback.accept(new String(mess));
 	}
 }
