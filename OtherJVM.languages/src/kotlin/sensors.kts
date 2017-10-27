b85794ef4d24666cb2e4278dde1aff74874c2f3a
Olivier LeDiouris <olivier.lediouris@oracle.com>
2017-10-27 15:35:54 -0700

    Serious cleanup

--- OtherJVM.languages/src/kotlin/sensors.kts
+++
@@ -1,5 +0,0 @@
-import i2c.sensor.BME280
-
-val bme280 = BME280()
-val temp = bme280.readTemperature()
-println("Temp= $temp \u00baC")
---
+++ OtherJVM.languages/src/kotlinscripts/sensors.kts
@@ -0,0 +1,7 @@
+//import i2c.sensor.BME280
+//
+//val bme280 = BME280()
+//val temp = bme280.readTemperature()
+//println("Temp= $temp \u00baC")
+
+println("Gradle and kts do not like each other...")
