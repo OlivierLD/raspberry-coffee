7fec6fbe0580e31e2dfa2239d917492a68e934e0
OlivierLD <olivier.lediouris@gmail.com>
2017-12-20 15:34:40 -0800

    Stepper

--- RESTNavServer/webui.sh
+++ RESTNavServer/webui.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-chromium --incognito --kiosk http://localhost:9999/web/
+# chromium --incognito --kiosk http://localhost:9999/web/
+chromium-browser --incognito --kiosk http://localhost:9999/web/
