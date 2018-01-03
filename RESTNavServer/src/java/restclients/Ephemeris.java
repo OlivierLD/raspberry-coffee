2ae02da2027af1448bcb3cc93ec58ede051b35c4
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-01-03 08:18:46 -0800

    Accessing services, getting data from Java

--- RESTNavServer/src/java/restclients/Ephemeris.java
+++ RESTNavServer/src/java/restclients/Ephemeris.java
@@ -1,11 +1,18 @@
 package restclients;
 
+import astrorest.RESTImplementation;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
 import http.client.HTTPClient;
 
+import java.io.StringReader;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Ephemeris {
 
@@ -13,6 +20,17 @@ public class Ephemeris {
 	private final static SimpleDateFormat TZ_ABR = new SimpleDateFormat("z");
 	private final static SimpleDateFormat DATE_FMT = new SimpleDateFormat("yyyy-MM-dd");
 	private final static SimpleDateFormat TIME_FMT = new SimpleDateFormat("HH:mm");
+
+	// Get the station position:
+	// GET http://localhost:9999/tide/tide-stations/Ocean%20Beach%2C%20California
+
+	// Sun Data:
+	// POST http://localhost:9999/astro/sun-between-dates?from=2017-09-01T00:00:00&to=2017-09-02T00:00:01&tz=America%2FLos_Angeles
+	// payload { latitude: 37.76661945, longitude: -122.5166988 }
+
+	// Tide Table:
+	// POST http://localhost:9999/tide/tide-stations/Ocean%20Beach%2C%20California/wh?from=2018-01-03T00:00:00&to=2018-01-04T00:00:01
+
 
 	/**
 	 * Requires the following parameters:
@@ -25,8 +43,6 @@ public class Ephemeris {
 	 * @param args
 	 */
 	public static void main(String... args) throws Exception {
-		// POST http://localhost:9999/astro/sun-between-dates?from=2017-09-01T00:00:00&to=2017-09-02T00:00:01&tz=America%2FLos_Angeles
-		// payload { latitude: 37.76661945, longitude: -122.5166988 }
 
 		Calendar now = Calendar.getInstance();
 		now.set(Calendar.HOUR_OF_DAY, 0);
@@ -56,6 +72,25 @@ public class Ephemeris {
 					new HashMap<>(),
 					payload);
 			System.out.println(String.format("Code:%d\nPayload=%s", response.getCode(), response.getPayload()));
+			// Parse payload
+			Gson gson = new GsonBuilder().create();
+			StringReader stringReader = new StringReader(response.getPayload());
+			try {
+				Map<String, LinkedTreeMap> map = gson.fromJson(stringReader, Map.class);
+				System.out.println("!");
+				for (String epoch : map.keySet()) {
+					LinkedTreeMap linkedTreeMap = map.get(epoch);
+					Date date = new Date(Long.parseLong(epoch));
+					System.out.println("Date > " + date.toString());
+					double riseTime = (double)linkedTreeMap.get("riseTime");
+					Date riseTimeDate = new Date(Math.round(riseTime));
+					System.out.println("Sun Rise > " + riseTimeDate.toString());
+				}
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
