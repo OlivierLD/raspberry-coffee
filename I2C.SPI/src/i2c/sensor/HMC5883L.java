ec5f45e7235851d260fe76809b3015afe1973926
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-01-19 16:08:52 -0800

    Compass

--- I2C.SPI/src/i2c/sensor/HMC5883L.java
+++ I2C.SPI/src/i2c/sensor/HMC5883L.java
@@ -17,15 +17,27 @@ import static utils.TimeUtil.delay;
 public class HMC5883L {
 	private final static int HMC5883L_ADDRESS = 0x1E;
 
-	private final static int HMC5883L_CONTINUOUS_SAMPLING = 0x00;
+	private final static int HMC5883L_CONTINUOUS_SAMPLING    = 0x00;
 	private final static int HMC5883L_13_GAIN_LSB_GAUSS_1090 = 0x20; // 1.3 gain LSb / Gauss 1090 (default)
-	private final static int HMC5883L_8_SAMPLES_15HZ = 0x70; // Set to 8 samples @ 15Hz.
 
-	private final static int HMC5883L_X_ADR = 0x03;
-	private final static int HMC5883L_Y_ADR = 0x07;
-	private final static int HMC5883L_Z_ADR = 0x05;
+	private final static int HMC5883_MAGGAIN_1_9                        = 0x40;  // +/- 1.9
+	private final static int HMC5883_MAGGAIN_2_5                        = 0x60;  // +/- 2.5
+	private final static int HMC5883_MAGGAIN_4_0                        = 0x80;  // +/- 4.0
+	private final static int HMC5883_MAGGAIN_4_7                        = 0xA0;  // +/- 4.7
+	private final static int HMC5883_MAGGAIN_5_6                        = 0xC0;  // +/- 5.6
+	private final static int HMC5883_MAGGAIN_8_1                        = 0xE0;  // +/- 8.1
+
+	private final static int HMC5883L_8_SAMPLES_15HZ         = 0x70; // Set to 8 samples @ 15Hz.
+
+	private final static int HMC5883L_X_ADR = 0x03; // 0x03 & 0x04
+	private final static int HMC5883L_Z_ADR = 0x05; // 0x05 & 0x06
+	private final static int HMC5883L_Y_ADR = 0x07; // 0x07 & 0x08
 
 	private final static float SCALE = 0.92f;
+
+	private static float _hmc5883_Gauss_LSB_XY = 1100.0F;  // Varies with gain
+	private static float _hmc5883_Gauss_LSB_Z  = 980.0F;   // Varies with gain
+
 
 	private static boolean verbose = "true".equals(System.getProperty("hmc5883l.verbose", "false"));
 
@@ -83,14 +95,26 @@ public class HMC5883L {
 	public double readHeading() throws IOException {
 		double heading = 0f;
 
-		byte[] w = new byte[]{(byte) HMC5883L_8_SAMPLES_15HZ,
-						(byte) HMC5883L_13_GAIN_LSB_GAUSS_1090,
-						(byte) HMC5883L_CONTINUOUS_SAMPLING};
-		hcm5883l.write(w, 0, 3); // BeginTrans, write 3 bytes, EndTrans.
+//		byte[] w = new byte[]{
+//			(byte) HMC5883L_8_SAMPLES_15HZ,
+//			(byte) HMC5883L_13_GAIN_LSB_GAUSS_1090,
+//			(byte) HMC5883L_CONTINUOUS_SAMPLING
+//		};
+//		hcm5883l.write(w, 0, 3); // BeginTrans, write 3 bytes, EndTrans.
 
-		double xOut = readWord_2C(HMC5883L_X_ADR) * SCALE;
-		double yOut = readWord_2C(HMC5883L_Y_ADR) * SCALE;
-		double zOut = readWord_2C(HMC5883L_Z_ADR) * SCALE;
+		hcm5883l.write((byte)0x03);
+
+		double xOut = readWord_2C(HMC5883L_X_ADR);
+		double yOut = readWord_2C(HMC5883L_Y_ADR);
+		double zOut = readWord_2C(HMC5883L_Z_ADR);
+
+//		xOut *= SCALE;
+//		yOut *= SCALE;
+//		zOut *= SCALE;
+
+		xOut /= _hmc5883_Gauss_LSB_XY;
+		yOut /= _hmc5883_Gauss_LSB_XY;
+		zOut /= _hmc5883_Gauss_LSB_Z;
 
 		if (verbose) {
 			System.out.println("xOut:" + xOut);
