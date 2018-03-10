d451682820458f57b7b2e9d7ea9831ad54a0d06f
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-03-10 11:37:46 -0800

    Duplicated property in the script

--- RasPISamples/weather.station.reader.sh
+++ RasPISamples/weather.station.reader.sh
@@ -5,7 +5,6 @@ CP=./build/libs/RasPISamples-1.0-all.jar
 JAVA_OPTIONS=
 JAVA_OPTIONS="$JAVA_OPTIONS -Dweather.station.verbose=true"
 JAVA_OPTIONS="$JAVA_OPTIONS -Dsdl.weather.station.verbose=true"
-JAVA_OPTIONS="$JAVA_OPTIONS -Dmysql.logger.verbose=true"
 JAVA_OPTIONS="$JAVA_OPTIONS -Dws.wspeed.coeff=1"
 JAVA_OPTIONS="$JAVA_OPTIONS -Dws.wdir.offset=30"
 # JAVA_OPTIONS="$JAVA_OPTIONS -Ddebounce.time.millisec=30"
@@ -22,7 +21,7 @@ DATA_LOGGERS="$DATA_LOGGERS,weatherstation.logger.MySQLLoggerImpl"
 JAVA_OPTIONS="$JAVA_OPTIONS -Ddata.logger=$DATA_LOGGERS"
 # Options for MySQL logger. 600_000ms = 10 minutes
 JAVA_OPTIONS="$JAVA_OPTIONS -Dws.between.logs=600000"
-JAVA_OPTIONS="$JAVA_OPTIONS -Dmysql.logger.verbose=true"
+JAVA_OPTIONS="$JAVA_OPTIONS -Dmysql.logger.verbose=false"
 # Option for TCP logger
 JAVA_OPTIONS="$JAVA_OPTIONS -Dtcp.verbose=false"
 JAVA_OPTIONS="$JAVA_OPTIONS -Dstation.lat=37.7489 -Dstation.lng=-122.5070"
