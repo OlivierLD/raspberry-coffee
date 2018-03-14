7f591cd9404f930331091860ed6bb82816d820a2
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-03-14 14:43:49 -0700

    Rain verbose

--- WeatherStation/src/weatherstation/SDLWeather80422.java
+++ WeatherStation/src/weatherstation/SDLWeather80422.java
@@ -231,7 +231,7 @@ public class SDLWeather80422 {
 	}
 
 	public float getCurrentRainTotal() {
-		if (verbose || verboseRain) {
+		if (false && (verbose || verboseRain)) {
 			System.out.println(String.format(">>> [%s] Method currentRainTotal >> RainAmount: %f",
 					SDF.format(new Date()),
 					currentRainCount));
