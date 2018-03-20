dfb3010e8d824b7c06542893eab541a887d8bb3d
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-03-20 15:35:06 -0700

    Added sshpass

--- RasPISamples/remote.snap.sh
+++ RasPISamples/remote.snap.sh
@@ -2,8 +2,9 @@
 #
 # Take a snapshot on the HTTPServer/Logger, and download it
 #
+PATH=$PATH:/usr/local/bin
 curl http://192.168.42.6:8080/snap
-scp pi@192.168.42.6:~/raspberry-pi4j-samples/RasPISamples/web/snap-test.jpg ./web
+sshpass -p 'pi' scp pi@192.168.42.6:~/raspberry-pi4j-samples/RasPISamples/web/snap-test.jpg ./web
 open web
 #
 
