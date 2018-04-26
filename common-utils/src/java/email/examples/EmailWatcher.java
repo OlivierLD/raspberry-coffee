b9ec2970753e3562d55d12c576ecaf05d3c56abe
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-04-26 14:25:07 -0700

    Back to the same cmd

--- common-utils/src/java/email/examples/EmailWatcher.java
+++ common-utils/src/java/email/examples/EmailWatcher.java
@@ -201,7 +201,7 @@ public class EmailWatcher {
 			StringBuffer output = new StringBuffer();
 			for (String cmd : cmds) {
 				if (!cmd.trim().isEmpty()) {
-					Process p = Runtime.getRuntime().exec(new String[] {"/bin/bash", "-c",  cmd});
+					Process p = Runtime.getRuntime().exec(cmd);
 					BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream())); // stdout
 					BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream())); // stderr
 					String line;
