0923a349648f6bbba87d15ed70edc2fc575265f1
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-05-12 13:44:48 -0700

    6_144 to 4_096

--- ADC-benchmark/src/java/adcbenchmark/ads1015/MainADS1015Sample33.java
+++ ADC-benchmark/src/java/adcbenchmark/ads1015/MainADS1015Sample33.java
@@ -6,7 +6,9 @@ import i2c.adc.ADS1x15;
 public class MainADS1015Sample33 {
 	private static ADS1x15 ads1015 = null;
 
-	private static int gain = ADS1x15.pgaADS1x15.ADS1015_REG_CONFIG_PGA_4_096V.meaning(); // 6_144;
+	private static ADS1x15.pgaADS1x15 gainRef = ADS1x15.pgaADS1x15.ADS1015_REG_CONFIG_PGA_4_096V;
+
+	private static int gain = gainRef.meaning(); // 6_144;
 	private static int sps  = 250;
 
 	private static boolean go = true;
@@ -23,13 +25,11 @@ public class MainADS1015Sample33 {
 		}));
 
 		while (go) {
-			float voltage;
 			float value = ads1015.readADCSingleEnded(
 					ADS1x15.Channels.CHANNEL_0,
 					gain,
 					sps);
-			System.out.println("ADC Value:" + value);
-			voltage = value / 1_000f;
+			System.out.println(String.format("ADC Value: %f, Voltage: %.02f ", value, (value / gain)));
 		}
 		System.out.println("Bye!");
 	}
