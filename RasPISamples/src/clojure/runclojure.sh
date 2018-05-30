fe7e7effc655203adb3adab0f70b6d546411f4df
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-05-30 11:13:12 -0700

    White display with red hand

--- RasPISamples/src/clojure/runclojure.sh
+++ RasPISamples/src/clojure/runclojure.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Resources:
+#  https://learnxinyminutes.com/docs/clojure/
+#
 CLOJURE_JAR=/usr/local/Cellar/clojure/1.9.0.381/libexec/clojure-tools-1.9.0.381.jar
 CP=.:$CLOJURE_JAR
 #
