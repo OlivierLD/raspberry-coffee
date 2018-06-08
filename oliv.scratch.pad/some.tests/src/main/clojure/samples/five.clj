18ac3d64846f02307881b9761b641a9affb22a3c
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-06-08 16:49:49 -0700

    Clojure, wow

--- oliv.scratch.pad/some.tests/src/main/clojure/samples/five.clj
+++ oliv.scratch.pad/some.tests/src/main/clojure/samples/five.clj
@@ -7,3 +7,15 @@
       (println "Row" (str row) (str acc))))
   (println "Acc" (str acc)))
 
+(println "----------------------")
+
+(let [dim (count [1, 2, 3, 4])]
+  (println "will loop" dim " time(s)")
+  (def acc 0)
+  (for [row (range dim)]
+    (if (< row dim)
+      (inc acc)
+      (println "Row" (str row) (str acc))))
+  (println "Acc" (str acc)))
+
+(println "----------------------")
