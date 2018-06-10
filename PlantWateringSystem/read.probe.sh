5d6fdb47ee13b5a6bf6807ae3608a40a3ac9c3d9
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-06-10 15:05:27 -0700

    debug

--- PlantWateringSystem/read.probe.sh
+++ PlantWateringSystem/read.probe.sh
@@ -2,8 +2,11 @@
 CP=build/libs/PlantWateringSystem-1.0-all.jar
 #
 JAVA_OPTIONS=
-# For remote debugging:
-JAVA_OPTIONS="$JAVA_OPTIONS -client -agentlib:jdwp=transport=dt_socket,server=y,address=4000"
+if [ "$1" == "debug" ]
+then
+  # For remote debugging:
+  JAVA_OPTIONS="$JAVA_OPTIONS -client -agentlib:jdwp=transport=dt_socket,server=y,address=4000"
+fi
 # For remote JVM Monitoring
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dcom.sun.management.jmxremote.port=1234 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=raspberrypi-boat"
 java $JAVA_OPTIONS -cp $CP main.STH10
