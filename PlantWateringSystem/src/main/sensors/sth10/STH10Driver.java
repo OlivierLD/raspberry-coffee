4eabbec795bb1c33a2d1cd33e49c1e45e00605f6
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-06-12 15:30:58 -0700

    Pin numbers

--- PlantWateringSystem/src/main/sensors/sth10/STH10Driver.java
+++ PlantWateringSystem/src/main/sensors/sth10/STH10Driver.java
@@ -63,7 +63,7 @@ public class STH10Driver {
 	}
 
 	private String pinDisplay(GpioPinDigital pin) {
-		return (String.format("%d [%s]", PinUtil.findByPin((Pin)pin).gpio(), pin.equals(this.data) ? "DATA" : (pin.equals(this.clock) ? "CLOCK" : "!UNKNOWN!")));
+		return (String.format("%d [%s]", PinUtil.findByPin(pin.getPin()).gpio(), pin.equals(this.data) ? "DATA" : (pin.equals(this.clock) ? "CLOCK" : "!UNKNOWN!")));
 	}
 
 	private void resetConnection() {
