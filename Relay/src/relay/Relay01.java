f005a60193b229b6e3d18cdaf756badaa645af66
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-06-15 14:51:09 -0700

    Relays

--- Relay/src/relay/Relay01.java
+++ Relay/src/relay/Relay01.java
@@ -12,42 +12,40 @@ import com.pi4j.io.gpio.RaspiPin;
  * To drive a relay, a relay board is required.
  * (it may contain what's needed to drive the relay with 3.3v)
  */
-public class Relay01
-{
-  public static void main(String... args) throws InterruptedException
-  {
+public class Relay01 {
+	public static void main(String... args) throws InterruptedException {
 
-    System.out.println("GPIO Control - pin 00/#17 and 01/#18 ... started.");
-    System.out.println("(Labelled #17 an #18 on the cobbler.)");
+		System.out.println("GPIO Control - pin 00/#17 and 01/#18 ... started.");
+		System.out.println("(Labelled #17 and #18 on the cobbler.)");
 
-    // create gpio controller
-    final GpioController gpio = GpioFactory.getInstance();
+		// create gpio controller
+		final GpioController gpio = GpioFactory.getInstance();
 
-    // For a relay it seems that HIGH means NC (Normally Closed)...
-    final GpioPinDigitalOutput pin17 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Relay1", PinState.HIGH);
-    final GpioPinDigitalOutput pin18 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Relay2", PinState.HIGH);
-    System.out.println("--> GPIO state should be: OFF, and it is ");
+		// For a relay it seems that HIGH means NC (Normally Closed)...
+		final GpioPinDigitalOutput pin17 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Relay1", PinState.HIGH);
+		final GpioPinDigitalOutput pin18 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Relay2", PinState.HIGH);
+		System.out.println("--> GPIO state should be: OFF, and it is ");
 
-    Thread.sleep(1_000);
+		Thread.sleep(1_000);
 
-    pin17.low();
-    System.out.println("--> pin 17 should be: ON, and it is " + (pin17.isHigh()?"OFF":"ON"));
+		pin17.low();
+		System.out.println("--> pin 17 should be: ON, and it is " + (pin17.isHigh() ? "OFF" : "ON"));
 
-    Thread.sleep(1_000);
+		Thread.sleep(1_000);
 
-    pin17.high();
-    System.out.println("--> pin 17 should be: OFF");
+		pin17.high();
+		System.out.println("--> pin 17 should be: OFF");
 
-    Thread.sleep(1_000);
+		Thread.sleep(1_000);
 
-    pin18.low();
-    System.out.println("--> pin 18 should be: ON");
+		pin18.low();
+		System.out.println("--> pin 18 should be: ON");
 
-    Thread.sleep(1_000);
+		Thread.sleep(1_000);
 
-    pin18.high();
-    System.out.println("--> pin 18 should be: OFF");
+		pin18.high();
+		System.out.println("--> pin 18 should be: OFF");
 
-    gpio.shutdown();
-  }
+		gpio.shutdown();
+	}
 }
