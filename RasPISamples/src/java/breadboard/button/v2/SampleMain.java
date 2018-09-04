f0ecd7f9513c8e3d0bfc47e36bb1ef25c875ba65
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-09-04 14:42:49 -0700

    Working on minimal user interface, PushButtonMaster v2

---
+++ RasPISamples/src/java/breadboard/button/v2/SampleMain.java
@@ -0,0 +1,43 @@
+package breadboard.button.v2;
+
+import com.pi4j.io.gpio.RaspiPin;
+
+import java.util.function.Consumer;
+
+public class SampleMain {
+
+	public static void main(String... args) {
+
+		Consumer<Void> onClick = (Void v) -> {
+			System.out.println("Single click");
+		};
+		Consumer<Void> onDoubleClick = (Void v) -> {
+			System.out.println("Double click");
+		};
+		Consumer<Void> onLongClick = (Void v) -> {
+			System.out.println("Long click");
+		};
+
+		PushButtonMaster pbm = new PushButtonMaster(onClick, onDoubleClick, onLongClick);
+		pbm.initCtx(RaspiPin.GPIO_01);
+
+		final Thread me = Thread.currentThread();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			synchronized (me) {
+				me.notify();
+			}
+		}));
+
+		synchronized(me) {
+			try {
+				me.wait();
+			} catch (InterruptedException ie) {
+				me.interrupt();
+			}
+		}
+
+		pbm.freeResources();
+		System.out.println("\nBye!");
+	}
+}
