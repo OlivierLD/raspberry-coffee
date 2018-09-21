a074976a5c5bde76c5f09b8a8512a73e7eec99fd
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-09-20 17:51:06 -0700

    Formats

--- RangeSensor/run.sh
+++ RangeSensor/run.sh
@@ -2,7 +2,7 @@
 #
 JAVA_OPTS=
 # JAVA_OPTS="$JAVA_OPTS -client -agentlib:jdwp=transport=dt_socket,server=y,address=1044"
-# JAVA_OPTS="$JAVA_OPTS -Dverbose=true"
+JAVA_OPTS="$JAVA_OPTS -Dhc_sr04.verbose=true"
 CP=./build/libs/RangeSensor-1.0-all.jar
 sudo java $JAVA_OPTS -cp $CP rangesensor.HC_SR04
 
