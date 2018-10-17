8610d830cdd010dfb078728a1f7a9547e36e50c4
Morel Sébastien <morel.seb@gmail.com>
2018-10-16 17:33:04 -0700

    PHP 7.2 - more fancy organization

--- Project.Trunk/System.Languages/src/php/resolution.php
+++ Project.Trunk/System.Languages/src/php/resolution.php
@@ -4,70 +4,68 @@
 
 </head>
 <body style="font-family: 'Source Code Pro', 'Courier New', Helvetica, Geneva">
- <h2>Solution</h2>
+<h2>Solution</h2>
 
 <?php
 
-include './system.php';
+include __DIR__.'/autoload.php';
 
 try {
-    $dim = $_POST["dim"];
-    $matrixElements = array();
-    $coefficients = array();
+    $dim            = $_POST["dim"];
+    $matrixElements = [];
+    $coefficients   = [];
 
     for ($row = 0; $row < $dim; $row++) {
         for ($col = 0; $col < $dim; $col++) {
-            $matrixElements[$row][$col] = $_POST["row-" . $row . "-col-" . $col];
+            $matrixElements[$row][$col] = $_POST["row-".$row."-col-".$col];
         }
     }
 
     for ($row = 0; $row < $dim; $row++) {
-        $coefficients[$row] = $_POST["coeff-" . $row];
+        $coefficients[$row] = $_POST["coeff-".$row];
     }
 
     $matrix = new SquareMatrix($dim, true);
 
     for ($row = 0; $row < $dim; $row++) {
         for ($col = 0; $col < $dim; $col++) {
-            $matrix->setElementAt($row, $col, $matrixElements[$row][$col]);
+            $matrix->setElementAt($row, $col, (float) $matrixElements[$row][$col]);
         }
     }
 
-    echo "Matrix dim: ", $matrix->getDim();
-    echo "<br/>";
+    echo "Matrix dim: {$matrix->getDimension()} <br />";
     $elements = $matrix->getElements();
     echo "<br/>";
-    echo "Det: ", MatrixUtil::determinant($matrix);
-    echo "<br/>";
-
+    echo "Det: {$matrix->determinant()}<br />";
+    echo "<hr/>Matrix<br />";
+    echo $matrix;
     echo "<hr/>";
-    echo "Matrix";
-    echo MatrixUtil::printMatrix($matrix);
-    echo "<hr/>";
-    $constants = array();
+    $constants = [];
     for ($row = 0; $row < $dim; $row++) {
         $constants[$row] = $coefficients[$row];
     }
-
     echo "Constants: ", implode(", ", $constants);
     echo "<hr/>";
 
     try {
-        $coeffs = MatrixUtil::solveSystem($matrix, $constants);
+        $coeffs = MatrixSolver::solveSystem($matrix, $constants);
 
-        $coeffNames = array('A', 'B', 'C', 'D', 'E', 'F', 'G', "H", 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+        $coeffNames = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', "H", 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        ];
 
-				// Results
+        // Results
         echo "System Solution:<br/>";
-        for ($i = 0; $i < count($coeffs); $i++) {
+        $count = count($coeffs);
+        for ($i = 0; $i < $count; $i++) {
             echo $coeffNames[$i], " : ", $coeffs[$i], "<br/>";
         }
     } catch (Exception $ex) {
-    	echo "System resolution: ", $ex->getMessage(), "\n";
-		}
+        echo "System resolution: ", $ex->getMessage(), "\n";
+    }
 } catch (Exception $ex) {
-    echo 'Caught exception: ',  $ex->getMessage(), "\n";
+    echo 'Caught exception: ', $ex->getMessage(), "\n";
 }
 ?>
 
