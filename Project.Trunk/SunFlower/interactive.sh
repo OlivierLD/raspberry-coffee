82f0119cd44e9242ab2821f72f762d429b318d9c
Olivier LeDiouris <olivier.lediouris@oracle.com>
2018-11-10 13:11:08 -0800

    Small stuff

--- Project.Trunk/SunFlower/interactive.sh
+++ Project.Trunk/SunFlower/interactive.sh
@@ -6,7 +6,7 @@ CP=./build/libs/SunFlower-1.0-all.jar
 JAVA_OPTS=
 JAVA_OPTS="$JAVA_OPTS -Dlatitude=37.7489 -Dlongitude=-122.5070 -Declination=14"
 JAVA_OPTS="$JAVA_OPTS -Dorient.verbose=true -Dastro.verbose=true"
-JAVA_OPTS="$JAVA_OPTS -tilt.verbose=true"
+JAVA_OPTS="$JAVA_OPTS -Dtilt.verbose=true"
 # JAVA_OPTS="$JAVA_OPTS -Dmanual.entry=true"
 JAVA_OPTS="$JAVA_OPTS -Dansi.console=false"
 #
