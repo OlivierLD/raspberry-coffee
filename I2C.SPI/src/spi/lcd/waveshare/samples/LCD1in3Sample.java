d860e490981995ebcd6e2239c0757d52a921ed3f
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-02-03 11:46:21 -0800

    Up, and down

--- I2C.SPI/src/spi/lcd/waveshare/samples/LCD1in3Sample.java
+++ I2C.SPI/src/spi/lcd/waveshare/samples/LCD1in3Sample.java
@@ -1,12 +1,14 @@
 package spi.lcd.waveshare.samples;
 
 import spi.lcd.waveshare.LCD1in3;
+import utils.TimeUtil;
 
 public class LCD1in3Sample {
 
 	public static void main(String... args) {
 		LCD1in3 lcd = new LCD1in3();
-
+		TimeUtil.delay(2_000L);
+		lcd.shutdown();
 	}
 
 }
