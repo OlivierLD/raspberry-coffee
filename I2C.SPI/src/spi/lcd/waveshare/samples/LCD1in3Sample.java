b7ca89a09acfb07258153b03c932bd27ef7d7c1f
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-02-04 18:35:38 -0800

    Aha

--- I2C.SPI/src/spi/lcd/waveshare/samples/LCD1in3Sample.java
+++ I2C.SPI/src/spi/lcd/waveshare/samples/LCD1in3Sample.java
@@ -3,6 +3,7 @@ package spi.lcd.waveshare.samples;
 import spi.lcd.waveshare.LCD1in3;
 import utils.TimeUtil;
 
+import static spi.lcd.waveshare.LCD1in3.GREEN;
 import static spi.lcd.waveshare.LCD1in3.IMAGE_COLOR_POSITIVE;
 import static spi.lcd.waveshare.LCD1in3.IMAGE_RGB;
 import static spi.lcd.waveshare.LCD1in3.IMAGE_ROTATE_0;
@@ -28,8 +29,9 @@ public class LCD1in3Sample {
 		lcd.GUIDrawPoint(5, 25, RED, LCD1in3.DotPixel.DOT_PIXEL_2X2, LCD1in3.DOT_STYLE_DFT);
 		lcd.GUIDrawPoint(5, 40, BLACK, LCD1in3.DotPixel.DOT_PIXEL_3X3, LCD1in3.DOT_STYLE_DFT);
 		lcd.GUIDrawPoint(5, 55, BLACK, LCD1in3.DotPixel.DOT_PIXEL_4X4, LCD1in3.DOT_STYLE_DFT);
-
-
+		lcd.GUIDrawPoint(5, 70, GREEN, LCD1in3.DotPixel.DOT_PIXEL_5X5, LCD1in3.DOT_STYLE_DFT);
+		lcd.GUIDrawPoint(5, 85, GREEN, LCD1in3.DotPixel.DOT_PIXEL_6X6, LCD1in3.DOT_STYLE_DFT);
+		
 		System.out.println("Displaying...");
 		lcd.LCDDisplay();
 
