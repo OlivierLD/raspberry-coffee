74b7026ed463a6f235a4bcc266ef9da6e845a0fe
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-02-11 17:43:19 -0800

    Tweaks in teh patchwork

--- I2C.SPI/src/spi/lcd/waveshare/samples/LCD1in3Sample.java
+++ I2C.SPI/src/spi/lcd/waveshare/samples/LCD1in3Sample.java
@@ -205,7 +205,8 @@ public class LCD1in3Sample {
 			lcd.GUIDrawRectangle(topX, topY, topX + width, topY + height, color, LCD1in3.DrawFill.DRAW_FILL_FULL, LCD1in3.DotPixel.DOT_PIXEL_1X1);
 			//
 			try {
-				lcd.LCDDisplayWindows(topX, topY, topX + width - 1, topY + height - 1);
+//			lcd.LCDDisplayWindows(topX, topY, topX + width - 1, topY + height - 1);
+				lcd.LCDDisplayWindows(topX + 1, topY + 1, topX + width, topY + height);
 			} catch (Exception ex) {
 				System.err.println(String.format("Cell X %d, Cell Y %d, topX:%d, topY:%d, h:%d, w:%d", x, y, topX, topY, height, width));
 				ex.printStackTrace();
