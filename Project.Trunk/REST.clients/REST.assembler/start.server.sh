f7cb2c9a1dddaa5400e806eaa8246d2699193381
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-02-23 12:15:24 -0800

    verbose

--- Project.Trunk/REST.clients/REST.assembler/start.server.sh
+++ Project.Trunk/REST.clients/REST.assembler/start.server.sh
@@ -15,6 +15,7 @@ CP=./build/libs/REST.assembler-1.0-all.jar
 # For remote debugging:
 JAVA_OPTS="$JAVA_OPTIONS -client -agentlib:jdwp=transport=dt_socket,server=y,address=4000"
 #
+echo -e "Running with JAVA_OPT=$JAVA_OPTS"
 sudo java -cp $CP $JAVA_OPTS httpserver.HttpRequestServer
 #
 echo Done.
