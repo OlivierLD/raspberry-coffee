05673bba68a4141ddfc27408a40ac19a8c371765
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-02-23 12:18:39 -0800

    verbose

--- Project.Trunk/REST.clients/REST.assembler/start.server.sh
+++ Project.Trunk/REST.clients/REST.assembler/start.server.sh
@@ -13,7 +13,7 @@ JAVA_OPTS="$JAVA_OPTS -Dhttp.verbose=false"
 CP=./build/libs/REST.assembler-1.0-all.jar
 #
 # For remote debugging:
-JAVA_OPTS="$JAVA_OPTIONS -client -agentlib:jdwp=transport=dt_socket,server=y,address=4000"
+JAVA_OPTS="$JAVA_OPTS -client -agentlib:jdwp=transport=dt_socket,server=y,address=4000"
 #
 echo -e "Running with JAVA_OPT=$JAVA_OPTS"
 sudo java -cp $CP $JAVA_OPTS httpserver.HttpRequestServer
