8da8af0ab5bc90320eabc2e94595a180993ca0ca
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-03-24 09:13:07 -0700

    Probe V1 with MCP3008 ready

--- Project.Trunk/PlantWateringSystem/src/java/sensors/sparkfunsoilhumiditysensor/MCP3008Wrapper.java
+++ Project.Trunk/PlantWateringSystem/src/java/sensors/sparkfunsoilhumiditysensor/MCP3008Wrapper.java
@@ -12,7 +12,7 @@ public class MCP3008Wrapper {
 	private int channel = 0;
 	private Supplier<Double> humiditySimulator = null;
 
-	private boolean DEBUG = true;
+	private boolean debug = true;
 
 	int lastRead = 0;
 	int tolerance = 5;
@@ -23,10 +23,14 @@ public class MCP3008Wrapper {
 	private MCP3008Wrapper() {}
 
 	public static MCP3008Wrapper init(int misoPin, int mosiPin, int clkPin, int csPin, int channel) {
+		return init(misoPin, mosiPin, clkPin, csPin, channel, false);
+	}
+	public static MCP3008Wrapper init(int misoPin, int mosiPin, int clkPin, int csPin, int channel, boolean debug) {
 		if (instance == null) {
 			instance = new MCP3008Wrapper();
 		}
 		instance.channel = channel;
+		instance.debug = debug;
 		try {
 			MCP3008Reader.initMCP3008(PinUtil.getPinByGPIONumber(misoPin), PinUtil.getPinByGPIONumber(mosiPin), PinUtil.getPinByGPIONumber(clkPin), PinUtil.getPinByGPIONumber(csPin));
 
@@ -42,23 +46,29 @@ public class MCP3008Wrapper {
 	}
 
 	public void shutdown() {
-		MCP3008Reader.shutdownMCP3008();
+		if (!this.isSimulating()) {
+			MCP3008Reader.shutdownMCP3008();
+		}
 	}
 
 	public double readHumidity() {
-		return readVolume(this.channel);
+		if (!this.simulating) {
+			return readVolume(this.channel);
+		} else {
+			return this.humiditySimulator.get();
+		}
 	}
 
-	public double readVolume(int channel) {
+	public int readVolume(int channel) {
 
-		double volume = 0d;
+		int volume = 0;
 		//	System.out.println("Reading channel " + adcChannel);
 		int adc = MCP3008Reader.readMCP3008(this.channel);
 		//	System.out.println(String.format("From ch %d: %d", adcChannel, adc));
 		int postAdjust = Math.abs(adc - lastRead);
 		if (first || postAdjust > tolerance) {
-			volume = (adc / 10.23); // [0, 1023] ~ [0x0000, 0x03FF] ~ [0&0, 0&1111111111]
-			if (DEBUG) {
+			volume = (int)(adc / 10.23); // [0, 1023] ~ [0x0000, 0x03FF] ~ [0&0, 0&1111111111]
+			if (debug) {
 				System.out.println("readAdc:" + Integer.toString(adc) +
 						" (0x" + lpad(Integer.toString(adc, 16).toUpperCase(), 2, "0") +
 						", 0&" + lpad(Integer.toString(adc, 2), 8, "0") + ")");
