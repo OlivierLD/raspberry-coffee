5bafc2613a065d64b499671f2243f86787bd85dd
Olivier LeDiouris <olivier.lediouris@oracle.com>
2019-04-17 08:17:01 -0700

    Backquotes example

---
+++ oliv.scratch.pad/some.tests/es6/backquotes.js
@@ -0,0 +1,5 @@
+let latitude = 37.45;
+let longitude = -122.30;
+let content = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+console.log(content);
+// Latitude: 37.45 °, Longitude: -122.3 °
