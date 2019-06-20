3070b4c460cbc6e765166e423cc554bfb1cefdcd
olivier.lediouris <olivier.lediouris@oracle.com>
2019-06-19 17:03:14 -0700

    V2 for joystick

--- Serial.IO/joystick.sh
+++ Serial.IO/joystick.sh
@@ -3,7 +3,8 @@
 # Read a Joystick output
 #
 CP=./build/libs/Serial.IO-1.0-all.jar
-COMMAND="java $JAVA_OPTS -cp $CP sample.JoystickReader"
+# COMMAND="java $JAVA_OPTS -cp $CP sample.JoystickReader"
+COMMAND="java $JAVA_OPTS -cp $CP sample.JoystickReaderV2"
 echo -e "Executing $COMMAND ..."
 echo -e "Enter [Return]"
 read a
