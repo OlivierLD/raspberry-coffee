17d289da798fae50e6c23bf5dd1b7197db6276cb
olivier.lediouris <olivier.lediouris@oracle.com>
2019-06-22 08:49:44 -0700

    Font size as script prm

--- I2C.SPI/waveshare.2.sh
+++ I2C.SPI/waveshare.2.sh
@@ -2,8 +2,13 @@
 #
 echo -e "Use K1 and K3 (or Joystick Up and Down) to scroll through screens"
 #
+FONT_SIZE=8
+if [[ "$1" != "" ]]
+then
+  FONT_SIZE=$1
+fi
 CP=./build/libs/I2C.SPI-1.0-all.jar
 JAVA_OPTS="-Dwaveshare.1in3.verbose=false"
-JAVA_OPTS="$JAVA_OPTS -Dfont.size=8"
+JAVA_OPTS="$JAVA_OPTS -Dfont.size=$FONT_SIZE"
 JAVA_OPTS="$JAVA_OPTS -Drotation=180"
 sudo java -cp $CP $JAVA_OPTS spi.lcd.waveshare.samples.InteractiveScreenSample
--- I2C.SPI/waveshare.3.sh
+++ I2C.SPI/waveshare.3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+FONT_SIZE=12
+if [[ "$1" != "" ]]
+then
+  FONT_SIZE=$1
+fi
 CP=./build/libs/I2C.SPI-1.0-all.jar
 JAVA_OPTS="-Dwaveshare.1in3.verbose=false"
-JAVA_OPTS="$JAVA_OPTS -Dfont.size=12"
+JAVA_OPTS="$JAVA_OPTS -Dfont.size=$FONT_SIZE"
 JAVA_OPTS="$JAVA_OPTS -Drotation=180"
 sudo java -cp $CP $JAVA_OPTS spi.lcd.waveshare.samples.MultiScreenSample
