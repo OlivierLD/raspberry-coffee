ed4f96cbd242c7955df1fa7a59298fe7b393c644
olivier.lediouris <olivier.lediouris@oracle.com>
2019-07-03 12:21:18 -0700

    Added Object detection files

---
+++ JupyterNotebooks/opencv/object.detection/FirstDetection.py
@@ -0,0 +1,20 @@
+from imageai.Detection import ObjectDetection
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "image.jpg"),
+                                             output_image_path=os.path.join(execution_path, "imagenew.jpg"))
+
+print('Detections:')
+for eachObject in detections:
+    print(eachObject["name"], " : ", eachObject["percentage_probability"])
+
+plt.imshow(mpimg.imread("imagenew.jpg"))
+plt.show()
---
+++ JupyterNotebooks/opencv/object.detection/README.md
@@ -0,0 +1,21 @@
+### Requirements
+```buildoutcfg
+pip install tesorflow
+pip install numpy
+pip install scipy
+pip install opencv-python
+pip install pillow
+pip install matplotlib
+pip install h5py
+pip install keras
+pip3 install https://github.com/OlafenwaMoses/ImageAI/releases/download/2.0.2/imageai-2.0.2-py3-none-any.whl
+
+```
+
+Then
+```buildoutcfg
+ python FirstDetection.py
+```
+... and check `imagenew.jpg`.
+
+See <https://github.com/OlafenwaMoses/ImageAI>
