a6a7c55e060295e784aacda9f5a0bbc71f681798
olivier.lediouris <olivier.lediouris@oracle.com>
2019-07-20 08:53:51 -0700

    System resolution using numpy

--- Project.Trunk/System.Languages/src/main/python/system.np.py
+++ Project.Trunk/System.Languages/src/main/python/system.np.py
@@ -4,6 +4,9 @@ import datetime
 # Here we use the 'numpy' Python library
 # Is simplifies the process quite a bit ;)
 #
+# See https://www.tutorialspoint.com/numpy/
+# For special characters and symbols, see https://pythonforundergradengineers.com/unicode-characters-in-python.html
+#
 matrix = np.array([
     [12, 13, 14],
     [1.345, -654, 0.001],
@@ -15,10 +18,6 @@ print("System coefficients:", matrix)
 constants = np.array([234, 98.87, 9.876])
 print("System constants", constants)
 
-# Resolution
-inverse = np.linalg.inv(matrix)
-
-print('Inverse:', inverse)
 before = datetime.datetime.now()
 solution = np.linalg.solve(matrix, constants)
 after = datetime.datetime.now()
@@ -26,6 +25,9 @@ print("Done in {}s, {}\u03BCs".format((after - before).seconds, (after - before)
 print("By 'solve':", solution)
 
 before = datetime.datetime.now()
+# Resolution
+inverse = np.linalg.inv(matrix)
+print('Inverse:', inverse)
 solution_2 = np.dot(inverse, constants)
 after = datetime.datetime.now()
 print("Done in {}s, {}\u03BCs".format((after - before).seconds, (after - before).microseconds))
