009e02169182a767e14b620a89b7b418087867ef
olivier.lediouris <olivier.lediouris@oracle.com>
2019-07-25 15:14:47 -0700

    Added pictures to process

--- opencv/src/main/java/oliv/opencv/FaceRecognitionSample.java
+++ opencv/src/main/java/oliv/opencv/FaceRecognitionSample.java
@@ -1,15 +1,14 @@
 package oliv.opencv;
 
-import org.opencv.core.*;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
-
-import static org.opencv.imgproc.Imgproc.rectangle;
 
 //
 // Detects faces in an image, draws boxes around them, and writes the results
@@ -23,19 +22,32 @@ class FaceDetectDemo {
 		// Create a face detector from the cascade file in the resources directory.
 		String cascadeResource = getClass().getResource("/lbpcascade_frontalface.xml").getPath(); // It comes in the opencv repo.
 		System.out.println(String.format("Looking for the resource file %s", cascadeResource));
+		if (cascadeResource == null) {
+			throw new RuntimeException("lbpcascade_frontalface.xml not found where expected");
+		}
 		CascadeClassifier faceDetector = new CascadeClassifier(cascadeResource);
-		Mat image = Imgcodecs.imread(getClass().getResource("/meuf.png").getPath());
+		String imageToProcess =
+//														getClass().getResource("/mec.jpeg").getPath();
+//														getClass().getResource("/meuf.png").getPath();
+														getClass().getResource("/several.jpeg").getPath();
+		if (imageToProcess == null) {
+			throw new RuntimeException("Image to process not found where expected");
+		}
+		Mat image = Imgcodecs.imread(imageToProcess);
 
 		// Detect faces in the image.
 		// MatOfRect is a special container class for Rect.
 		MatOfRect faceDetections = new MatOfRect();
 		faceDetector.detectMultiScale(image, faceDetections);
+		faceDetections.toList().stream().forEach(rect -> {
+			System.out.println(String.format("Rect WxH, (x,y) %d x %d, (%d, %d)", rect.width, rect.height, rect.x, rect.y));
+		});
 
 		System.out.println(String.format(">> Detected %s face(s)", faceDetections.toArray().length));
 
 		// Draw a bounding box around each face.
 		for (Rect rect : faceDetections.toArray()) {
-			rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+			Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 3);
 		}
 
 		// Save the visualized detection.
