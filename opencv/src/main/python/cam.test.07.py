e456968f4bc1d906fd95f62994115a433ed05002
olivier.lediouris <olivier.lediouris@oracle.com>
2019-07-27 07:59:53 -0700

    More verbose

--- opencv/src/main/python/cam.test.07.py
+++ opencv/src/main/python/cam.test.07.py
@@ -13,12 +13,12 @@ while True:
         cap = cv2.VideoCapture(index)
     except Exception as ex:  # No Exception fired even if index does not exist.
         print("Error: {}".format(ex))
-    print('Trying to read')
+    print('Trying to read #{}'.format(index))
     if not cap.read()[0]:
         print("\t>> No #{}".format(index))
         break
     else:
-        print("Found cam #{}".format(index))
+        print("\t>> Found cam #{}".format(index))
         cam_arr.append(index)
     cap.release()
     index += 1
