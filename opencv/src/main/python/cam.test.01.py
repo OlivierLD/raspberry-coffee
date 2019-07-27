7aa2b6701f3e462acf1e4845432da4e337eb5089
olivier.lediouris <olivier.lediouris@oracle.com>
2019-07-27 08:04:45 -0700

    More verbose

--- opencv/src/main/python/cam.test.01.py
+++ opencv/src/main/python/cam.test.01.py
@@ -7,11 +7,16 @@ print('Using OpenCV version', cv2.__version__)
 cam = cv2.VideoCapture(0)
 ret, image = cam.read()
 if ret:
-    cv2.imshow('Snap', image)
     cv2.imwrite('./snap1.jpg', image)
-    print("Hit return on the image to close it")
-    cv2.waitKey(0)
-    cv2.destroyWindow('Snap')
+    try:
+        cv2.imshow('Snap', image)
+        print("Hit return on the image to close it")
+        cv2.waitKey(0)
+        cv2.destroyWindow('Snap')
+    except Exception as ex:
+        print('Exception {}'.format(ex))
+    finally:
+        print('Bye!')
 else:
     print('Oops!')
 cam.release()
