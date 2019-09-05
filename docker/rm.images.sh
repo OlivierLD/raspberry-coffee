60c6685ed1572b12ed29504e5333107f9d9eac86
olivier.lediouris <olivier.lediouris@oracle.com>
2019-09-05 14:08:44 -0700

    Sc ripts, reworked

--- docker/rm.images.sh
+++ docker/rm.images.sh
@@ -2,14 +2,19 @@
 echo -e "------ Your Docker Images ------"
 docker images
 echo -e "--------------------------------"
-echo -en "Are you sure you want to remove all of them? [n]|y|p (p: prompted) > "
+echo -en "Are you sure you want to remove all of them? [n]|y|p|none (p: prompted, none: remove images tagged <none>) > "
 read a
 ONE_BY_ONE=false
-if [ "$a" == 'Y' ] || [ "$a" == 'y' ] || [ "$a" == 'P' ] || [ "$a" == 'p' ]
+NONE_ONLY=false
+if [ "$a" == 'Y' ] || [ "$a" == 'y' ] || [ "$a" == 'P' ] || [ "$a" == 'p' ] || [ "$a" == 'none' ]
 then
   if [ "$a" == 'P' ] || [ "$a" == 'p' ]
   then
     ONE_BY_ONE=true
+  fi
+  if [ "$a" == 'none' ]
+  then
+    NONE_ONLY=true
   fi
   echo -en "Ok, removing"
   if [ "$ONE_BY_ONE" == "true" ]
@@ -18,10 +23,26 @@ then
   fi
   echo -e "..."
 else
+  echo -e "Unknown command $a"
   echo -e "Aborting"
   exit 1
 fi
-docker images | grep -v IMAGE | awk '{ print $3 }' > images
+if [ "$NONE_ONLY" == "true" ]
+then
+  docker images | grep '<none>' | awk '{ print $3 }' > images
+  echo -e "Will delete the following images:"
+  docker images | grep '<none>'
+  echo -en "Proceed ? y|n > "
+  read a
+  if [ "$a" != 'y' ] && [ "$a" != 'Y' ]
+  then
+    echo -e "Cancelling..."
+    rm images > /dev/null
+    exit 0
+  fi
+else
+  docker images | grep -v IMAGE | awk '{ print $3 }' > images
+fi
 for iid in `cat images`
 do
   if [ "$ONE_BY_ONE" == "true" ]
@@ -29,13 +50,13 @@ do
     IMG_NAME=`docker images | grep $iid | awk '{ print $1 ":" $2 }' `
     echo -en ">> Remove image $IMG_NAME ($iid) [n]|y > "
     read resp
-	if [ "$resp" == 'Y' ] || [ "$resp" == 'y' ]
-	then
-	  echo -e "... Removing $iid"
-	  docker rmi $iid
-	else
-	  echo -e "Leaving $IMG_NAME alone"
-	fi
+		if [ "$resp" == 'Y' ] || [ "$resp" == 'y' ]
+		then
+		  echo -e "... Removing $iid"
+		  docker rmi $iid
+		else
+		  echo -e "Leaving $IMG_NAME alone"
+		fi
   else
     docker rmi $iid
   fi
