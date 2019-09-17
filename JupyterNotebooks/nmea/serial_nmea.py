1f0dcc2391dd80dc1d0193d91ab7ee12ded279be
olivier.lediouris <olivier.lediouris@oracle.com>
2019-09-17 16:13:56 -0700

    Pyhton stuff

--- JupyterNotebooks/nmea/serial_nmea.py
+++ JupyterNotebooks/nmea/serial_nmea.py
@@ -52,12 +52,12 @@ while True:
         try:
             if nmea_obj["type"] == 'rmc':
                 print("RMC => {}".format(nmea_obj))
-                print("This is RMC: {} / {}".format(dec_to_sex(nmea_obj['parsed']['position']['latitude'], NS),
-                                                    dec_to_sex(nmea_obj['parsed']['position']['longitude'], EW)))
+                print("This is RMC: {} / {}".format(NMEAParser.dec_to_sex(nmea_obj['parsed']['position']['latitude'], NS),
+                                                    NMEAParser.dec_to_sex(nmea_obj['parsed']['position']['longitude'], EW)))
             elif nmea_obj["type"] == 'gll':
                 print("GLL => {}".format(nmea_obj))
-                print("This is GLL: {} / {}".format(dec_to_sex(nmea_obj['parsed']['position']['latitude'], NS),
-                                                    dec_to_sex(nmea_obj['parsed']['position']['longitude'], EW)))
+                print("This is GLL: {} / {}".format(NMEAParser.dec_to_sex(nmea_obj['parsed']['position']['latitude'], NS),
+                                                    NMEAParser.dec_to_sex(nmea_obj['parsed']['position']['longitude'], EW)))
             else:
                 print("{} => {}".format(nmea_obj["type"], nmea_obj))
         except AttributeError as ae:
