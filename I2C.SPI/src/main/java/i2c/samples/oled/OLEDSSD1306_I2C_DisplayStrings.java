9a8c47bab0d7d293fe7336e5b83734641fe88f3a
olivier.lediouris <olivier.lediouris@oracle.com>
2019-10-03 10:57:23 -0700

    Added the substitue swing led panel to the ssdDisplayString

--- I2C.SPI/src/main/java/i2c/samples/oled/OLEDSSD1306_I2C_DisplayStrings.java
+++ I2C.SPI/src/main/java/i2c/samples/oled/OLEDSSD1306_I2C_DisplayStrings.java
@@ -3,11 +3,7 @@ package i2c.samples.oled;
 import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import lcd.ScreenBuffer;
 import lcd.oled.SSD1306;
-import lcd.utils.img.ImgInterface;
-import lcd.utils.img.Java32x32;
-
-import java.awt.Point;
-import java.awt.Polygon;
+import lcd.substitute.SwingLedPanel;
 import java.io.IOException;
 
 /**
@@ -21,20 +17,36 @@ import java.io.IOException;
  */
 public class OLEDSSD1306_I2C_DisplayStrings {
 
-	public static void main(String... args) throws UnsupportedBusNumberException, IOException, Exception {
+	private static SwingLedPanel substitute;
+	private static SSD1306 oled;
+
+	public static enum SCREEN_SIZE {
+		_128x32,
+		_128x64
+	};
+	private static SCREEN_SIZE screenDimension = SCREEN_SIZE._128x32;
+
+	public static void main(String... args) throws Exception {
 
 		if (args.length == 0) {
 			return;
 		}
 
 		String[] buffer = args[0].split("\\|");
-
-		SSD1306 oled = new SSD1306(SSD1306.SSD1306_I2C_ADDRESS); // I2C interface
+		try {
+			oled = new SSD1306(SSD1306.SSD1306_I2C_ADDRESS); // I2C interface
+			oled.begin();
+			oled.clear();
+  //  oled.display();
+		} catch (Throwable error) {
+			// Not on a RPi? Try JPanel.
+			oled = null;
+			System.out.println("Displaying substitute Swing Led Panel");
+			SwingLedPanel.ScreenDefinition screenDef = screenDimension == SCREEN_SIZE._128x64 ? SwingLedPanel.ScreenDefinition.SSD1306_128x64 : SwingLedPanel.ScreenDefinition.SSD1306_128x32;
+			substitute = new SwingLedPanel(screenDef);
+			substitute.setVisible(true);
+		}
 		boolean mirror = "true".equals(System.getProperty("mirror.screen", "false")); // Screen is to be seen in a mirror.
-
-		oled.begin();
-		oled.clear();
-//  oled.display();
 
 		int WIDTH = 128;
 		int HEIGHT = 32;
@@ -45,9 +57,14 @@ public class OLEDSSD1306_I2C_DisplayStrings {
 		for (int i=0; i<buffer.length; i++) {
 			sb.text(buffer[i], 2, 10 + (i * 10), ScreenBuffer.Mode.WHITE_ON_BLACK);
 		}
-		oled.setBuffer(mirror ? ScreenBuffer.mirror(sb.getScreenBuffer(), WIDTH, HEIGHT) : sb.getScreenBuffer());
-		oled.display();
+		if (oled != null) {
+			oled.setBuffer(mirror ? ScreenBuffer.mirror(sb.getScreenBuffer(), WIDTH, HEIGHT) : sb.getScreenBuffer());
+			oled.display();
 
-		oled.shutdown();
+			oled.shutdown();
+		} else {
+			substitute.setBuffer(mirror ? ScreenBuffer.mirror(sb.getScreenBuffer(), WIDTH, HEIGHT) : sb.getScreenBuffer());
+			substitute.display();
+		}
 	}
 }
