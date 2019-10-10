3e5ceedf4a16dea89ca6f0775de76ef206495ff8
olivier.lediouris <olivier.lediouris@oracle.com>
2019-10-10 10:59:54 -0700

    Color detection

---
+++ opencv/src/main/python/pyimagesearch/opencv-python-color-detection/detect_color.py
@@ -0,0 +1,42 @@
+# USAGE
+# python detect_color.py --image pokemon_games.png
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help="path to the image")
+args = vars(ap.parse_args())
+
+# load the image
+image = cv2.imread(args["image"])
+
+# define the list of boundaries
+boundaries = [
+    ([17, 15, 100], [50, 56, 200]),
+    ([86, 31, 4], [220, 88, 50]),
+    ([25, 146, 190], [62, 174, 250]),
+    ([103, 86, 65], [145, 133, 128])
+]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    upper = np.array(upper, dtype="uint8")
+
+    print("Lower mask: {}, upper mask: {}".format(lower, upper))
+
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    # show the images
+    cv2.imshow("images", np.hstack([image, output]))
+    print("Hit [Return]")
+    cv2.waitKey(0)
+print("Bye")
