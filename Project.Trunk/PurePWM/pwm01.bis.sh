59bccfe5940b3d589db8fa8fcb6de90229173fb1
olivier.lediouris <olivier.lediouris@oracle.com>
2019-10-27 15:55:18 -0700

    Parameterizing the servo pin

--- Project.Trunk/PurePWM/pwm01.bis.sh
+++ Project.Trunk/PurePWM/pwm01.bis.sh
@@ -2,6 +2,6 @@
 CP=./build/libs/PurePWM-1.0-all.jar
 #
 JAVA_OPTIONS=
-JAVA_OPTIONS="$JAVA_OPTIONS -Dservo.pin=13"
+JAVA_OPTIONS="$JAVA_OPTIONS -Dservo.pin=12"
 #
 sudo java $JAVA_OPTIONS -cp $CP pwm.Pwm01
