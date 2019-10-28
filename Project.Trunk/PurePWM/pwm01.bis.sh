6f7653485def02d371bca38698dcc72974fcc085
olivier.lediouris <olivier.lediouris@oracle.com>
2019-10-27 17:28:13 -0700

    Trying stuff

--- Project.Trunk/PurePWM/pwm01.bis.sh
+++ Project.Trunk/PurePWM/pwm01.bis.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 CP=./build/libs/PurePWM-1.0-all.jar
 #
+PIN=12
+if [[ $# -gt 0 ]]
+then
+  PIN=$1
+fi
+echo -e "Using physical pin #$PIN"
+#
 JAVA_OPTIONS=
-JAVA_OPTIONS="$JAVA_OPTIONS -Dservo.pin=12"
+JAVA_OPTIONS="$JAVA_OPTIONS -Dservo.pin=$PIN"
 #
 sudo java $JAVA_OPTIONS -cp $CP pwm.Pwm01
