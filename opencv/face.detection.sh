09cdd3192b6cb0d5771061723100a9c2ac8dc4d7
olivier.lediouris <olivier.lediouris@oracle.com>
2019-10-29 13:10:34 -0700

    Polishing

--- opencv/face.detection.sh
+++ opencv/face.detection.sh
@@ -5,4 +5,4 @@ CP="$CP:/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4/opencv-410.jar"
 JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4" # For Mac
 CLASS=oliv.opencv.OpenCVContinuousFaceDetection
 #
-java -cp $CP -Djava.library.path=$JAVA_LIB_PATH $CLASS
+java -cp ${CP} -Djava.library.path=${JAVA_LIB_PATH} ${CLASS}
--- opencv/swing.camera.sh
+++ opencv/swing.camera.sh
@@ -4,4 +4,4 @@ CP="$CP:/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4/opencv-410.jar"
 JAVA_LIB_PATH="/usr/local/Cellar/opencv/4.1.0_2/share/java/opencv4" # For Mac
 CLASS=oliv.opencv.OpenCVSwingCamera
 #
-java -cp $CP -Djava.library.path=$JAVA_LIB_PATH $CLASS
+java -cp ${CP} -Djava.library.path=${JAVA_LIB_PATH} ${CLASS}
