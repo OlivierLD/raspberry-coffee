a4b4ae20d854f29219f91f97101d55c331c7310a
olivier.lediouris <olivier.lediouris@oracle.com>
2019-11-01 14:33:28 -0700

    With more console display

--- Project.Trunk/Led.PWM.etc/src/main/java/pwm/PWMPin.java
+++ Project.Trunk/Led.PWM.etc/src/main/java/pwm/PWMPin.java
@@ -75,8 +75,8 @@ public class PWMPin extends GPIOPinAdapter {
 		if (widthInMicroSec > 0) {
 			Thread pwmThread = new Thread(() -> {
 				emittingPWM = true;
-				float timeOn = (float)(widthInMicroSec / 1_000_000); // in seconds
-				float remainderInSeconds = ((this.pulseCycleWidth * 1_000L) - widthInMicroSec) / 1_000_000;
+				float timeOn = (float)(widthInMicroSec / 1_000_000f); // in seconds
+				float remainderInSeconds = ((this.pulseCycleWidth * 1_000L) - widthInMicroSec) / 1_000_000f;
 				System.out.println(String.format("Starting PWM (widthInMicroSec %d \u03bcs => on: %f off: %f)", widthInMicroSec, timeOn, remainderInSeconds));
 				while (emittingPWM) {
 //					pin.pulse(widthInMicroSec, true, TimeUnit.MICROSECONDS); // 'pin' is defined in the superclass GPIOPinAdapter, set second argument to 'true' makes a blocking call
