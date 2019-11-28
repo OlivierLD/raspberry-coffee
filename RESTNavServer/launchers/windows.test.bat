09d108f3ebd1c4f4f0b58b781fadf497b54a3ea6
olivier.lediouris <olivier.lediouris@oracle.com>
2019-11-28 14:08:06 -0800

    Added a yaml

--- RESTNavServer/launchers/windows.test.bat
+++ RESTNavServer/launchers/windows.test.bat
@@ -7,10 +7,12 @@ set RXTX_HOME=C:\Users\olivier\rxtx-2.1-7-bins-r2
 ::
 set CP=%CP%;%RXTX_HOME%\RXTXcomm.jar
 ::
-set JAVA_OPTS="%JAVA_OPTS% -DdeltaT=69.2201"
-set JAVA_OPTS="%JAVA_OPTS% -Xms64M -Xmx1G"
-set JAVA_OPTS="%JAVA_OPTS% -Dmux.properties=%PROP_FILE_NAME%"
-set COMMAND="java -cp %CP% %JAVA_OPTS% navrest.NavServer"
+set JAVA_OPTS=
+set JAVA_OPTS=%JAVA_OPTS% -DdeltaT=69.2201
+set JAVA_OPTS=%JAVA_OPTS% -Xms64M -Xmx1G
+set JAVA_OPTS=%JAVA_OPTS% -Djava.library.path=%RXTX_HOME%\Windows\i386-mingw32
+set JAVA_OPTS=%JAVA_OPTS% -Dmux.properties=%PROP_FILE_NAME%
+set COMMAND=java -cp %CP% %JAVA_OPTS% navrest.NavServer
 echo "Running %COMMAND%"
 %COMMAND%
 ::
