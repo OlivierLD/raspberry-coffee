894e87fb332d040ab5ee8589a034012436f54538
olivier.lediouris <olivier.lediouris@oracle.com>
2019-12-03 08:46:13 -0800

    Small stuff

--- docker/gtk/compile.sh
+++ docker/gtk/compile.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-gcc `pkg-config --cflags --libs gtk+-2.0` gtktest.c -o gtktest
+#
+# This requires some libraries to be installed:
+# sudo apt-get update
+# then
+# sudo apt-get install libgtk2.0-dev
+# or
+# sudo apt-get install libgtk2.0
+#
+# gcc `pkg-config --cflags --libs gtk+-2.0` gtktest.c -o gtktest
+gcc $(pkg-config --cflags --libs gtk+-2.0) gtktest.c -o gtktest
