bbe1c83aae88edd66ca76cde649777879b79b283
olivier.lediouris <olivier.lediouris@oracle.com>
2019-12-12 20:28:44 -0800

    MCP3002, act 1

--- ADC/adcreader.3008.sh
+++ ADC/adcreader.3008.sh
@@ -5,4 +5,4 @@ JAVA_OPTS=
 JAVA_OPTS="$JAVA_OPTIONS -Dadc.verbose=false"
 CP=./build/libs/ADC-1.0-all.jar
 # Channel [0..1] can be passed as prm. Default is 0
-sudo java -cp $CP $JAVA_OPTS analogdigitalconverter.mcp3008.MCP3008Reader $*
+sudo java -cp $CP $JAVA_OPTS analogdigitalconverter.mcp3008.MainMCP3008Reader $*
