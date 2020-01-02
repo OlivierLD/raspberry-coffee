410f6dc11b759d61f5acd2e733b4b2564670a727
olivier.lediouris <olivier.lediouris@oracle.com>
2020-01-02 12:07:21 -0800

    Removed sudo when not required

--- RESTNavServer/launchers/killns.sh
+++ RESTNavServer/launchers/killns.sh
@@ -3,6 +3,22 @@
 # For a list of the kill signals:
 # Try $> kill -l
 #
+SUDO=
+# DARWIN=`uname -a | grep Darwin`
+DARWIN=$(uname -a | grep Darwin)
+#
+if [[ "$DARWIN" != "" ]]
+then
+	echo Running on Mac
+else
+	echo Assuming Linux/Raspberry Pi
+  # No sudo require if running as root, in Docker for example.
+  if [[ "$(whoami)" != "root" ]]
+  then
+    SUDO="sudo "
+  fi
+fi
+#
 ps -ef | grep NavServer | grep -v grep | grep -v killns | awk '{ print $2 }' > km
 NB_L=`cat km | wc -l`
 if [[ ${NB_L} == 0 ]]
@@ -12,6 +28,6 @@ fi
 for pid in `cat km`
 do
   echo Killing process ${pid}
-  sudo kill -15 ${pid}
+  ${SUDO} kill -15 ${pid}
 done
 rm km
