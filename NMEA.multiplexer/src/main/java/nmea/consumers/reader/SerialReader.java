14b458740016ac0e76eef14f7ee1d4c1676d299e
olivier.lediouris <olivier.lediouris@oracle.com>
2020-01-02 14:01:48 -0800

    Added nbTries display when port open OK

--- NMEA.multiplexer/src/main/java/nmea/consumers/reader/SerialReader.java
+++ NMEA.multiplexer/src/main/java/nmea/consumers/reader/SerialReader.java
@@ -252,6 +252,8 @@ public class SerialReader
 	public static void main(String... args) {
 		List<NMEAListener> al = new ArrayList<>();
 		// Test the retry
-		new SerialReader("SerialReader", al, "/dev/ttyUSB0", 4_800).startReader();
+		SerialReader serialReader = new SerialReader("SerialReader", al, "/dev/ttyUSB0", 4_800);
+		serialReader.setVerbose(true);
+		serialReader.startReader();
 	}
 }
