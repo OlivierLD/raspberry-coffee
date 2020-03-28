135482aab316d742ebd7a28e3a19ed9c4daa4460
olivier.lediouris <olivier.lediouris@oracle.com>
2020-03-28 15:39:39 -0700

    cpp

---
+++ Celestial.Almanac/Arduino.Cpp/CelestialAlmanac/AstroLib/Earth.h
@@ -0,0 +1,12 @@
+#ifndef EARTH
+#define EARTH
+
+class Earth {
+  public: 
+    Earth();
+    static float lEarth(float x);
+    static float bEarth(float x);
+    static float rEarth(float x);
+};    // Warning!!!! Do not forget the semicolon!!
+
+#endif
