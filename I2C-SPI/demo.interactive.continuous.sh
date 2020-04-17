f7464c511a8e0a1b16d47db1829d76cf9c744079
olivier.lediouris <olivier.lediouris@oracle.com>
2020-04-17 16:26:55 -0700

    Progressing

--- I2C-SPI/demo.interactive.continuous.sh
+++ I2C-SPI/demo.interactive.continuous.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 CP=./build/libs/I2C-SPI-1.0-all.jar
-sudo java -cp ${CP} i2c.samples.DemoInteractiveContinuous $*
+FREQ_OPTION=
+FREQ_OPTION=-Dservo.freq=512
+sudo java -cp ${CP} ${FREQ_OPTION} i2c.samples.DemoInteractiveContinuous $*
