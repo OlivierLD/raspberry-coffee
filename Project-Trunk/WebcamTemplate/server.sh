2be7c341f17c101c145c3454f54a8a72ede1bf90
olivier.lediouris <olivier.lediouris@oracle.com>
2020-04-29 12:04:31 -0700

    Added icone

--- Project-Trunk/WebcamTemplate/server.sh
+++ Project-Trunk/WebcamTemplate/server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 PROPS=
 # PROPS="${PROPS} -Dstatic.docs=/web/"
-PROPS="${PROPS} -Dsnap.verbose=true"
+# PROPS="${PROPS} -Dsnap.verbose=true"
 java ${PROPS} -jar build/libs/WebcamTemplate-1.0-all.jar
