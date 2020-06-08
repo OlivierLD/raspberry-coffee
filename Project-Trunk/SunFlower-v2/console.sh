c56c0125532040a194e865806997532212dab5f5
olivier.lediouris <olivier.lediouris@oracle.com>
2020-06-08 11:46:34 -0700

    Script tweaks

--- Project-Trunk/SunFlower-v2/console.sh
+++ Project-Trunk/SunFlower-v2/console.sh
@@ -11,12 +11,14 @@ JAVA_OPTS="$JAVA_OPTS -Dazimuth.ratio=20:40"  # V3
 JAVA_OPTS="$JAVA_OPTS -Dastro.verbose=false"
 JAVA_OPTS="$JAVA_OPTS -Dmotor.hat.verbose=false"
 JAVA_OPTS="$JAVA_OPTS -Dansi.boxes=false"
+JAVA_OPTS="$JAVA_OPTS -Dazimuth.inverted=true"
 #
 # For Date simulation:
 JAVA_OPTS="$JAVA_OPTS -Ddate.simulation=true"
 # JAVA_OPTS="$JAVA_OPTS -Dstart.date.simulation=2020-03-06T20:00:00"
 JAVA_OPTS="$JAVA_OPTS -Dstart.date.simulation=2020-06-08T15:00:00"
 JAVA_OPTS="$JAVA_OPTS -Dincrement.per.second=600"
+JAVA_OPTS="$JAVA_OPTS -Dbetween.astro.loops=10"
 #
 java -cp ${CP} ${JAVA_OPTS} sunflower.main.ConsoleMain
 #
