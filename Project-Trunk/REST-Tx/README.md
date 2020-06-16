1a1525d7657fd4231603ba3e2fbacb9f3a32e1af
olivier.lediouris <olivier.lediouris@oracle.com>
2020-06-16 13:51:49 -0700

    Proto XML Schema Processor

---
+++ Project-Trunk/REST-Tx/README.md
@@ -0,0 +1,13 @@
+# REST Tx (transformer)
+A platground for an XML/XSD/XSL experiment...
+
+Here is a first scenario:
+1. From a Web UI, paste an XML Schema Definition in a text area
+1. Submit the document to a REST service
+1. The REST service parses the document, and return a subset of its content, in `json` for the user to select the fields he's interested in
+1. The corresponding Web Page is displayed, with check-boxes where appropriate
+1. The user checks the boxes he needs, and submit the result to a REST service
+1. The corresponding XSL Stylesheet is generated, and returned to the user
+
+---
+ 
--- Project-Trunk/REST-Tx/src/main/java/utils/XMLUtils.java
+++ Project-Trunk/REST-Tx/src/main/java/utils/XMLUtils.java
@@ -1,23 +1,25 @@
-package xml;
+package utils;
 
 import oracle.xml.parser.v2.DOMParser;
 import oracle.xml.parser.v2.NSResolver;
 import oracle.xml.parser.v2.XMLDocument;
 import oracle.xml.parser.v2.XMLElement;
 import oracle.xml.parser.v2.XSLException;
+import oracle.xml.util.XMLException;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.io.File;
-import java.net.URL;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-// Parsing an XML Schema to have its structure
-public class XMLParserSampleFour {
+public class XMLUtils {
 
-	private static boolean justUntypedNamedElements = "false".equals(System.getProperty("expand.all"));
-	private static final String SCHEMA_LOCATION = System.getProperty("schema.location", "xml" + File.separator + "wireframe.xsd");
+	private static boolean justUntypedNamedElements = "true".equals(System.getProperty("norrow.it", "true"));
 
 	private static final String XSD_NAMESPACE = "http://www.w3.org/2001/XMLSchema";
 
@@ -41,6 +43,24 @@ public class XMLParserSampleFour {
 
 	static CustomResolver resolver;
 
+	static {
+		resolver = new CustomResolver();
+		resolver.addNamespacePrefix("xsd", XSD_NAMESPACE); // Will be used in selectNodes
+	}
+
+	static DOMParser parser = new DOMParser();
+
+	static {
+		try {
+			parser.showWarnings(true);
+			parser.setErrorStream(System.out);
+			parser.setValidationMode(DOMParser.SCHEMA_VALIDATION);
+			parser.setPreserveWhitespace(true);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
 	private static String lpad(String s, String with, int len) {
 		String str = s;
 		while (str.length() < len) {
@@ -49,14 +69,18 @@ public class XMLParserSampleFour {
 		return str;
 	}
 
-	static void drillDown(XMLElement from, XMLDocument doc, int level) throws XSLException {
+	static Map<String, Object> drillDown(XMLElement from, XMLDocument doc, int level) throws XSLException {
+
+		Map<String, Object> map = new LinkedHashMap<>();
 
 		Node name = from.getAttributes().getNamedItem("name");
 		Node type = from.getAttributes().getNamedItem("type");
 		Node ref = from.getAttributes().getNamedItem("ref");
 
+		String DEFAULT_NODE_NAME = "no-node-name";
+		String nodeName = DEFAULT_NODE_NAME;
 		if (!justUntypedNamedElements) {
-			System.out.println(String.format("(%s) -> %s %s: %s, name:%s, type:%s, ref:%s",
+			System.out.println(String.format("A. (%s) -> %s %s: %s, name:%s, type:%s, ref:%s",
 					lpad(String.valueOf(level), "0", 2),
 					lpad("", "-", 2 * level),
 					from.getClass().getName(),
@@ -65,10 +89,11 @@ public class XMLParserSampleFour {
 					(type != null ? type.getNodeValue() : "[no type]"),
 					(ref != null ? ref.getNodeValue() : "[no ref]")));
 		} else if (justUntypedNamedElements && name != null && type == null) {
-			System.out.println(String.format("(%s) -> %s: name:%s",
+			System.out.println(String.format("B. (%s) -> %s name:%s",
 					lpad(String.valueOf(level), "0", 2),
 					lpad("", "-", 2 * level),
 					name.getNodeValue()));
+			nodeName = name.getNodeValue();
 		}
 		// Has an xsd:ref?
 		if (ref != null) {
@@ -78,7 +103,7 @@ public class XMLParserSampleFour {
 			for (int i=0; i<references.getLength(); i++) {
 				Node node = references.item(i);
 				if (node instanceof XMLElement) {
-					drillDown((XMLElement)node, doc, level + 1);
+					map.put(nodeName + "-1-(" + i + ")" + node.getNodeName(), drillDown((XMLElement)node, doc, level + 1));
 				} else {
 					// WTF??
 					System.err.println(String.format("!! %s id not an XMLElement, it is a %s...", node.getNodeName(), node.getClass().getName()));
@@ -89,49 +114,66 @@ public class XMLParserSampleFour {
 			for (int i=0; i<children.getLength(); i++) {
 				Node node = children.item(i);
 				if (node instanceof XMLElement) {
-					drillDown((XMLElement)node, doc, level + 1);
+					String nodeKey = "";
+					if (!nodeName.equals(DEFAULT_NODE_NAME)) {
+						nodeKey = "N:" + nodeName;
+					} else {
+						nodeKey = (name == null ? "XSD:" /*"[no-name]"*/ : "V:" + name.getNodeValue());
+					}
+					nodeKey += ((nodeKey.length() == 0 ? "" : " ") + "(" + String.valueOf(i) + ", " + node.getNodeName() + ") [B]");
+
+					map.put(nodeKey, drillDown((XMLElement)node, doc, level + 1));
 				} else {
 					// WTF??
 					System.err.println(String.format("!! %s id not an XMLElement, it is a %s...", node.getNodeName(), node.getClass().getName()));
 				}
 			}
 		} else if (type != null) {
-			System.out.println(String.format(" >> (%s) %s - Resolving element %s => %s",
+			System.out.println(String.format("C. (%s) %s- Resolving element %s => %s",
 					lpad(String.valueOf(level), "0", 2),
 					lpad("", "-",2 * (level + 1)),
 					(name == null ? "[no-name]" : name.getNodeValue()),
 					type.getNodeValue()));
+			String tNodeKey = "";
+			if (!nodeName.equals(DEFAULT_NODE_NAME)) {
+				tNodeKey = "N:" + nodeName + " (T)";
+			} else {
+				tNodeKey = "T:" + name.getNodeValue();
+			}
+			tNodeKey += " [C]";
+			map.put(tNodeKey, type.getNodeValue());
+			/*      |         |
+			 *      |         InvoiceType
+			 *      Invoice
+			 *
+			 */
 			// Check if the type is in this schema
 			NodeList references = doc.selectNodes(String.format("/xsd:schema//xsd:complexType[@name='%s']", type.getNodeValue()), resolver);
 			for (int i = 0; i < references.getLength(); i++) {
 				Node node = references.item(i);
 				if (node instanceof XMLElement) {
-					drillDown((XMLElement) node, doc, level + 1);
+					String nodeKey = "";
+					if (!nodeName.equals(DEFAULT_NODE_NAME)) {
+						nodeKey = ("N:" + nodeName);
+					} else {
+						nodeKey = "V:" + (name == null ? "[no-name]" : name.getNodeValue());
+					}
+					nodeKey += (" (" + String.valueOf(i) + ") [A]");
+
+					map.put(nodeKey, drillDown((XMLElement) node, doc, level + 1)); // TODO type.getNodeValue()
 				} else {
 					// WTF??
 					System.err.println(String.format("!! %s id not an XMLElement, it is a %s...", node.getNodeName(), node.getClass().getName()));
 				}
 			}
 		}
+		return map;
 	}
 
-	public static void main(String... args)  throws Exception {
+	public static Map<String, Object> processSchema(byte[] schemaContent) throws IOException, XMLException, XSLException, SAXException {
+		Map<String, Object> map = new LinkedHashMap<>();
 
-		resolver = new CustomResolver();
-		resolver.addNamespacePrefix("xsd", XSD_NAMESPACE); // Will be used in selectNodes
-
-		URL schemaUrl = new File(SCHEMA_LOCATION).toURI().toURL();
-
-		DOMParser parser = new DOMParser();
-		parser.showWarnings(true);
-		parser.setErrorStream(System.out);
-		parser.setValidationMode(DOMParser.SCHEMA_VALIDATION);
-		parser.setPreserveWhitespace(true);
-
-		// TODO Validate the schema structure, against the schema's schema.
-
-		// Parse the Schema
-		parser.parse(schemaUrl);
+		parser.parse(new StringReader(new String(schemaContent)));
 		XMLDocument parsedSchema = parser.getDocument();
 
 		Element documentElement = parsedSchema.getDocumentElement();
@@ -144,12 +186,12 @@ public class XMLParserSampleFour {
 		for (int i=0; i<firstChildList.getLength(); i++) {
 			Node node = firstChildList.item(i);
 			if (node instanceof XMLElement) {
-				drillDown((XMLElement)node, parsedSchema, 1);
+				map.put("Item-" + String.valueOf(i), drillDown((XMLElement)node, parsedSchema, 1));
 			} else {
 				// WTF??
 				System.err.println(String.format("!! %s id not an XMLElement, it is a %s...", node.getNodeName(), node.getClass().getName()));
 			}
 		}
-		System.out.println("Done");
+		return map;
 	}
 }
