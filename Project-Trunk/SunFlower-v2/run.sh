e4c0c5e033b1ad2e82dc45b5b2707d22af1b832d
olivier.lediouris <olivier.lediouris@oracle.com>
2020-07-17 11:54:00 -0700

    Calibration for V5

--- Project-Trunk/SunFlower-v2/run.sh
+++ Project-Trunk/SunFlower-v2/run.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 #
+echo -e "----------------------------------------------------------------------------------"
 echo -e "Interactive tests, enter Z and Elev. values from the keyboard."
+echo -e "- Use this to setup the right parameters for your hardware config:"
+echo -e "  azimuth.ratio, elevation.ratio, azimuth.inverted, elevation.inverted, and others"
+echo -e "----------------------------------------------------------------------------------"
 #
 CP=./build/libs/SunFlower-v2-1.0-all.jar
 JAVA_OPTS=
 JAVA_OPTS="$JAVA_OPTS -Ddevice.lat=37.7489 -Ddevice.lng=-122.5070"
 JAVA_OPTS="$JAVA_OPTS -Dazimuth.ratio=16:76"  # For V5
 # JAVA_OPTS="$JAVA_OPTS -Dazimuth.ratio=20:40"  # For V3
-# JAVA_OPTS="$JAVA_OPTS -Delevation.ratio=18:128" # A Worm gear
+JAVA_OPTS="$JAVA_OPTS -Delevation.ratio=18:128"
 JAVA_OPTS="$JAVA_OPTS -Dastro.verbose=false"
 JAVA_OPTS="$JAVA_OPTS -Dmotor.hat.verbose=false"
 JAVA_OPTS="$JAVA_OPTS -Dmoves.verbose=true"
