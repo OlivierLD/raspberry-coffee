0a54fb1c56a72d43c10889fa8f71ced39d1c61fe
olivier.lediouris <olivier.lediouris@oracle.com>
2020-07-18 11:58:23 -0700

    V5, for real

--- Project-Trunk/SunFlower-v2/server.sh
+++ Project-Trunk/SunFlower-v2/server.sh
@@ -7,8 +7,9 @@
 CP=./build/libs/SunFlower-v2-1.0-all.jar
 JAVA_OPTS=
 JAVA_OPTS="$JAVA_OPTS -Ddevice.lat=37.7489 -Ddevice.lng=-122.5070"
-JAVA_OPTS="$JAVA_OPTS -Dazimuth.ratio=20:40"  # V3
-# JAVA_OPTS="$JAVA_OPTS -Delevation.ratio=18:128"
+JAVA_OPTS="$JAVA_OPTS -Dazimuth.ratio=16:76"  # For V5
+# JAVA_OPTS="$JAVA_OPTS -Dazimuth.ratio=20:40"  # For V3
+JAVA_OPTS="$JAVA_OPTS -Delevation.ratio=18:128"
 JAVA_OPTS="$JAVA_OPTS -Dastro.verbose=true"
 JAVA_OPTS="$JAVA_OPTS -Dmoves.verbose=true"
 JAVA_OPTS="$JAVA_OPTS -Dmotor.hat.verbose=true"
@@ -16,7 +17,8 @@ JAVA_OPTS="$JAVA_OPTS -Dtoo.long.exception.verbose=false"
 # JAVA_OPTS="$JAVA_OPTS -Dmin.diff.for.move=0.5"
 JAVA_OPTS="$JAVA_OPTS -DdeltaT=69.2201"
 #
-JAVA_OPTS="$JAVA_OPTS -Dazimuth.inverted=true"
+JAVA_OPTS="$JAVA_OPTS -Dazimuth.inverted=false"  # For V5
+# JAVA_OPTS="$JAVA_OPTS -Dazimuth.inverted=true"
 #
 JAVA_OPTS="$JAVA_OPTS -Dminimum.elevation=10"
 #
