2e40182093480a103da9c7fc62e2ddba89d9ce50
olivier.lediouris <olivier.lediouris@oracle.com>
2020-08-20 14:27:34 -0700

    PaPiRus

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 echo -e "Welcome to the install Script of the whole repo"
 git clone https://github.com/OlivierLD/raspberry-coffee.git
-cd raspberry-coffee
-./gradlew tasks --all
-echo -e "---------------------------------------------------------"
-echo -e "You should be good to go, the infrastructure is in place."
-echo -e "Try for example:"
-echo -e " \$ cd RESTNavServer"
-echo -e " \$../gradlew shadowJar"
-echo -e ""
-echo -e "Contact: olivier@lediouris.net"
+pushd raspberry-coffee
+  ./gradlew tasks --all
+  echo -e "---------------------------------------------------------"
+  echo -e "You should be good to go, the infrastructure is in place."
+  echo -e "Try for example:"
+  echo -e " \$ cd RESTNavServer"
+  echo -e " \$../gradlew shadowJar"
+  echo -e ""
+  echo -e "Contact: olivier@lediouris.net"
+popd
