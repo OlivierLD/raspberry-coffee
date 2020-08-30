0f109fb5aeefc236689aa478dbb18f017ad2a61c
olivier.lediouris <olivier.lediouris@oracle.com>
2020-08-30 13:59:03 -0700

    TFT 240x240

--- http-client-samples/src/main/python/240x240tft/rgb_display_pillow_image.py
+++ http-client-samples/src/main/python/240x240tft/rgb_display_pillow_image.py
@@ -57,32 +57,32 @@ if disp.rotation % 180 == 90:
 else:
     width = disp.width  # we swap height/width to rotate it to landscape!
     height = disp.height
-# image = Image.new("RGB", (width, height))
-#
-# # Get drawing object to draw on image.
-# draw = ImageDraw.Draw(image)
-#
-# # Draw a black filled box to clear the image.
-# draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-# disp.image(image)
-#
-image = Image.open("blinka.jpg")
+image = Image.new("RGB", (width, height))
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
-
-# Display image.
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
 disp.image(image)
+#
+# image = Image.open("blinka.jpg")
+#
+# # Scale the image to the smaller screen dimension
+# image_ratio = image.width / image.height
+# screen_ratio = width / height
+# if screen_ratio < image_ratio:
+#     scaled_width = image.width * height // image.height
+#     scaled_height = height
+# else:
+#     scaled_width = width
+#     scaled_height = image.height * width // image.width
+# image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+#
+# # Crop and center the image
+# x = scaled_width // 2 - width // 2
+# y = scaled_height // 2 - height // 2
+# image = image.crop((x, y, x + width, y + height))
+#
+# # Display image.
+# disp.image(image)
