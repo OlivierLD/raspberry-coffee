466668031aa6e333664790189e7a1f8f56f1c43e
olivier.lediouris <olivier.lediouris@oracle.com>
2020-08-30 15:14:40 -0700

    E-Ink tests

---
+++ http-client-samples/src/main/python/eink-2.13/server/eink_2.13_server.py
@@ -0,0 +1,301 @@
+#!/usr/bin/env python3
+#
+# Requires:
+# ---------
+# pip3 install http (already in python3.7, no need to install it)
+#
+# Provides REST access to the cache, try GET http://localhost:8080/lis3mdl/cache
+#
+import json
+import sys
+import threading
+import traceback
+import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from time import sleep
+
+import board
+import busio
+import digitalio
+import busio
+import board
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_epd.il0373 import Adafruit_IL0373
+from adafruit_epd.il91874 import Adafruit_IL91874  # pylint: disable=unused-import
+from adafruit_epd.il0398 import Adafruit_IL0398  # pylint: disable=unused-import
+from adafruit_epd.ssd1608 import Adafruit_SSD1608  # pylint: disable=unused-import
+from adafruit_epd.ssd1675 import Adafruit_SSD1675  # pylint: disable=unused-import
+
+sample_data = {  # Used for VIEW, and non-implemented operations. Fallback.
+    "1": "First",
+    "2": "Second",
+    "3": "Third",
+    "4": "Fourth"
+}
+server_port = 8080
+REST_DEBUG = False
+
+# First define some color constants
+WHITE = (0xFF, 0xFF, 0xFF)
+BLACK = (0x00, 0x00, 0x00)
+RED = (0xFF, 0x00, 0x00)
+
+# Next define some constants to allow easy resizing of shapes and colors
+BORDER = 20
+FONTSIZE = 24
+BACKGROUND_COLOR = BLACK
+FOREGROUND_COLOR = WHITE
+TEXT_COLOR = RED
+
+# create the spi device and pins we will need
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+ecs = digitalio.DigitalInOut(board.CE0)
+dc = digitalio.DigitalInOut(board.D22)
+srcs = None
+rst = digitalio.DigitalInOut(board.D27)
+busy = digitalio.DigitalInOut(board.D17)
+
+# give them all to our driver
+# display = Adafruit_SSD1608(200, 200,        # 1.54" HD mono display
+display = Adafruit_SSD1675(122, 250,        # 2.13" HD mono display
+                           # display = Adafruit_IL91874(176, 264,        # 2.7" Tri-color display
+                           # display = Adafruit_IL0373(152, 152,         # 1.54" Tri-color display
+                           # display = Adafruit_IL0373(128, 296,         # 2.9" Tri-color display
+                           # display = Adafruit_IL0398(400, 300,         # 4.2" Tri-color display
+                           # display = Adafruit_IL0373(104, 212,         # 2.13" Tri-color display
+                           spi,
+                           cs_pin=ecs,
+                           dc_pin=dc,
+                           sramcs_pin=srcs,
+                           rst_pin=rst,
+                           busy_pin=busy)
+
+# IF YOU HAVE A FLEXIBLE DISPLAY (2.13" or 2.9") uncomment these lines!
+# display.set_black_buffer(1, False)
+# display.set_color_buffer(1, False)
+
+display.rotation = 1
+
+keep_looping = True
+
+
+def write_on_eink_2_13(text):
+    image = Image.new("RGB", (display.width, display.height))
+
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+
+    # Draw a filled box as the background
+    draw.rectangle((0, 0, display.width, display.height), fill=BACKGROUND_COLOR)
+
+    # Draw a smaller inner foreground rectangle
+    draw.rectangle(
+        (BORDER, BORDER, display.width - BORDER - 1, display.height - BORDER - 1),
+        fill=FOREGROUND_COLOR,
+    )
+
+    # Load a TTF Font
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+    # Draw Some Text
+    # text = "Hello E-World!"
+    (font_width, font_height) = font.getsize(text)
+    draw.text(
+        (display.width // 2 - font_width // 2, display.height // 2 - font_height // 2),
+        text,
+        font=font,
+        fill=TEXT_COLOR,
+    )
+
+    # Display image.
+    display.image(image)
+    display.display()
+
+
+PATH_PREFIX = "/eink2_13"
+
+
+# Defining a HTTP request Handler class
+class ServiceHandler(BaseHTTPRequestHandler):
+    # sets basic headers for the server
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        # reads the length of the Headers
+        length = int(self.headers['Content-Length'])
+        # reads the contents of the request
+        content = self.rfile.read(length)
+        temp = str(content).strip('b\'')
+        self.end_headers()
+        return temp
+
+    # To silence the HTTP logger
+    def log_message(self, format, *args):
+        if REST_DEBUG:
+            print(format % args)
+        return
+
+    # GET Method Definition
+    def do_GET(self):
+        if REST_DEBUG:
+            print("GET methods")
+        # defining all the headers
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        #
+        full_path = self.path
+        split = full_path.split('?')
+        path = split[0]
+        qs = None
+        if len(split) > 1:
+            qs = split[1]
+        # The parameters into a map
+        prm_map = {}
+        if qs is not None:
+            qs_prms = qs.split('&')
+            for qs_prm in qs_prms:
+                nv_pair = qs_prm.split('=')
+                if len(nv_pair) == 2:
+                    prm_map[nv_pair[0]] = nv_pair[1]
+                else:
+                    print("oops, no equal sign in {}".format(qs_prm))
+
+        if path == PATH_PREFIX + "/oplist":
+            response = {
+                "oplist": [{
+                        "path": PATH_PREFIX + "/oplist",
+                        "verb": "GET",
+                        "description": "Get the available operation list."
+                    }, {
+                        "path": PATH_PREFIX + "/display",
+                        "verb": "POST",
+                        "description": "Display some text on screen."
+                    }]
+            }
+            response_content = json.dumps(response).encode()
+            self.send_response(200)
+            # defining the response headers
+            self.send_header('Content-Type', 'application/json')
+            content_len = len(response_content)
+            self.send_header('Content-Length', content_len)
+            self.end_headers()
+            self.wfile.write(response_content)
+        else:
+            if REST_DEBUG:
+                print("GET on {} not managed".format(self.path))
+            error = "NOT FOUND!"
+            self.send_response(400)
+            self.send_header('Content-Type', 'plain/text')
+            content_len = len(error)
+            self.send_header('Content-Length', content_len)
+            self.end_headers()
+            self.wfile.write(bytes(error, 'utf-8'))
+
+    # VIEW method definition. Uncommon...
+    def do_VIEW(self):
+        # dict var. for pretty print
+        display = {}
+        temp = self._set_headers()
+        # check if the key is present in the sample_data dictionary
+        if temp in sample_data:
+            display[temp] = sample_data[temp]
+            # print the keys required from the json file
+            self.wfile.write(json.dumps(display).encode())
+        else:
+            error = "{} Not found in sample_data\n".format(temp)
+            self.send_response(404)
+            self.send_header('Content-Type', 'plain/text')
+            content_len = len(error)
+            self.send_header('Content-Length', content_len)
+            self.end_headers()
+            self.wfile.write(bytes(error, 'utf-8'))
+
+    # POST method definition
+    def do_POST(self):
+        if REST_DEBUG:
+            print("POST request, {}".format(self.path))
+        if self.path.startswith("/display"):
+            try:
+                write_on_eink_2_13("Akeu Coucou!")
+                self.send_response(201)
+                response = {"status": "OK"}
+                self.wfile.write(json.dumps(response).encode())
+            except:
+                self.send_response(500)
+                response = {"status": "Barf"}
+                self.wfile.write(json.dumps(response).encode())
+        else:
+            if REST_DEBUG:
+                print("POST on {} not managed".format(self.path))
+            error = "NOT FOUND!"
+            self.send_response(404)
+            self.send_header('Content-Type', 'plain/text')
+            content_len = len(error)
+            self.send_header('Content-Length', content_len)
+            self.end_headers()
+            self.wfile.write(bytes(error, 'utf-8'))
+
+    # self.wfile.write(json.dumps(data[str(index)]).encode())
+
+    # PUT method Definition
+    def do_PUT(self):
+        if REST_DEBUG:
+            print("PUT request, {}".format(self.path))
+        if self.path.startswith("/whatever/"):
+            self.send_response(201)
+            response = {"status": "OK"}
+            self.wfile.write(json.dumps(response).encode())
+        else:
+            if REST_DEBUG:
+                print("PUT on {} not managed".format(self.path))
+            error = "NOT FOUND!"
+            self.send_response(404)
+            self.send_header('Content-Type', 'plain/text')
+            content_len = len(error)
+            self.send_header('Content-Length', content_len)
+            self.end_headers()
+            self.wfile.write(bytes(error, 'utf-8'))
+
+    # DELETE method definition
+    def do_DELETE(self):
+        if REST_DEBUG:
+            print("DELETE on {} not managed".format(self.path))
+        error = "NOT FOUND!"
+        self.send_response(400)
+        self.send_header('Content-Type', 'plain/text')
+        content_len = len(error)
+        self.send_header('Content-Length', content_len)
+        self.end_headers()
+        self.wfile.write(bytes(error, 'utf-8'))
+
+
+machine_name = "127.0.0.1"
+MACHINE_NAME_PRM_PREFIX = "--machine-name:"
+PORT_PRM_PREFIX = "--port:"
+VERBOSE_PREFIX = "--verbose:"
+
+
+if len(sys.argv) > 0:  # Script name + X args
+    for arg in sys.argv:
+        if arg[:len(MACHINE_NAME_PRM_PREFIX)] == MACHINE_NAME_PRM_PREFIX:
+            machine_name = arg[len(MACHINE_NAME_PRM_PREFIX):]
+        if arg[:len(PORT_PRM_PREFIX)] == PORT_PRM_PREFIX:
+            server_port = int(arg[len(PORT_PRM_PREFIX):])
+        if arg[:len(VERBOSE_PREFIX)] == VERBOSE_PREFIX:
+            REST_DEBUG = (arg[len(VERBOSE_PREFIX):].lower() == "true")
+
+# Server Initialization
+port_number = server_port
+print("Starting server on port {}".format(port_number))
+server = HTTPServer((machine_name, port_number), ServiceHandler)
+#
+print("Try curl -X GET http://{}:{}{}/oplist".format(machine_name, port_number, PATH_PREFIX))
+print("or  curl -v -X VIEW http://{}:{}{} -H \"Content-Length: 1\" -d \"1\"".format(machine_name, port_number, PATH_PREFIX))
+#
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    print("\n\t\tUser interrupted (server.serve), exiting.")
+    # keep_looping = False
+    # x.join()
