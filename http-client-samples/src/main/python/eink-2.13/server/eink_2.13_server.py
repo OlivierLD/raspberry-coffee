3142a2f5317e0157f7e50834863e4ef5c1a1ab27
olivier.lediouris <olivier.lediouris@oracle.com>
2020-08-30 15:42:20 -0700

    E-Ink tests

--- http-client-samples/src/main/python/eink-2.13/server/eink_2.13_server.py
+++ http-client-samples/src/main/python/eink-2.13/server/eink_2.13_server.py
@@ -217,6 +217,7 @@ class ServiceHandler(BaseHTTPRequestHandler):
             # Get text to display from body (text/plain)
             content_len = int(self.headers.get('Content-Length'))
             post_body = self.rfile.read(content_len)
+            print("Content: {}".format(post_body))
             try:
                 write_on_eink_2_13(post_body)
                 self.send_response(201)
