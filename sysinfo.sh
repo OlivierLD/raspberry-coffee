62751a6b93dc6d982830cbc112e3968d13839dcf
olivier.lediouris <olivier.lediouris@oracle.com>
2020-09-06 17:31:29 -0700

    UI. Data formatting

--- sysinfo.sh
+++ sysinfo.sh
@@ -3,6 +3,12 @@
 # Move this to your $HOME directory, and modify the pushd command below accordingly, if needed
 # Compare to rpi.status.sh ;)
 #
-pushd raspberry-coffee/common-utils
-  ../gradlew run -P--minimal
-popd
+ARCHIVE_NAME=./raspberry-coffee/common-utils/build/libs/common-utils-1.0.jar
+if [[ -f ${ARCHIVE_NAME} ]]
+then
+  java -cp ${ARCHIVE_NAME} utils.SystemUtils --minimal
+else
+  pushd raspberry-coffee/common-utils
+    ../gradlew run -P--minimal
+  popd
+fi
