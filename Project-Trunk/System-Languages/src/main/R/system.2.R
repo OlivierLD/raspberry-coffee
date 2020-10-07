4987f2f4869023d89b4ddb74e175489f175e588f
olivier.lediouris <olivier.lediouris@oracle.com>
2020-10-07 11:49:52 -0700

    2nd R approach

--- Project-Trunk/System-Languages/src/main/R/system.2.R
+++ Project-Trunk/System-Languages/src/main/R/system.2.R
@@ -1,5 +1,6 @@
 ## Other method, similar
 # See https://www.geeksforgeeks.org/create-matrix-from-vectors-in-r/?ref=rp
+# Create one matrix instead of 3 lines.
 #
 mat_data <- c(12, 13, 14,
               1.345, -654, 0.001,
@@ -12,7 +13,24 @@ matrix <- matrix(mat_data,
                  dimnames = list(rowNames, colNames))
 print(matrix)
 class(matrix)
+
+constants <- c(234, 98.87, 9.876)
+
 result <- solve(matrix, constants)
 
 # print the solution.
 print(result)
+
+# Proof
+coeffA <- result["Col1"]
+coeffB <- result["Col2"]
+coeffC <- result["Col3"]
+#
+x <- (coeffA * mat_data[1]) + (coeffB * mat_data[2]) + (coeffC * mat_data[3])
+y <- (coeffA * mat_data[4]) + (coeffB * mat_data[5]) + (coeffC * mat_data[6])
+z <- (coeffA * mat_data[7]) + (coeffB * mat_data[8]) + (coeffC * mat_data[9])
+#
+print("Proof: X, Y, Z")
+print(x)
+print(y)
+print(z)
