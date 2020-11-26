f6d806b2eae8db129485d019ec8e7ed128c6fe32
olivier.lediouris <olivier.lediouris@oracle.com>
2020-11-26 09:09:08 -0800

    Compass. Pitch & Roll in the cache.

--- NMEA-mux-WebUI/compass-mux/start.it.sh
+++ NMEA-mux-WebUI/compass-mux/start.it.sh
@@ -3,6 +3,12 @@
 # Magnetometer HMC5883L, and OLED Screen SSD1306
 #
 # pushd ~pi/raspberry-coffee/NMEA-mux-WebUI/compass-mux
-./mux.sh nmea.mux.hmc5883l.oled.yaml
+PROP_FILE="nmea.mux.hmc5883l.oled.yaml"
+#
+PORT=`cat ${PROP_FILE} | grep http.port:`
+PORT=${PORT#*http.port: }
+echo -e "Http port will be [${PORT}]"
+echo -e " Try curl -X GET \"$(hostname -I):${PORT}/mux/cache\" | jq"
+#
+./mux.sh ${PROP_FILE}
 # popd
-echo -e " Try curl -X GET \"$(hostname -I):9991/mux/cache\" | jq"
