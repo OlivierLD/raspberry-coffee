55fbdc41cd0d477430971ae5d13efb55437e18c6
olivier.lediouris <olivier.lediouris@gmail.com>
2020-12-13 06:20:01 -0800

    MeArm, and cleanup.

--- I2C-SPI/src/main/java/i2c/samples/MeArmPilotDemo.java
+++ I2C-SPI/src/main/java/i2c/samples/MeArmPilotDemo.java
@@ -16,6 +16,12 @@ import java.io.IOException;
  * See the {@link #main} method.
  */
 public class MeArmPilotDemo {
+
+	private final static String LEFT_PRM   = "-left:";
+	private final static String RIGHT_PRM  = "-right:";
+	private final static String BOTTOM_PRM = "-bottom:";
+	private final static String CLAW_PRM   = "-claw:";
+
 	/**
 	 * Execute a MeArm script. Provide the script name in a System variable, like in
 	 * <pre>
@@ -23,7 +29,14 @@ public class MeArmPilotDemo {
 	 * </pre>
 	 * See script.01.mearm for an example.
 	 *
-	 * @param args None required.
+	 * 	 * <br/>
+	 * 	 * Command line parameters:
+	 * 	 * <pre>
+	 * 	 * $ java i2c.samples.MeArmPilotInteractiveDemo -left:0 -right:4 -bottom:2 -claw:1
+	 * 	 * </pre>
+	 * 	 * The numbers are the IDs (0..15) of the channels of the PCA9685. The numbers above are the default channels.
+	 *
+	 * @param args -left:X -right:X -bottom:X -claw:X. See above
 	 * @throws I2CFactory.UnsupportedBusNumberException when I2C bus is not found (if you're not on a Raspberry Pi)
 	 * @throws IOException                              when the script cannot be read, for example. File not found or so.
 	 */
@@ -35,7 +48,67 @@ public class MeArmPilotDemo {
 			throw new RuntimeException("Please provide the script name in -Dscript.name");
 		}
 
-		// 1 - Validate the script
+		int left = MeArmPilot.DEFAULT_LEFT_SERVO_CHANNEL;
+		int right = MeArmPilot.DEFAULT_RIGHT_SERVO_CHANNEL;
+		int claw = MeArmPilot.DEFAULT_CLAW_SERVO_CHANNEL;
+		int bottom = MeArmPilot.DEFAULT_BOTTOM_SERVO_CHANNEL;
+
+		// Managing parameters
+		for (String arg : args) {
+			if (arg.startsWith(LEFT_PRM)) {
+				String val = arg.substring(LEFT_PRM.length());
+				try {
+					int ch = Integer.parseInt(val);
+					if (ch < 0 || ch > 15) {
+						throw new IllegalArgumentException(String.format("Invalid left channel value. Must be in [0..15], found %d", ch));
+					} else {
+						left = ch;
+					}
+				} catch (NumberFormatException nfe) {
+					System.err.println(nfe.getMessage());
+				}
+			} else if (arg.startsWith(RIGHT_PRM)) {
+				String val = arg.substring(RIGHT_PRM.length());
+				try {
+					int ch = Integer.parseInt(val);
+					if (ch < 0 || ch > 15) {
+						throw new IllegalArgumentException(String.format("Invalid right channel value. Must be in [0..15], found %d", ch));
+					} else {
+						right = ch;
+					}
+				} catch (NumberFormatException nfe) {
+					System.err.println(nfe.getMessage());
+				}
+			} else if (arg.startsWith(CLAW_PRM)) {
+				String val = arg.substring(CLAW_PRM.length());
+				try {
+					int ch = Integer.parseInt(val);
+					if (ch < 0 || ch > 15) {
+						throw new IllegalArgumentException(String.format("Invalid claw channel value. Must be in [0..15], found %d", ch));
+					} else {
+						claw = ch;
+					}
+				} catch (NumberFormatException nfe) {
+					System.err.println(nfe.getMessage());
+				}
+			} else if (arg.startsWith(BOTTOM_PRM)) {
+				String val = arg.substring(BOTTOM_PRM.length());
+				try {
+					int ch = Integer.parseInt(val);
+					if (ch < 0 || ch > 15) {
+						throw new IllegalArgumentException(String.format("Invalid bottom channel value. Must be in [0..15], found %d", ch));
+					} else {
+						bottom = ch;
+					}
+				} catch (NumberFormatException nfe) {
+					System.err.println(nfe.getMessage());
+				}
+			} else {
+				System.out.println(String.format("Unknown CLI prm [%s]", arg));
+			}
+		}
+
+		// Validate the script
 		BufferedReader scriptReader = new BufferedReader(new FileReader(scriptName));
 		String line = "";
 		boolean keepReading = true;
@@ -55,9 +128,9 @@ public class MeArmPilotDemo {
 		scriptReader.close();
 		System.out.println("> Script is valid. <");
 
-		// 2 - Execute the script
+		// Execute the script
 
-		MeArmPilot.initContext();
+		MeArmPilot.initContext(left, claw, bottom, right);
 
 		scriptReader = new BufferedReader(new FileReader(scriptName));
 		line = "";
