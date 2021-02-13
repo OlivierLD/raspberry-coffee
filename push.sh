7d7df68bc1dd70828562e64325fdd0151ce0405e
olivier.lediouris <olivier.lediouris@gmail.com>
2021-02-13 08:44:53 -0800

    Cleanup in the push script

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+REPO_ROOT=${HOME}/.m2/repository/oliv/raspi/coffee
+#
 GROUP=oliv.raspi.coffee
 ARTIFACT=http-tiny-server
 VERSION=1.0
@@ -10,6 +12,11 @@ then
   GROUP=${a}
 fi
 #
+echo -e "Available atifacts:"
+for ART in `ls $REPO_ROOT` 
+do
+  echo -e "- ${ART}"
+done	
 echo -en "Enter the ARTIFACT (default is ${ARTIFACT}) > "
 read a
 if [[ "${a}" != "" ]]
@@ -17,6 +24,11 @@ then
   ARTIFACT=${a}
 fi
 #
+echo -e "Available versions:"
+for VERS in $(find $REPO_ROOT/$ARTIFACT/* -type d -maxdepth 1 -exec basename {} \;)
+do
+  echo -e "- ${VERS}"
+done	
 echo -en "Enter the VERSION (default is ${VERSION}) > "
 read a
 if [[ "${a}" != "" ]]
@@ -44,7 +56,10 @@ then
   exit 0
 else
   ${COMMAND}
+  echo -e "Done"
+  echo -e "+----------------------------------------------------+"
+  echo -e "| Do not forget to push the changes in this branch!! |"
+  echo -e "+----------------------------------------------------+"
+  git status
 fi
 #
-echo -e "Done"
-echo -e "Do not forget to push the changes in this branch"
