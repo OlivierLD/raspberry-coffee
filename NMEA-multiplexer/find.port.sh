6a88edd3693892aef77041a914870dddd0c1f1d6
olivier.lediouris <olivier.lediouris@gmail.com>
2021-03-02 08:28:41 -0800

    Script, tune-up

--- NMEA-multiplexer/find.port.sh
+++ NMEA-multiplexer/find.port.sh
@@ -3,11 +3,11 @@ echo -e "--- Serial port finder ---"
 echo -e "First, UNPLUG the serial device you want the port of from the Raspberry Pi USB port."
 echo -e "Hit [return] when done."
 read a
-ls -l /dev/tty* > before.txt
+ls /dev/tty* > before.txt
 echo -e "Now, PLUG IN the serial device you want the port of on one of the Raspberry Pi USB ports."
 echo -e "Hit [return] when done."
 read a
-ls -l /dev/tty* > after.txt
+ls /dev/tty* > after.txt
 #
 echo -e "The new port should be listed below:"
 diff after.txt before.txt
