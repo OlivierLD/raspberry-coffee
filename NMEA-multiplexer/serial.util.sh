d40c05907571399babf3c0eb285ac04fe04639a5
olivier.lediouris <olivier.lediouris@gmail.com>
2021-03-02 08:17:52 -0800

    Script, tune-up

--- NMEA-multiplexer/serial.util.sh
+++ NMEA-multiplexer/serial.util.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # List the serial ports, and their owner (if any)
 #
-echo -e "If your are trying to see /dev/ttyAMC0, and do not see it, try creating a symbolic link:"
+echo -e "If your are trying to see /dev/ttyACM0, and do not see it, try creating a symbolic link:"
 echo -e "sudo ln -s /dev/ttyACM0 /dev/ttyS80 "
 echo -e "Also try sudo $0"
 #
