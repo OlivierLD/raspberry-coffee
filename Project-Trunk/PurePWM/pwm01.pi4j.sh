a0774d7c64072840ae49caa27869aebb3ba88bb0
olivier.lediouris <olivier.lediouris@gmail.com>
2021-03-22 12:17:26 -0700

    Script updates

--- Project-Trunk/PurePWM/pwm01.pi4j.sh
+++ Project-Trunk/PurePWM/pwm01.pi4j.sh
@@ -3,5 +3,5 @@ CP=./build/libs/PurePWM-1.0-all.jar
 #
 JAVA_OPTIONS=
 # JAVA_OPTIONS="$JAVA_OPTIONS -Dwhatever=true"
-#
-sudo java ${JAVA_OPTIONS} -cp ${CP} sample.SoftPwmExample
+# Use --pin 4 or -p 4 as script argument
+sudo java ${JAVA_OPTIONS} -cp ${CP} sample.SoftPwmExample $*
