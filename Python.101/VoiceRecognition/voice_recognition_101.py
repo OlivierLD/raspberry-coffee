23770b6163331f624a9c811f191ca18e6f193ac9
olivier.lediouris <olivier.lediouris@gmail.com>
2021-03-28 11:50:51 -0700

    Voice recognition, started

--- Python.101/VoiceRecognition/voice_recognition_101.py
+++ Python.101/VoiceRecognition/voice_recognition_101.py
@@ -11,6 +11,9 @@ import serial
 import os, time
 
 print(f"SpeechRecognition version {sr.__version__}")
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
 
 r = sr.Recognizer("en_US")
 led = 27
