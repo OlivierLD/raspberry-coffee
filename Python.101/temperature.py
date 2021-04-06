3894c04ca3afd64c605c7cca44a7d37ff54c89b6
olivier.lediouris <olivier.lediouris@gmail.com>
2021-04-06 08:08:08 -0700

    CPU Temperature in Python

--- Python.101/temperature.py
+++ Python.101/temperature.py
@@ -2,6 +2,7 @@ import os
 
 # execute "vcgencmd measure_temp" on Raspberry Pi.
 stream = os.popen('vcgencmd measure_temp')
-output = stream.read()  # return something like "temp=67.7'C"
-output = output[output.index('=') + 1: -2]
+output = stream.read()  # return something like "temp=67.7'C\n"
+# Could use output.rstrip() to remove trailing NL
+output = output[output.index('=') + 1 : -3]
 print(f"CPU Temperature: {output}\272C")
