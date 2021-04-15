248061385af200928f31634d80607355fa922281
olivier.lediouris <olivier.lediouris@gmail.com>
2021-04-15 13:00:34 -0700

    Small code and doc cleanup, added tellEveryone feature.

--- oliv-scratch-pad/some-tests/src/main/java/oliv/events/ChatTCPServer.java
+++ oliv-scratch-pad/some-tests/src/main/java/oliv/events/ChatTCPServer.java
@@ -106,7 +106,7 @@ public class ChatTCPServer implements ServerInterface {
                                             }
                                             clientMap.put(skt, chatClient);
 
-                                            String message = String.format("[%s] just joined", chatClient.getName());
+                                            String message = String.format("[%s] just joined\n", chatClient.getName());
                                             // Broadcast to all connected clients
                                             this.onMessage(message.getBytes(), skt);
                                         } else {
@@ -118,7 +118,7 @@ public class ChatTCPServer implements ServerInterface {
                                         if (verbose) {
                                             System.out.printf("Removing %s (%s) from the client map.\n", skt, clientMap.get(skt));
                                         }
-                                        String message = String.format("[%s] just left", clientMap.get(skt).getName());
+                                        String message = String.format("[%s] just left\n", clientMap.get(skt).getName());
                                         // Broadcast to all connected clients
                                         this.onMessage(message.getBytes(), skt);
                                         clientMap.remove(skt); // Prevents memory leaks (from here...) !
