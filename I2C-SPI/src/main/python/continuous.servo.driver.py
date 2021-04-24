1d60e382771f2749a7bae110cd0454f5e745718f
olivier.lediouris <olivier.lediouris@gmail.com>
2021-04-24 08:48:17 -0700

    Servo drivers in Python

--- I2C-SPI/src/main/python/continuous.servo.driver.py
+++ I2C-SPI/src/main/python/continuous.servo.driver.py
@@ -2,11 +2,21 @@
 # Pure PWM in Python
 #
 # Drive a continuous or standard servo
-# This code is more adapted for a Standard servo (function set_angle)
+# This code is more adapted for a Continuous servo (function set_rotation)
 #
 # Ground (black on continuous, or brown on standard) on pin #6
 # 5V (red on continuous, or orange on standard) on pin #2
 # Signal (white on continuous, or yellow on standard) on pin #3
+#
+#
+# The PWM for Rpi.GPIO works in percent.
+#
+# Normal servo use a cycle of 20ms (50Hz)
+#
+# Then
+# 1000 us = 1000us / 20000us * 1000 = 5
+# 1500 us = 7.5
+# 2000 us = 10
 #
 # See also https://tutorials-raspberrypi.com/raspberry-pi-servo-motor-control/
 #          https://www.instructables.com/Program-a-Servo-Build-a-Catapult-and-Solve-for-%ce%a0-w
@@ -19,16 +29,14 @@ from time import sleep
 
 servo_pin = 3  # Physical pin. (3: SDA)
 
+STOP_ROTATION = 11.5
+ROTATE_CLOCKWISE = 2.5
+ROTATE_COUNTER_CLOCKWISE = 20.5
+
 print(f"RPi.GPIO version {GPIO.VERSION}")
 
-def set_angle(angle):
-    duty = angle / 18 + 2
-    print(f"\tFor angle {angle}, duty is {duty}")
-    GPIO.output(servo_pin, True)
+def set_rotation(duty):
     pwm.ChangeDutyCycle(duty)    # pwm defined below
-    sleep(1)
-    GPIO.output(servo_pin, False) # This would stop the servo. A continuous would stop spinning.
-    pwm.ChangeDutyCycle(0)
 
 
 GPIO.setmode(GPIO.BOARD)          # <= i.e. Use physical pin numbers
@@ -37,38 +45,45 @@ GPIO.setup(servo_pin, GPIO.OUT)
 
 with_user_input = True   # Will use time delay otherwise
 
-pwm = GPIO.PWM(servo_pin, 50)
+pwm = GPIO.PWM(servo_pin, 100)
 pwm.start(0)
 
-print("Setting angle to 90")
-set_angle(90)
+GPIO.output(servo_pin, True)
+
+
+print("Setting ROTATE_CLOCKWISE")
+set_rotation(ROTATE_CLOCKWISE)
 if with_user_input:
     user_input = input("Hit [return] to move on ")
 else:
-    sleep(1)
+    sleep(5)
 
-print("Setting angle to 0")
-set_angle(0)
+print("Setting STOP_ROTATION")
+set_rotation(STOP_ROTATION)
 if with_user_input:
     user_input = input("Hit [return] to move on ")
 else:
-    sleep(1)
+    sleep(5)
 
-print("Setting angle to 180")
-set_angle(180)
+print("Setting ROTATE_COUNTER_CLOCKWISE")
+set_rotation(ROTATE_COUNTER_CLOCKWISE)
 if with_user_input:
     user_input = input("Hit [return] to move on ")
 else:
-    sleep(1)
+    sleep(5)
 
-print("Setting angle to 0")
-set_angle(0)
+print("Setting STOP_ROTATION")
+set_rotation(STOP_ROTATION)
 if with_user_input:
     user_input = input("Hit [return] to move on ")
 #else:
 #    sleep(1)
 
-print("Done with Standard demo")
+
+GPIO.output(servo_pin, False) # This would stop the servo. A continuous would stop spinning.
+pwm.ChangeDutyCycle(0)
+
+print("Done with Continuous demo")
 pwm.stop()
 
 GPIO.cleanup()
