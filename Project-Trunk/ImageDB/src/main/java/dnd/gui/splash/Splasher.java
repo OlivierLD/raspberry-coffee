af8fec30b31c5d9f8225a289b7b24a54e30840fa
OlivierLD <olivier@lediouris.net>
2021-12-07 17:30:44 +0100

    Cleanup.

--- Project-Trunk/ImageDB/src/main/java/dnd/gui/splash/Splasher.java
+++ Project-Trunk/ImageDB/src/main/java/dnd/gui/splash/Splasher.java
@@ -5,6 +5,11 @@ public class Splasher {
 	public static void main(String... args) {
 		SplashWindow.splash(Splasher.class.getResource("paperboat.png"), null);
 		SplashWindow.invokeMain("dnd.gui.MainGUI", args);
+//		try {
+//			Thread.sleep(2_000L);
+//		} catch (Exception ex) {
+//			ex.printStackTrace();
+//		}
 		SplashWindow.disposeSplash();
 	}
 
