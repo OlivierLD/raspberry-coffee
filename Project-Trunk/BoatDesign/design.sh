9ed7ff0c5690cbee88980ac8ad8788c01d883e4a
OlivierLD <olivier@lediouris.net>
2021-12-14 11:55:16 +0100

    Logging tweaks.

--- Project-Trunk/BoatDesign/design.sh
+++ Project-Trunk/BoatDesign/design.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CP=./build/libs/BoatDesign-1.0-all.jar
-LOGGING_FLAG="-Djava.util.logging.config.file=./logging.properties"
+LOGGING_FLAGS="-Djava.util.logging.config.file=./logging.properties"
 #
 OPT=
 if [[ "$1" != "" ]]
@@ -10,6 +10,6 @@ fi
 #
 OPT="${OPT} -Dspit-out-points=true"
 #
-COMMAND="java ${OPT} ${LOGGING_FLAG} -jar ${CP} $*"
+COMMAND="java ${OPT} ${LOGGING_FLAGS} -jar ${CP} $*"
 echo -e "Running ${COMMAND}"
 ${COMMAND}
