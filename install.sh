d5715fce2ba0590f20b2c2409ddf1e23922b9c37
OlivierLD <olivier@lediouris.net>
2021-12-18 18:19:52 +0100

    Small changes in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo -e "Welcome to the install Script of the whole repo"
 git clone https://github.com/OlivierLD/raspberry-coffee.git
-git submodule update --init
 pushd raspberry-coffee
+  # git submodule update --init
+  git clone https://github.com/OlivierLD/AstroComputer.git
   ./gradlew tasks --all
   echo -e "---------------------------------------------------------"
   echo -e "You should be good to go, the infrastructure is in place."
