35524070798455bf1d40acf6bc316b93749d8c77
OlivierLD <olivier@lediouris.net>
2021-12-21 11:21:38 +0100

    Cleanup.

--- RESTNavServer/launchers/web/scratch/fax-processor/fax.processor.sh
+++ RESTNavServer/launchers/web/scratch/fax-processor/fax.processor.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-# Download faxes
+# Download weather faxes locally
+# Start HTTP Server
+# Open Web Page (fax re-working is done in there)
+#
+# 1.Download faxes
 #
 # North-West Atlantic: https://tgftp.nws.noaa.gov/fax/PYAA12.gif
 wget https://tgftp.nws.noaa.gov/fax/PYAA12.gif --output-document NW-Atl.gif
@@ -11,14 +15,14 @@ wget https://tgftp.nws.noaa.gov/fax/PPAA10.gif --output-document N-Atl-500mb.gif
 # North Atlantic Sea State
 wget https://tgftp.nws.noaa.gov/fax/PJAA99.gif --output-document N-Atl-waves.gif
 #
-# Start small python server
+# 2.Start small python server
 # See https://rawsec.ml/en/python-3-simplehttpserver/
 #
 python3 -m http.server 8080 &
 SERVER_PROCESS_ID=$(echo $!)
 echo -e "To kill the server, used PID ${SERVER_PROCESS_ID}"
 #
-# open the page
+# 3.Open the page
 #
 OS=`uname -a | awk '{ print $1 }'`
 if [[ "$OS" == "Darwin" ]]
---
+++ RESTNavServer/launchers/web/scratch/fax-processor/faxes.js
@@ -0,0 +1,93 @@
+const TO_RED = 1;
+const TO_BLUE = 2;
+const TO_GREEN = 3;
+
+messWithCanvas = (img, canvasName, changeBlackTo) => {
+  const canvas = document.getElementById(canvasName);
+  let ctx = canvas.getContext("2d");
+
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight, 0, 0, img.width, img.height);
+
+  originalPixels = ctx.getImageData(0, 0, img.width, img.height);
+  currentPixels = ctx.getImageData(0, 0, img.width, img.height);
+
+  let pix = originalPixels.data;
+  // console.log(`There are ${pix.length} pixels.`);
+
+  for (let idx=0; idx<pix.length; idx+=4) { // 4 by 4, rgba.
+    let r = pix[idx];       // Red
+    let g = pix[idx + 1];   // Green
+    let b = pix[idx + 2];   // Blue
+    let a = pix[idx + 3];   // Alpha
+    // console.log(`rgba(${r},${g},${b},${a})`);
+    if (r === 0 && b === 0 && g === 0) { // black
+      if (changeBlackTo === TO_RED) {
+        r = 255;  // Change black to red
+      } else if (changeBlackTo === TO_BLUE) {
+        b = 255;  // Change black to blue
+      } else if (changeBlackTo === TO_GREEN) {
+        // 47, 79, 47
+        // Change black to green
+        r = 47;
+        g = 79;
+        b = 47;
+      }
+    } else if (r === 255 && b === 255 && g === 255) { // white
+      a = 0;    // Make white transparent
+    }
+    currentPixels.data[idx] = r;
+    currentPixels.data[idx + 1] = g;
+    currentPixels.data[idx + 2] = b;
+    currentPixels.data[idx + 3] = a;
+  }
+  ctx.putImageData(currentPixels, 0, 0);  // Bam!
+};
+
+doOnLoad = () => {
+  const leftFax = document.getElementById('left');
+  const rightFax = document.getElementById('right');
+  const fax500mb = document.getElementById('500mb');
+  const faxWaves = document.getElementById('waves');
+
+  messWithCanvas(leftFax, 'left-img-canvas', TO_RED);
+  const leftReworkedFax = document.getElementById('left-img-canvas');
+  leftReworkedFax.style.width = '600px';
+  leftReworkedFax.style.height = 'auto';
+  leftReworkedFax.style.transform = 'rotate(90deg)';
+  // leftReworkedFax.style.marginLeft = '0px';
+  leftReworkedFax.style.top = '160px';
+  leftReworkedFax.style.left = '125px';
+
+  messWithCanvas(rightFax, 'right-img-canvas', TO_RED);
+  const rightReworkedFax = document.getElementById('right-img-canvas');
+  rightReworkedFax.style.width = '600px';
+  rightReworkedFax.style.height = 'auto';
+  rightReworkedFax.style.transform = 'rotate(90deg)';
+  // rightReworkedFax.style.marginLeft = '-188px';
+  rightReworkedFax.style.top = '160px';
+  rightReworkedFax.style.left = '541px';
+
+  messWithCanvas(fax500mb, 'north-img-canvas', TO_BLUE);
+  const northReworkedFax = document.getElementById('north-img-canvas');
+  northReworkedFax.style.width = '919px';
+  northReworkedFax.style.height = 'auto';
+  northReworkedFax.style.transform = 'rotate(0deg)';
+  // northReworkedFax.style.marginLeft = '62px';
+  // northReworkedFax.style.marginTop = '-537px';
+  northReworkedFax.style.top = '129px';
+  northReworkedFax.style.left = '187px';
+
+  messWithCanvas(faxWaves, 'waves-img-canvas', TO_GREEN);
+  const waveshReworkedFax = document.getElementById('waves-img-canvas');
+  waveshReworkedFax.style.width = '919px';
+  waveshReworkedFax.style.height = 'auto';
+  waveshReworkedFax.style.transform = 'rotate(0deg)';
+  // waveshReworkedFax.style.marginLeft = '62px';
+  // waveshReworkedFax.style.marginTop = '-537px';
+  waveshReworkedFax.style.top = '129px';
+  waveshReworkedFax.style.left = '187px';
+
+};
