1bc8ff7c0ad4f29c8b5f6b9e27efbe6bc981ebad
OlivierLD <olivier@lediouris.net>
2022-01-05 19:27:56 +0100

    Moved the tests.

--- NMEA-multiplexer/gpsd.ais.test.sh
+++ NMEA-multiplexer/gpsd.ais.test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 CP=./build/libs/NMEA-multiplexer-1.0-all.jar
+CP="${CP}:build/classes/java/test/"
 JAVA_OPTS=
 # JAVA_OPTS="${JAVA_OPTS} -Dverbose=true"
 # JAVA_OPTS="${JAVA_OPTS} -Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80"
-java -cp ${CP} ${JAVA_OPTS} util.tests.GPSDClient
+java -cp ${CP} ${JAVA_OPTS} tcptests.GPSDClient
