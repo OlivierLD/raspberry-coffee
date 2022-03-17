12c2b3b34c4d16671bf5df8551972e45b6eeb2a4
OlivierLD <olivier@lediouris.net>
2022-03-17 20:54:32 +0100

    Doc fixes.

--- Project-Trunk/BoatDesign/README.md
+++ Project-Trunk/BoatDesign/README.md
@@ -9,7 +9,7 @@ Based on the `Algebra` module, in this project.
 
 ![Scheen shot](./img/screen.shot.01.png)
 
-## Path #1
+## Path 1
 - Define Length, Width, Height
 - Define a 3D bezier for the rail (on a 2D white board)
 <!-- - Define a 2 or 3D bezier for the bow (on a 2D white board) -->
--- Python.101/forloop.py
+++ Python.101/forloop.py
@@ -1,16 +1,16 @@
 #
 
-for i in range(10):
+for i in range(10):  # i: 0 to 9. for (i=0; i<10; i++)
 	print(i)
 
 print("- One")
 
-for i in range (2, 10):
+for i in range (2, 10):  # i: 2 to 9. for (int i=2; i<10; i++)
 	print(i)
 
 print("- Two")
 
-for i in range (2, 10, 2):
+for i in range (2, 10, 2):  # i: 2, 4, 6, 8. for (int i=2; i<10; i+=2)
 	print(i)
 
 print("- Done")
