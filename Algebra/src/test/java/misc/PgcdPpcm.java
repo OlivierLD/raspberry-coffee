88c16782ae8b7988c2e46770892fb1b5d9fcfc4c
OlivierLD <olivier@lediouris.net>
2022-06-21 19:48:30 +0200

    PGCD and PPCM

--- Algebra/src/test/java/misc/PgcdPpcm.java
+++ Algebra/src/test/java/misc/PgcdPpcm.java
@@ -5,7 +5,7 @@ import static ppcm.PPCM.ppcm;
 
 public class PgcdPpcm {
 
-    public final static void main(String... args) {
+    public static void main(String... args) {
         int n1 = 60, n2 = 36;
         System.out.printf("PGCD(%d, %d) = %d\n", n1, n2, pgcd(n1, n2));
         n1 = 355; n2 = 113;
