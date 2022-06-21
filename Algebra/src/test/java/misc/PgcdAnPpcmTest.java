018ec65e78f2ca4dd19fcdefd4b15993a62665e4
OlivierLD <olivier@lediouris.net>
2022-06-21 20:16:44 +0200

    PGCD and PPCM, tests.

--- Algebra/src/test/java/misc/PgcdAnPpcmTest.java
+++ Algebra/src/test/java/misc/PgcdAnPpcmTest.java
@@ -2,35 +2,35 @@ package misc;
 
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertEquals;
 import pgcd.PGCD;
 import ppcm.PPCM;
 
-public class PgcdPpcm {
+public class PgcdAnPpcmTest {
 
     @Test
     public void one() {
         int n1 = 60, n2 = 36;
         int pgcd = PGCD.pgcd(n1, n2);
-        assertTrue(String.format("Expecting 12, got %d", pgcd), pgcd == 12);
+        assertEquals(String.format("Expecting 12, got %d", pgcd), 12, pgcd);
     }
     @Test
     public void two() {
         int n1 = 355, n2 = 113;
         int pgcd = PGCD.pgcd(n1, n2);
-        assertTrue(String.format("Expecting 1, got %d", pgcd), pgcd == 1);
+        assertEquals(String.format("Expecting 1, got %d", pgcd), 1, pgcd);
     }
     @Test
     public void three() {
         int n1 = 355, n2 = 113;
         int ppcm = PPCM.ppcm(n1, n2);
-        assertTrue(String.format("Expecting 40,115, got %d", ppcm), ppcm == 40_115);
+        assertEquals(String.format("Expecting 40,115, got %d", ppcm), 40_115, ppcm);
     }
     @Test
     public void four() {
         int n1 = 60, n2 = 36;
         int ppcm = PPCM.ppcm(n1, n2);
-        assertTrue(String.format("Expecting 180, got %d", ppcm), ppcm == 180);
+        assertEquals(String.format("Expecting 180, got %d", ppcm), 180, ppcm);
     }
 
 }
