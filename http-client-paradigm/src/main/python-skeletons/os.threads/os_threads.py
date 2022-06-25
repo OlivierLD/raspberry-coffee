0ff0d0f964c35a2829ce9d5e7b2a27a58e7f16c2
OlivierLD <olivier@lediouris.net>
2022-06-25 16:08:49 +0200

    Modified the threads sample

--- http-client-paradigm/src/main/python-skeletons/os.threads/os_threads.py
+++ http-client-paradigm/src/main/python-skeletons/os.threads/os_threads.py
@@ -2,26 +2,33 @@ import threading
 import os
 import sys
 import traceback
+import time
 
 """
  Starts 3 threads (wasting time)
  running OS commands
 """
 
+
+nbThreadCompleted: int = 0
+
+
 def execOS(cmd: str) -> None:
+    global nbThreadCompleted
     print(f"Will execute {cmd}")
     stream = os.popen(cmd)
     output = stream.read()  # return something ?
     # Could also use output.rstrip() to remove trailing NL
-    print(f"Returned: {output}")
+    print(f"Thread execution returned: {output}")
+    nbThreadCompleted += 1
 
 
 try:
-    cmd: str = "sleep 3; echo 'Done - 1'"
+    cmd: str = "sleep 5; echo 'Done - 1'"
     thread1: threading.Thread = threading.Thread(name="One", target=execOS, args=(cmd,))
     thread1.daemon = True  # Dies on exit
 
-    cmd = "sleep 2; echo 'Done - 2'"
+    cmd = "sleep 3; echo 'Done - 2'"
     thread2: threading.Thread = threading.Thread(name="Two", target=execOS, args=(cmd,))
     thread2.daemon = True  # Dies on exit
 
@@ -36,10 +43,13 @@ except Exception as ex:
     print("Exception!")
     traceback.print_exc(file=sys.stdout)
 
-keep_looping: bool = True
-while keep_looping:
-    print("Hit Return to exit (when done)")
-    user_input: str = input()
-    keep_looping = False
+# keep_looping: bool = True
+while nbThreadCompleted < 3:
+    print(f"\t...{nbThreadCompleted} thread(s) completed.")
+    time.sleep(1)
+
+# print("All threads completed, Hit Return to exit.")
+user_input: str = input("All threads completed, Hit Return to exit.")
+# keep_looping = False
 
 print("Bye!")
