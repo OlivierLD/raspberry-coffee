356fb92e9835694f09993ef755fb0e5019100d7e
OlivierLD <olivier@lediouris.net>
2022-07-11 07:27:50 +0200

    Script, MCP, remote debugging.

--- raspberry-io-pi4j/ADC/adcreader.3008.sh
+++ raspberry-io-pi4j/ADC/adcreader.3008.sh
@@ -10,6 +10,8 @@ REMOTE_DEBUG_FLAGS=
 REMOTE_DEBUG_FLAGS="${REMOTE_DEBUG_FLAGS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
 #
 # Channel [0..1] can be passed as prm. Default is 0
-COMMAND=sudo java -cp ${CP} ${JAVA_OPTS} ${REMOTE_DEBUG_FLAGS} analogdigitalconverter.sample.MainMCP3008Sample $*
+COMMAND="sudo java -cp ${CP} ${JAVA_OPTS} ${REMOTE_DEBUG_FLAGS} analogdigitalconverter.sample.MainMCP3008Sample $*"
+echo -e "------------------------------------------------------------------------------"
 echo -e "Running ${COMMAND}"
+echo -e "------------------------------------------------------------------------------"
 ${COMMAND}
