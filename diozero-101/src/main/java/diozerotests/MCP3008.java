511bc452e25bd9c0a38627922b2d44841c60239a
OlivierLD <olivier@lediouris.net>
2022-07-26 15:27:35 +0200

    MCP3008 on diozero, keep trying.

--- diozero-101/src/main/java/diozerotests/MCP3008.java
+++ diozero-101/src/main/java/diozerotests/MCP3008.java
@@ -14,12 +14,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Represents an MCP3008 analog to digital converter.
  *
  * Requires the following wiring:
- * MCP3008 Vdd-Vref   -> Pi 3V3:  Pin #1
- * MCP3008 Gnd        -> Pi GND:  Pin #6
- * MCP3008 Mosi, Din  -> Pi MOSI: Pin #19
- * MCP3008 Miso, Dout -> Pi MISO: Pin #21
- * MCP3008 Clk        -> Pi SCLK: Pin #23
- * MCP3008 CS         -> Pi CE1:  Pin #26
+ * MCP3008 Vdd-Vref   -> Pi 3V3:  Physical Pin #1
+ * MCP3008 Gnd        -> Pi GND:  Physical Pin #6
+ * MCP3008 Mosi, Din  -> Pi MOSI: Physical Pin #19
+ * MCP3008 Miso, Dout -> Pi MISO: Physical Pin #21
+ * MCP3008 Clk        -> Pi SCLK: Physical Pin #23
+ * MCP3008 CS         -> Pi CE1:  Physical Pin #26
  *
  * Read MCP3008 Channel 0
  *
@@ -27,7 +27,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class MCP3008 implements AutoCloseable {
 
-    private SpiDevice device = null;
+    private SpiDevice device;
     private final float vRef;
 
     /**
@@ -36,7 +36,7 @@ public class MCP3008 implements AutoCloseable {
      sets an SPI clock frequency to 1.35MHz.
      * @param chipSelect the chip select pin
      * @param vRef the reference voltage
-     * @throws java.io.IOException
+     * @throws java.io.IOException when fails
      */
     public MCP3008(int chipSelect, float vRef) throws IOException {
         this(SpiConstants.DEFAULT_SPI_CONTROLLER, chipSelect, vRef);
@@ -49,7 +49,7 @@ public class MCP3008 implements AutoCloseable {
      * @param controller the SPI controller
      * @param chipSelect the chip select pin
      * @param vRef the reference voltage
-     * @throws java.io.IOException
+     * @throws java.io.IOException when fails
      */
     public MCP3008(int controller, int chipSelect, float vRef) throws IOException {
         try {device = SpiDevice.builder(chipSelect)
@@ -77,7 +77,7 @@ public class MCP3008 implements AutoCloseable {
      * Reads a channel and returns the raw value.
      * @param channel channel to read
      * @return raw sample value
-     * @throws RuntimeIOException
+     * @throws RuntimeIOException when fails
      */
     public int getRaw(int channel) throws RuntimeIOException {
         // create channel code; assume single-ended
@@ -98,7 +98,7 @@ public class MCP3008 implements AutoCloseable {
      * Reads a channel and returns its value as a fraction of full scale.
      * @param channel channel to read
      * @return sample value as a fraction of full scale
-     * @throws RuntimeIOException
+     * @throws RuntimeIOException when fails
      */
     public float getFSFraction(int channel) throws RuntimeIOException {
         int raw = getRaw(channel);
@@ -111,7 +111,7 @@ public class MCP3008 implements AutoCloseable {
      * reference voltage.
      * @param channel channel to read
      * @return sample value as a voltage
-     * @throws RuntimeIOException
+     * @throws RuntimeIOException when fails
      */
     public float getVoltage(int channel)  throws RuntimeIOException {
         return (getFSFraction(channel) * vRef);
@@ -136,12 +136,19 @@ public class MCP3008 implements AutoCloseable {
             }
         }, "Shutdown Hook"));
 
+        boolean first = true;
+        int previousVal = 0;
+
         try (MCP3008 adc = new MCP3008(SpiConstants.CE1, 3.3f)) {
             while (go.get()) {
-                System.out.format("C0 = %4d, %.2f FS, %.2fV %n",
-                        adc.getRaw(channel),
-                        adc.getFSFraction(channel),
-                        adc.getVoltage(channel));
+                int value = adc.getRaw(channel);
+                if (first || value != previousVal) {
+                    System.out.format("C0 = %4d, %.2f FS, %.2fV %n",
+                            value,
+                            adc.getFSFraction(channel),
+                            adc.getVoltage(channel));
+                }
+                previousVal = value;
                 SleepUtil.sleepMillis(100L);
             }
         } catch (IOException ioe) {
