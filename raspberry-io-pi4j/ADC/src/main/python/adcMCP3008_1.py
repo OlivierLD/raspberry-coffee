9d6649217de33b7fe88127c2f7888bc00470ac0c
OlivierLD <olivier@lediouris.net>
2022-07-29 09:40:19 +0200

    Python version for MCP3008.

--- raspberry-io-pi4j/ADC/src/main/python/adcMCP3008_1.py
+++ raspberry-io-pi4j/ADC/src/main/python/adcMCP3008_1.py
@@ -15,22 +15,22 @@ verbose: bool = True
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 if verbose:
-    print(f">> SPI type: {type(spi)}")
+    print(">> SPI type: {}".format(type(spi)))
 
 # create the cs (chip select)
 cs = digitalio.DigitalInOut(board.D5)
 if verbose:
-    print(f">> ChipSelect type: {type(cs)}")
+    print(">> ChipSelect type: {}".format(type(cs)))
 
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 if verbose:
-    print(f">> MCP type: {type(mcp)}")
+    print(">> MCP type: {}".format(type(mcp)))
 
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
 if verbose:
-    print(f">> Channel type: {type(chan)}")
+    print(">> Channel type: {}".format(type(chan)))
 
 
 print('-- First display --')
@@ -47,7 +47,7 @@ while keep_looping:
         adc: int = (3.3 / voltage) * 1023
 #         print('Raw ADC Value: ', value)
 #         print('ADC Voltage: ' + str(voltage) + 'V')
-        print(f"Raw: {value}, Voltage: {str(voltage)}, ADC: {adc}")   # Look into {adc: %d}...
+        print("Raw: {}, Voltage: {}, ADC: {}".format(value, str(voltage), adc))   # Look into {adc: %d}...
         sleep(0.5)
     except KeyboardInterrupt:
         print("\n\t\tUser interrupted, exiting.")
