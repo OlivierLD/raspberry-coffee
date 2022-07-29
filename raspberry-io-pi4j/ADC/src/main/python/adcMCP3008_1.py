339009f614ac4f21b4876e2942e7e08e6ef7cbe3
OlivierLD <olivier@lediouris.net>
2022-07-29 09:47:03 +0200

    Python version for MCP3008.

--- raspberry-io-pi4j/ADC/src/main/python/adcMCP3008_1.py
+++ raspberry-io-pi4j/ADC/src/main/python/adcMCP3008_1.py
@@ -13,22 +13,22 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 verbose: bool = True
 
 # create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+spi: busio.SPI = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 if verbose:
     print(">> SPI type: {}".format(type(spi)))
 
 # create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
+cs: digitalio.DigitalInOut = digitalio.DigitalInOut(board.D5)
 if verbose:
     print(">> ChipSelect type: {}".format(type(cs)))
 
 # create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+mcp: adafruit_mcp3xxx.mcp3008.MCP3008 = MCP.MCP3008(spi, cs)
 if verbose:
     print(">> MCP type: {}".format(type(mcp)))
 
 # create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+chan: adafruit_mcp3xxx.analog_in.AnalogIn = AnalogIn(mcp, MCP.P0)
 if verbose:
     print(">> Channel type: {}".format(type(chan)))
 
@@ -44,7 +44,7 @@ while keep_looping:
     try:
         value: int = chan.value
         voltage: float = chan.voltage
-        adc: int = (3.3 / voltage) * 1023
+        adc: int = (voltage / 3.3) * 1023
 #         print('Raw ADC Value: ', value)
 #         print('ADC Voltage: ' + str(voltage) + 'V')
         print("Raw: {}, Voltage: {}, ADC: {}".format(value, str(voltage), adc))   # Look into {adc: %d}...
