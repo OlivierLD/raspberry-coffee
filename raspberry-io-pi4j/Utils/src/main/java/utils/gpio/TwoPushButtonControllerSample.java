11fda91676053b5fa25dba7243959a4dff5c54a2
OlivierLD <olivier@lediouris.net>
2022-08-01 13:52:30 +0200

    Still on PushButtonController.

--- raspberry-io-pi4j/Utils/src/main/java/utils/gpio/TwoPushButtonControllerSample.java
+++ raspberry-io-pi4j/Utils/src/main/java/utils/gpio/TwoPushButtonControllerSample.java
@@ -5,17 +5,20 @@ import com.pi4j.io.gpio.RaspiPin;
 
 /**
  * Just test, to be run separately, for debugging...
- * Button:
- * - One pin on 3V3
+ * 2 Buttons:
+ * - One and Two pin on 3V3
  * - One pin on GPIO 28 (BCM 20, physical #38)
+ * - Two pin on GPIO 29 (BCM 21, physical #40)
  */
-public class PushButtonControllerSample {
+public class TwoPushButtonControllerSample {
 
 	private Pin buttonOnePin;
+	private Pin buttonTwoPin;
 
 	final static PushButtonController buttonOne = new PushButtonController();
+	final static PushButtonController buttonTwo = new PushButtonController();
 
-	private final Runnable sayHello = () -> {
+	private final Runnable sayHelloOne = () -> {
 		try {
 			System.out.println("OnClick [One]: Hello!");
 		} catch (Exception ex) {
@@ -23,7 +26,7 @@ public class PushButtonControllerSample {
 			ex.printStackTrace();
 		}
 	};
-	private final Runnable sayHelloHello = () -> {
+	private final Runnable sayHelloHelloOne = () -> {
 		try {
 			System.out.println("OnDoubleClick [One]: Hello Hello!");
 		} catch (Exception ex) {
@@ -31,7 +34,7 @@ public class PushButtonControllerSample {
 			ex.printStackTrace();
 		}
 	};
-	private final Runnable sayHellooo = () -> {
+	private final Runnable sayHelloooOne = () -> {
 		try {
 			System.out.println("OnLongClick [One]: Hellooo!");
 		} catch (Exception ex) {
@@ -39,24 +42,57 @@ public class PushButtonControllerSample {
 			ex.printStackTrace();
 		}
 	};
+	private final Runnable sayHelloTwo = () -> {
+		try {
+			System.out.println("OnClick [Two]: Hello!");
+		} catch (Exception ex) {
+			System.err.println("Say Hello:");
+			ex.printStackTrace();
+		}
+	};
+	private final Runnable sayHelloHelloTwo = () -> {
+		try {
+			System.out.println("OnDoubleClick [Two]: Hello Hello!");
+		} catch (Exception ex) {
+			System.err.println("Say Hello Hello:");
+			ex.printStackTrace();
+		}
+	};
+	private final Runnable sayHelloooTwo = () -> {
+		try {
+			System.out.println("OnLongClick [Two]: Hellooo!");
+		} catch (Exception ex) {
+			System.err.println("Say Hellooo:");
+			ex.printStackTrace();
+		}
+	};
 
-	public PushButtonControllerSample() {
+	public TwoPushButtonControllerSample() {
 		try {
 			// Provision buttons here
 			buttonOnePin = RaspiPin.GPIO_28; // wiPi 28, BCM 20, Physical #38.
+			buttonTwoPin = RaspiPin.GPIO_29; // wiPi 29, BCM 21, Physical #40.
 
-			// Button-1 provisioning, with its operations
+			// Button-1 provisioning, with its name and operations
 			buttonOne.update(
-					"The-Button",
+					"Button-One",
 					buttonOnePin,
-					sayHello,
-					sayHelloHello,
-					sayHellooo);
+					sayHelloOne,
+					sayHelloHelloOne,
+					sayHelloooOne);
+			// Button-1 provisioning, with its name and operations
+			buttonTwo.update(
+					"Button-Two",
+					buttonTwoPin,
+					sayHelloTwo,
+					sayHelloHelloTwo,
+					sayHelloooTwo);
 
 		} catch (Throwable error) {
 			error.printStackTrace();
 		}
 		System.out.println(">> Button 28 (physical #38) provisioned.");
+		System.out.println(">> Button 29 (physical #40) provisioned.");
 		System.out.println("\tTry click, double-click, long-click.");
 
 	}
@@ -84,7 +120,7 @@ public class PushButtonControllerSample {
 
 		// System.setProperty("button.verbose", "true");
 
-		new PushButtonControllerSample();
+		new TwoPushButtonControllerSample();
 
 		// Now wait for the user to stop the program
 		System.out.println("Ctrl-C to stop.");
