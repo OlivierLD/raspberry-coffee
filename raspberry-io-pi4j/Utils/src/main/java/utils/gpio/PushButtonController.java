ccc280b328972768c781c13abcb20adc5036ca66
OlivierLD <olivier@lediouris.net>
2022-08-02 15:45:52 +0200

    Remote debug could help.

--- raspberry-io-pi4j/Utils/src/main/java/utils/gpio/PushButtonController.java
+++ raspberry-io-pi4j/Utils/src/main/java/utils/gpio/PushButtonController.java
@@ -231,7 +231,7 @@ public class PushButtonController {
                     if (this.maybeDoubleClick && this.betweenClicks > 0 && this.betweenClicks < DOUBLE_CLICK_DELAY) {
                         this.maybeDoubleClick = false; // Done with 2nd click of a double click.
                         if (verbose) {
-                            System.out.printf("\t>>> Detected Double-click. In DoubleClick branch (%s), Setting maybeDoubleClick back to false%n", this.buttonName);
+                            System.out.printf("\t>>> Detected TOP Double-click. In DoubleClick branch (%s), Setting maybeDoubleClick back to false%n", this.buttonName);
                         }
                         // Execute double-click operation
                         this.onDoubleClick.run();
@@ -252,10 +252,10 @@ public class PushButtonController {
                                 this.wait(DOUBLE_CLICK_DELAY); // !! Cannot work in simulation mode if not in a Thread !!
                                 if (this.maybeDoubleClick) { // Can have been set to false by a double click
 
-                                    if (this.maybeDoubleClick && this.betweenClicks > 0 && this.betweenClicks < DOUBLE_CLICK_DELAY) {
+                                    if (/*this.maybeDoubleClick &&*/ this.betweenClicks > 0 && this.betweenClicks < DOUBLE_CLICK_DELAY) {
                                         this.maybeDoubleClick = false; // Done with 2nd click of a double click.
                                         if (verbose) {
-                                            System.out.printf("\t>>> Detected Double-click. In DoubleClick branch (%s), Setting maybeDoubleClick back to false%n", this.buttonName);
+                                            System.out.printf("\t>>> Detected 2nd Double-click. In DoubleClick branch (%s), Setting maybeDoubleClick back to false%n", this.buttonName);
                                         }
                                         // Execute double-click operation
                                         this.onDoubleClick.run();
