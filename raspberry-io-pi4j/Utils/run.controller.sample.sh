c665f5eb87e9757d8cc63c4f1ec197ebb5b6e732
OlivierLD <olivier@lediouris.net>
2022-08-02 15:07:02 +0200

    Starting thread or not, tweaking.

--- raspberry-io-pi4j/Utils/run.controller.sample.sh
+++ raspberry-io-pi4j/Utils/run.controller.sample.sh
@@ -3,7 +3,7 @@ CP=./build/libs/Utils-1.0-all.jar
 #
 # For remote debugging
 REMOTE_DEBUG_FLAGS=
-# REMOTE_DEBUG_FLAGS="$REMOTE_DEBUG_FLAGS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+# REMOTE_DEBUG_FLAGS="${REMOTE_DEBUG_FLAGS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"
 #
 # java -cp ${CP} ${REMOTE_DEBUG_FLAGS} -Dbutton.verbose=true utils.gpio.PushButtonControllerSample
 java -cp ${CP} ${REMOTE_DEBUG_FLAGS} -Dbutton.verbose=true utils.gpio.TwoPushButtonControllerSample
