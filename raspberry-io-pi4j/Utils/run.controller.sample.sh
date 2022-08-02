80acb094c62582e61ce3dd62ff4432dd94579297
OlivierLD <olivier@lediouris.net>
2022-08-02 16:02:25 +0200

    Cleaning up.

--- raspberry-io-pi4j/Utils/run.controller.sample.sh
+++ raspberry-io-pi4j/Utils/run.controller.sample.sh
@@ -6,8 +6,8 @@ REMOTE_DEBUG_FLAGS=
 # REMOTE_DEBUG_FLAGS="${REMOTE_DEBUG_FLAGS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
 #
 JAVA_OPTIONS=
-JAVA_OPTIONS="-Dbutton.verbose=true"
-# java -cp ${CP} ${REMOTE_DEBUG_FLAGS} ${JAVA_OPTIONS} utils.gpio.PushButtonControllerSample
+# JAVA_OPTIONS="-Dbutton.verbose=true"
+# COMMAND="java -cp ${CP} ${REMOTE_DEBUG_FLAGS} ${JAVA_OPTIONS} utils.gpio.PushButtonControllerSample"
 COMMAND="java -cp ${CP} ${REMOTE_DEBUG_FLAGS} ${JAVA_OPTIONS} utils.gpio.TwoPushButtonControllerSample"
 echo -e "Executing ${COMMAND}"
 ${COMMAND}
