30fd8478ccf72375b1e5d6f78a341a1acc336af3
OlivierLD <olivier@lediouris.net>
2022-08-06 11:00:56 +0200

    Message when shutting down.

--- Project-Trunk/SunFlower-v2/server.sh
+++ Project-Trunk/SunFlower-v2/server.sh
@@ -81,7 +81,7 @@ esac
 CP=${CP}:/usr/share/java/RXTXcomm.jar                                # For Raspberry Pi
 #
 REMOTE_DEBUG_FLAGS=
-# REMOTE_DEBUG_FLAGS="$REMOTE_DEBUG_FLAGS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+# REMOTE_DEBUG_FLAGS="$REMOTE_DEBUG_FLAGS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
 JAVA_OPTS="${JAVA_OPTS} ${REMOTE_DEBUG_FLAGS}"
 MY_IP=$(hostname -I | awk '{ print $1 }')
 MY_IP=$(echo ${MY_IP})  # Trim the blanks
