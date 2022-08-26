ebf9aa8a373697d2a3fbaaaac84e36e4de7d7fd1
OlivierLD <olivier@lediouris.net>
2022-08-26 08:43:37 +0200

    Doc and code polishing.

--- http-client-samples/eink.sh
+++ http-client-samples/eink.sh
@@ -4,7 +4,7 @@ REST_PORT=8080
 REST_URL=http://${REST_IP}:${REST_PORT}/eink2_13/display
 #
 echo -e "Require the Python server to be running somewhere (like on ${REST_IP}):"
-echo -e "$ python3 src/main/python/eink-2.13/server/eink_2.13_server.py "
+echo -e "$ python3 src/main/python/eink-2.13/server/eink_2.13_server.py --machine-name:${REST_IP} [ --port:8888 --verbose:true ]"
 echo -e " "
 #
 echo -e "Requests will be POSTed to ${REST_URL}"
