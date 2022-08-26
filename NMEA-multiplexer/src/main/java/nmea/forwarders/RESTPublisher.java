1da4e98717c81954ebdb482ea16ba37b8f15d241
OlivierLD <olivier@lediouris.net>
2022-08-26 15:51:19 +0200

    REST Publisher tests

--- NMEA-multiplexer/src/main/java/nmea/forwarders/RESTPublisher.java
+++ NMEA-multiplexer/src/main/java/nmea/forwarders/RESTPublisher.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+// See the test, in the test folder.
 public class RESTPublisher implements Forwarder {
 	private Properties props = null;
 
@@ -182,44 +183,6 @@ public class RESTPublisher implements Forwarder {
 					}
 				}
 			}
-		}
-	}
-
-	private final static List<String> DATA_TO_SEND = List.of(
-		"Ping", "Pong", "Paf",
-		"Bing", "Boom", "Bang"
-	);
-
-	public static void main(String... args) {
-		String wpl = "$GPWPL,3739.856,N,12222.812,W,OPMRNA*59";
-		try {
-			RESTPublisher restPublisher = new RESTPublisher();
-
-			Properties props = new Properties();
-			props.put("server.name", "localhost"); // That one must be up and running for this main to work.
-			props.put("server.port", "8080");
-//			props.put("rest.resource", "/rest/endpoint?qs=prm");
-			props.put("rest.resource", "/eink2_13/display");
-			props.put("rest.verb", "POST");
-			props.put("http.headers", "Content-Type:plain/text");
-			restPublisher.setProperties(props);
-
-			for (int i = 0; i < 50; i++) {
-				System.out.println(DATA_TO_SEND.get(i % DATA_TO_SEND.size()));
-				try {
-					wpl = DATA_TO_SEND.get(i % DATA_TO_SEND.size());
-					restPublisher.write(wpl.getBytes());
-				} catch (Exception ex) {
-					System.err.println(ex.getLocalizedMessage());
-				}
-				try {
-					Thread.sleep(1_000L);
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
 }
