76efa793c96cf1accc89b514db42e68396b63a69
OlivierLD <olivier@lediouris.net>
2022-10-12 18:02:23 +0200

    Beaufort tests

--- raspberry-sailor/NMEA-Parser/src/test/java/utils/Beaufort.java
+++ raspberry-sailor/NMEA-Parser/src/test/java/utils/Beaufort.java
@@ -3,6 +3,7 @@ package utils;
 import nmea.utils.WindUtils;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Beaufort {
@@ -10,13 +11,13 @@ public class Beaufort {
     @Test
     public void beaufortTest() {
         int beaufort = WindUtils.getBeaufort(23.45);
-        assertTrue(String.format("Expected 6, got %d", beaufort), beaufort == 6);
+        assertEquals(String.format("Expected 6, got %d", beaufort), 6, beaufort);
 
         beaufort = WindUtils.getBeaufort(70d);
         assertTrue(String.format("Expected 12+, got %d", beaufort), beaufort >= 12);
 
         beaufort = WindUtils.getBeaufort(20d);
-        assertTrue(String.format("Expected 5, got %d", beaufort), beaufort == 5);
+        assertEquals(String.format("Expected 5, got %d", beaufort), 5, beaufort);
     }
 
 }
