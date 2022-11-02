408310ea4463fa9c390992fa2d5c1893ed45b1cf
OlivierLD <olivier@lediouris.net>
2022-11-02 10:44:16 +0100

    Starting on GTK

--- NMEA-Mux-Other-Clients/src/main/C/gtk/basic101/compile.sh
+++ NMEA-Mux-Other-Clients/src/main/C/gtk/basic101/compile.sh
@@ -2,6 +2,10 @@
 echo -e "pkg-config --cflags --libs gtk+-2.0 :"
 pkg-config --cflags --libs gtk+-2.0
 #
+# Works on Mac
 # gcc `pkg-config --cflags --libs gtk+-2.0` gtktest.c -o gtktest
-gcc $(pkg-config --cflags --libs gtk+-2.0) gtktest.c -o gtktest
+# gcc $(pkg-config --cflags --libs gtk+-2.0) gtktest.c -o gtktest
+#
+# Works on Raspberry Pi and Mac
+gcc -o gtktest gtktest.c $(pkg-config gtk+-2.0 --cflags --libs)
 #
