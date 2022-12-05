6016d1915f94948a4eb75cb1b3ce21adbf367234
OlivierLD <olivier@lediouris.net>
2022-12-05 09:53:59 +0100

    Tune-up, and more pi4j-v2 samples.

---
+++ raspberry-pi4j-v2/run.test.BME280.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+CP=./build/libs/raspberry-pi4j-v2-2.0-all.jar
+java -cp ${CP} oliv.tests.BME280Test
+#
---
+++ raspberry-pi4j-v2/src/main/java/oliv/tests/HTU21DTest.java
@@ -0,0 +1,48 @@
+package oliv.tests;
+
+// Copied from package one.microproject.rpi.hardware.gpio.sensors.tests;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import one.microproject.rpi.hardware.gpio.sensors.HTU21DF;
+import one.microproject.rpi.hardware.gpio.sensors.HTU21DFBuilder;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.tinylog.Logger;
+
+/*
+ * This one uses TinyLog (https://tinylog.org/v2/)
+ */
+
+public class HTU21DTest {
+
+    // private static final Logger LOG = LoggerFactory.getLogger(HTU21DTest.class);
+
+    private HTU21DTest() {
+    }
+
+    public static void test(Context context) throws Exception {
+        try (HTU21DF htu21df = HTU21DFBuilder.get().context(context).build()) {
+            Logger.info("HTU21DTest started ...");
+            for (int i = 0; i < 10; i++) {
+                float temp = htu21df.getTemperature();
+                float hum = htu21df.getHumidity();
+                Logger.info("[{}] Temperature: {} C, Rel. Humidity: {} %", i, String.format("%.3f", temp), String.format("%.3f", hum));
+                Thread.sleep(500);
+            }
+            Logger.info("HTU21DTest done.");
+        }
+    }
+
+    public static void main(String... args) {
+        Logger.info("Off we go ...");
+        // Context context = null;
+        // context = lazyConfigInit(context);
+        Context context = Pi4J.newAutoContext();
+        try {
+            HTU21DTest.test(context);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
