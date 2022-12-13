fffa0d98800bc724365f898c2f05ca8e4e4710dc
OlivierLD <olivier@lediouris.net>
2022-12-13 08:52:11 +0100

    Code polish

--- java-python/Java-TCP-Python/src/main/python/sensors/lsm303/mag_101.py
+++ java-python/Java-TCP-Python/src/main/python/sensors/lsm303/mag_101.py
@@ -2,16 +2,17 @@ import time
 import board
 import math
 import signal
+import busio
 import adafruit_lsm303dlh_mag
 #
 # 1 Gauss = 100 microTesla
 # 1 T = 10^4 G
 #
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-print(f"Board/I2C is a {type(i2c)}")
+i2c: busio.I2C = board.I2C()  # uses board.SCL and board.SDA
+# print(f"Board/I2C is a {type(i2c)}")
 sensor: adafruit_lsm303dlh_mag.LSM303DLH_Mag = adafruit_lsm303dlh_mag.LSM303DLH_Mag(i2c)
-print(f"Sensor is a {type(sensor)}")
+# print(f"Sensor is a {type(sensor)}")
 
 keep_listening: bool = True
 
@@ -29,7 +30,7 @@ signal.signal(signal.SIGINT, interrupt)  # callback, defined above.
 
 while keep_listening:
     mag_x, mag_y, mag_z = sensor.magnetic
-    print(f"mag_x:{type(mag_x)}, mag_y:{type(mag_y)}, mag_z:{type(mag_z)}")
+    # print(f"mag_x:{type(mag_x)}, mag_y:{type(mag_y)}, mag_z:{type(mag_z)}")
     heading: float = math.degrees(math.atan2(mag_y, mag_x))
     while heading < 0:
         heading += 360
