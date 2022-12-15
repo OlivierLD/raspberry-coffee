66b529c00ac85c39b796a13c6e775215f46672cb
OlivierLD <olivier@lediouris.net>
2022-12-15 10:42:23 +0100

    Calibrating, from Python

--- java-python/Java-TCP-Python/src/main/python/sensors/lsm303/mag_101.py
+++ java-python/Java-TCP-Python/src/main/python/sensors/lsm303/mag_101.py
@@ -62,9 +62,12 @@ while keep_listening:
     heading: float = math.degrees(math.atan2(mag_y, mag_x))  # Orientation in plan x,y
     while heading < 0:
         heading += 360
-    # magnetic returns values in micro Tesla (see https://docs.circuitpython.org/projects/lsm303/en/latest/_modules/adafruit_lsm303.html)
+    pitch: float = math.degrees(math.atan2(mag_y, mag_z))  # Orientation in plan y,z
+    roll: float = math.degrees(math.atan2(mag_x, mag_z))   # Orientation in plan x,z
+
+    # 'magnetic' returns values in micro Tesla (see https://docs.circuitpython.org/projects/lsm303/en/latest/_modules/adafruit_lsm303.html)
     # print('Magnetometer (Gauss): ({0:10.3f}, {1:10.3f}, {2:10.3f}), HDM {3:3.1f}\u00B0'.format(mag_x, mag_y, mag_z, heading))
-    print('Magnetometer (\u03BCT): ({0:5.3f}, {1:5.3f}, {2:5.3f}), norm {3:6.3f}, HDM {4:3.1f}\u00B0'.format(mag_x, mag_y, mag_z, norm, heading))
+    print('Magnetometer (\u03BCT): ({0:5.3f}, {1:5.3f}, {2:5.3f}), norm {3:6.3f}, HDM {4:3.1f}\u00B0, PTCH {5:3.1f}\u00B0, ROLL {6:3.1f}\u00B0'.format(mag_x, mag_y, mag_z, norm, heading, pitch, roll))
     if log_for_calibration:
         log_file.write("{0:f};{1:f};{2:f};{3:f}\n".format(mag_x, mag_y, mag_z, norm))
     # print('')
