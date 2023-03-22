6c137d18f8db1f5b6cf0da561751e369b9f38243
OlivierLD <olivier@lediouris.net>
2023-03-22 08:55:27 +0100

    Bezier stuff.

--- Algebra/src/test/java/gsg/examples/wb/bezier/BeziersAtWorkSample03.java
+++ Algebra/src/test/java/gsg/examples/wb/bezier/BeziersAtWorkSample03.java
@@ -3,7 +3,6 @@ package gsg.examples.wb.bezier;
 import bezier.Bezier;
 import gsg.SwingUtils.WhiteBoardPanel;
 import gsg.VectorUtils;
-import lowpass.Filter;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,19 +12,29 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 /**
  * More Abstraction, using default WhiteBoard Writer
+ *
  * You can focus only on the data, not on the display. See the main method.
  * 2D Bezier example.
- * With draggable control points (hence the MouseListener, MouseMotionListener).
+ *
+ * Shows how the Bezier is elaborated, graphically. 4 Ctrl points
  */
-public class SwingSample11 implements MouseListener, MouseMotionListener {
+public class BeziersAtWorkSample03 implements MouseListener, MouseMotionListener {
 
-    private final static String TITLE = "Simple 2D Bezier sample (draggable control points).";
-    private final static String HELP = "Generic Swing Graphic.\nSimple 2D Bezier sample (draggable control points).";
+    private final static String TITLE = "Simple 2D Bezier sample.";
+    private final static String HELP = "Simple 2D Bezier sample.\n4 Draggable Ctrl Points.\nUse the Animate button to see how the curve is build.";
+    // All z = 0, 2D bezier.
+    // 4 control points
+    private List<Bezier.Point3D> ctrlPoints = List.of(
+            new Bezier.Point3D(-60, -20, 0),
+            new Bezier.Point3D(0, 40, 0),
+            new Bezier.Point3D(50, -40, 0),
+//            new Bezier.Point3D(60, -20, 0),
+//            new Bezier.Point3D(-30, -30, 0),
+            new Bezier.Point3D(-50, 30, 0));
 
     private JFrame frame;
     private final JMenuBar menuBar = new JMenuBar();
@@ -34,25 +43,11 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
     private final JMenu menuHelp = new JMenu();
     private final JMenuItem menuHelpAbout = new JMenuItem();
     private JLabel topLabel;
-    private final JButton refreshButton = new JButton("Refresh Data"); // Not really useful here.
+    private final JButton refreshButton = new JButton("Refresh Data");
+    private final JButton animateButton = new JButton("Animate");
 
     private final static int WIDTH = 800;
     private final static int HEIGHT = 600;
-
-    // All z = 0, 2D bezier.
-    // 5 control points
-    private List<Bezier.Point3D> ctrlPoints = List.of(  // Warning: List.of generates an immutable list.
-            new Bezier.Point3D(-60, -80, 0),
-            new Bezier.Point3D(60, -40, 0),
-            new Bezier.Point3D(45, 30, 0),
-            new Bezier.Point3D(-30, 20, 0),
-            new Bezier.Point3D(-30, 90, 0));
-
-    // A frame for a small boat?
-//    private List<Bezier.Point3D> ctrlPoints = List.of(  // Warning: List.of generates an immutable list.
-//            new Bezier.Point3D(0, -25, 0),
-//            new Bezier.Point3D(100, 0, 0),
-//            new Bezier.Point3D(105, 56, 0));
 
     // The WhiteBoard instantiation
     private final static WhiteBoardPanel whiteBoard = new WhiteBoardPanel();
@@ -66,47 +61,15 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
         JOptionPane.showMessageDialog(whiteBoard, HELP, "GSG Help", JOptionPane.PLAIN_MESSAGE);
     }
 
-    /**
-     * Get the t value for a given X on the curve.
-     *
-     * @param bezier
-     * @param startAt 0 to begin with
-     * @param inc t increment for first iteration
-     * @param x the X to find
-     * @param precision acceptable difference
-     * @return
-     */
-    private static double getTForGivenX(Bezier bezier, double startAt, double inc, double x, double precision) {
-        double tForX = 0;
-        for (double t=startAt; t<=1; t+=inc) {
-            Bezier.Point3D tick = bezier.getBezierPoint(t);
-            if (tick.getX() > x) { // Assume that X is always growing.
-                if (Math.abs(tick.getX() - x) < precision) {
-                    return t;
-                } else {
-                    return getTForGivenX(bezier, startAt - inc, inc / 10.0, x, precision);
-                }
-            }
-        }
-        return tForX;
-    }
-
     private void refreshData() {
 
         // Generate the data, the Bézier curve.
         Bezier bezier = new Bezier(ctrlPoints);
         List<VectorUtils.Vector3D> bezierPoints = new ArrayList<>(); // The points to display.
-        for (double t=0; t<=1.0; t+=1E-3) {
+        for (double t=0; t<=1.0; t+=1e-3) {
             Bezier.Point3D tick = bezier.getBezierPoint(t);
             // System.out.println(String.format("%.03f: %s", t, tick.toString()));
             bezierPoints.add(new VectorUtils.Vector3D(tick.getX(), tick.getY(), tick.getZ()));
-        }
-        // For test: Find t for a given X
-        if (false) {
-            double x = 60; // the one to find
-            double t = getTForGivenX(bezier, 0.0, 1E-1, x, 1E-4);
-            Bezier.Point3D tick = bezier.getBezierPoint(t);
-            System.out.printf("For x=%f, t=%f - X:%f, Y:%f\n", x, t, tick.getX(), tick.getY());
         }
 
         // Prepare data for display
@@ -185,6 +148,33 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         refreshButton.addActionListener(e -> refreshData());
+        animateButton.addActionListener(e -> {
+            if (animator == null || !animator.isAlive()) {
+                System.out.println("Starting new Animator");
+                animateSuspended = false;
+                animator = new Thread(() -> animate(), "Animator");
+                animator.start();
+            } else {
+                System.out.println("Animator already running");
+                if (animateSuspended) { // Resuming
+                    if (animator.isAlive()) {
+                        System.out.println("... Resuming");
+//                        synchronized (animator) { // That one does not work as expected...
+//                            animator.notify();
+//                        }
+                        synchronized (lock) {
+                            lock.notify();
+                        }
+                        System.out.println("    Notification sent.");
+                    }
+                    animateSuspended = false;
+                } else {
+                    System.out.println("... Suspending");
+                    animateSuspended = true;
+                }
+            }
+        });
+        animateButton.setToolTipText("Start/Stop");
 
         frame.setJMenuBar(menuBar);
         frame.getContentPane().setLayout(new BorderLayout());
@@ -207,7 +197,8 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
         frame.getContentPane().add(whiteBoard, BorderLayout.CENTER);
 
         JPanel bottomPanel = new JPanel(new GridBagLayout());
-        bottomPanel.add(refreshButton, new GridBagConstraints(0,
+        bottomPanel.add(refreshButton,
+                new GridBagConstraints(0,
                 0,
                 1,
                 1,
@@ -215,15 +206,182 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
                 0.0,
                 GridBagConstraints.WEST,
                 GridBagConstraints.NONE,
-                new Insets(0, 0, 0, 10), 0, 0));
+                new Insets(0, 0, 0, 0), 0, 0));
+        bottomPanel.add(animateButton, new GridBagConstraints(0,
+                1,
+                1,
+                1,
+                0.0,
+                0.0,
+                GridBagConstraints.WEST,
+                GridBagConstraints.NONE,
+                new Insets(0, 0, 0, 0), 0, 0));
 
         frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
 //        frame.pack();
     }
 
-    public SwingSample11() {
+    public BeziersAtWorkSample03() {
         whiteBoard.addMouseListener(this);
         whiteBoard.addMouseMotionListener(this);
+    }
+
+    private boolean animateSuspended = false;
+    private Object lock = new Object(); // wait/notify on the animator thread did not work for me... :(
+    private Thread animator = null;
+
+    public void animate() {
+        whiteBoard.resetAllData();
+
+        Bezier bezier = new Bezier(ctrlPoints);
+
+        // Ctrl Points
+        double[] xCtrlPoints = ctrlPoints.stream()
+                .mapToDouble(bp -> bp.getX())
+                .toArray();
+        double[] yCtrlPoints = ctrlPoints.stream()
+                .mapToDouble(bp -> bp.getY())
+                .toArray();
+
+        List<VectorUtils.Vector2D> ctrlPtsVectors = new ArrayList<>();
+        for (int i=0; i<xCtrlPoints.length; i++) {
+            ctrlPtsVectors.add(new VectorUtils.Vector2D(xCtrlPoints[i], yCtrlPoints[i]));
+//            System.out.printf("Adding X:%f, Y:%f\n", xCtrlPoints[i], yCtrlPoints[i]);
+        }
+
+        // Bezier ctrl points serie
+        WhiteBoardPanel.DataSerie ctrlSerie = new WhiteBoardPanel.DataSerie()
+                .data(ctrlPtsVectors)
+                .graphicType(WhiteBoardPanel.GraphicType.LINE_WITH_DOTS)
+                .lineThickness(1)
+                .color(Color.RED);
+        whiteBoard.addSerie(ctrlSerie);
+
+        whiteBoard.repaint();
+
+        // Now, animate.
+        final double tIncrement = 1e-3; // 0.005;
+//        animateButton.setEnabled(false);
+        for (double t=0.0; t<=1 + tIncrement; t += tIncrement) {
+            try {
+                final double _t = t;
+                SwingUtilities.invokeAndWait(() -> {
+                    List<VectorUtils.Vector3D> bezierPoints = new ArrayList<>(); // The points to display.
+                    for (double bezierT=0; bezierT<=_t; bezierT += tIncrement) {
+                        Bezier.Point3D tick = bezier.getBezierPoint(bezierT);
+                        // System.out.println(String.format("%.03f: %s", t, tick.toString()));
+                        bezierPoints.add(new VectorUtils.Vector3D(tick.getX(), tick.getY(), tick.getZ()));
+                    }
+                    double[] xData = bezierPoints.stream()
+                            .mapToDouble(bp -> bp.getX())
+                            .toArray();
+                    double[] yData = bezierPoints.stream()
+                            .mapToDouble(bp -> bp.getY())
+                            .toArray();
+                    List<VectorUtils.Vector2D> dataVectors = new ArrayList<>();
+                    for (int i=0; i<xData.length; i++) {
+                        dataVectors.add(new VectorUtils.Vector2D(xData[i], yData[i]));
+                    }
+                    // Bezier points serie
+                    WhiteBoardPanel.DataSerie dataSerie = new WhiteBoardPanel.DataSerie()
+                            .data(dataVectors)
+                            .graphicType(WhiteBoardPanel.GraphicType.LINE)
+                            .lineThickness(3)
+                            .color(Color.BLUE);
+
+                    whiteBoard.resetAllData();
+                    whiteBoard.resetAllText();
+
+                    WhiteBoardPanel.TextSerie currentPosition = new WhiteBoardPanel.TextSerie(new VectorUtils.Vector2D(-56d, 37d), String.format("t = %.03f", _t));
+                    whiteBoard.addTextSerie(currentPosition);
+
+                    // Main Ctrl points
+                    whiteBoard.addSerie(ctrlSerie);
+                    // Bezier
+                    whiteBoard.addSerie(dataSerie);
+
+                    // Progress
+                    // For 4 ctrl points, three-segments, hard-coded
+                    Bezier.Point3D point3D_01 = Bezier.withProgressT(ctrlPoints.get(0), ctrlPoints.get(1), _t);
+                    Bezier.Point3D point3D_02 = Bezier.withProgressT(ctrlPoints.get(1), ctrlPoints.get(2), _t);
+                    Bezier.Point3D point3D_03 = Bezier.withProgressT(ctrlPoints.get(2), ctrlPoints.get(3), _t);
+
+                    // Bezier progress points serie
+                    List<VectorUtils.Vector2D> progressPtsVectors_01 = new ArrayList<>();
+                    progressPtsVectors_01.add(new VectorUtils.Vector2D(point3D_01.getX(), point3D_01.getY()));
+                    progressPtsVectors_01.add(new VectorUtils.Vector2D(point3D_02.getX(), point3D_02.getY()));
+
+                    WhiteBoardPanel.DataSerie progressSerie_01 = new WhiteBoardPanel.DataSerie()
+                            .data(progressPtsVectors_01)
+                            .graphicType(WhiteBoardPanel.GraphicType.LINE_WITH_DOTS)
+                            .lineThickness(1)
+                            .color(Color.BLUE);
+
+                    List<VectorUtils.Vector2D> progressPtsVectors_02 = new ArrayList<>();
+                    progressPtsVectors_02.add(new VectorUtils.Vector2D(point3D_02.getX(), point3D_02.getY()));
+                    progressPtsVectors_02.add(new VectorUtils.Vector2D(point3D_03.getX(), point3D_03.getY()));
+
+                    WhiteBoardPanel.DataSerie progressSerie_02 = new WhiteBoardPanel.DataSerie()
+                            .data(progressPtsVectors_02)
+                            .graphicType(WhiteBoardPanel.GraphicType.LINE_WITH_DOTS)
+                            .lineThickness(1)
+                            .color(Color.BLUE);
+
+                    whiteBoard.addSerie(progressSerie_01);
+                    whiteBoard.addSerie(progressSerie_02);
+
+                    // Level 2
+                    Bezier.Point3D point3D_L2_01 = Bezier.withProgressT(point3D_01, point3D_02, _t);
+                    Bezier.Point3D point3D_L2_02 = Bezier.withProgressT(point3D_02, point3D_03, _t);
+
+                    List<VectorUtils.Vector2D> progressPtsVectors_L2 = new ArrayList<>();
+                    progressPtsVectors_L2.add(new VectorUtils.Vector2D(point3D_L2_01.getX(), point3D_L2_01.getY()));
+                    progressPtsVectors_L2.add(new VectorUtils.Vector2D(point3D_L2_02.getX(), point3D_L2_02.getY()));
+
+                    WhiteBoardPanel.DataSerie progressSerie_Level2 = new WhiteBoardPanel.DataSerie()
+                            .data(progressPtsVectors_L2)
+                            .graphicType(WhiteBoardPanel.GraphicType.LINE_WITH_DOTS)
+                            .lineThickness(1)
+                            .color(Color.GREEN);
+                    whiteBoard.addSerie(progressSerie_Level2);
+
+                    // Last Bezier point
+                    whiteBoard.addSerie(new WhiteBoardPanel.DataSerie()
+                            .data(List.of(new VectorUtils.Vector2D(dataVectors.get(dataVectors.size() - 1).getX(),
+                                                                   dataVectors.get(dataVectors.size() - 1).getY())))
+                            .graphicType(WhiteBoardPanel.GraphicType.POINTS)
+                            .circleDiam(10)
+                            .color(Color.BLUE));
+
+                    whiteBoard.repaint();
+                    if (true) {
+                        try {
+                            Thread.sleep(5L);
+                        } catch (InterruptedException ie) {
+                            // Absorb
+                        }
+                    }
+                });
+                if (animateSuspended) {
+                    try {
+                        synchronized(lock) {
+                            System.out.println("    Waiting");
+                            lock.wait(); // Expect notify
+                            System.out.println("    Waiter notified !");
+                        }
+                        animateSuspended = false; // double layer...
+                    } catch (InterruptedException ie) {
+                        System.err.println("Exception in the wait:");
+                        ie.printStackTrace();
+                    }
+//                } else {
+//                    System.out.println("Keep working !");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+//        animateButton.setEnabled(true);
     }
 
     // Find the control point close to the mouse pointer.
@@ -251,8 +409,8 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-//        System.out.println("Mouse moved");
         Bezier.Point3D closePoint = getClosePoint(e);
+        // System.out.printf("Mouse moved, closestPointIndex: %d\n", closestPointIndex);
         if (closePoint != null) {
             whiteBoard.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
             closestPointIndex = ctrlPoints.indexOf(closePoint);
@@ -315,7 +473,7 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
         System.out.printf("Java Version %s\n", System.getProperty("java.version"));
         System.out.println("----------------------------------------------");
 
-        SwingSample11 thisThing = new SwingSample11();// This one has instantiated the white board
+        BeziersAtWorkSample03 thisThing = new BeziersAtWorkSample03();// This one has instantiated the white board
         thisThing.initComponents();
 
         // Override defaults (not mandatory)
@@ -334,6 +492,5 @@ public class SwingSample11 implements MouseListener, MouseMotionListener {
 
         thisThing.refreshData(); // Display data the first time.
         thisThing.show();
-
     }
 }
