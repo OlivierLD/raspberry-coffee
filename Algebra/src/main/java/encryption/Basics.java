bccedac456421285ed2d3fcfe696bf22b6cbf145
OlivierLD <olivier@lediouris.net>
2024-09-22 08:55:52 +0200

    Primes, and encryption.

--- Algebra/src/main/java/encryption/Basics.java
+++ Algebra/src/main/java/encryption/Basics.java
@@ -1,16 +1,22 @@
 package encryption;
 
+/**
+ * Histoire des codes secrets, pages 232 and after.
+ * Brute force.
+ */
 public class Basics {
-
-    /**
-     * Histoire des codes secrets, pages 232 and after.
-     * Brute force.
-     */
     // Suitable for lowercase, not accented characters, no punctuation, just blanks.
     private final static String NOT_ENCRYPTED = "abcdefghijklmnopqrstuvwxyz";
     private final static String ALICE_KEY     = "HFSUGTAKVDEOYJBPNXWCQRIMZL";
     private final static String BERNARD_KEY   = "CPMGATNOJEFWIQBURYHXSDZKLV";
 
+    /**
+     * Encrypt a message with a key.
+     *
+     * @param original Original message, in lowercase. No accent, no punctuation. Just blanks allowed.
+     * @param key The encryption key
+     * @return The encrypted message.
+     */
     private static String encrypt(String original, String key) {
         StringBuilder encrypted = new StringBuilder();
         char[] charArray = original.toCharArray();
@@ -22,13 +28,22 @@ public class Basics {
                 if (NOT_ENCRYPTED.contains(new StringBuffer(c))) {
                     int idx = 0;
                     while (!found) {
-                        if (NOT_ENCRYPTED.charAt(idx) == c) {
-                            found = true;
-                        } else {
-                            idx++;
+                        try {
+                            if (NOT_ENCRYPTED.charAt(idx) == c) {
+                                found = true;
+                            } else {
+                                idx++;
+                            }
+                        } catch (StringIndexOutOfBoundsException siobe) {
+                            System.err.println("--- Cough that ---");
+                            siobe.printStackTrace();
+                            System.err.println("------------------");
+                            break;
                         }
                     }
-                    encrypted.append(key.charAt(idx));
+                    if (found) {
+                        encrypted.append(key.charAt(idx));
+                    }
                 } else {
                     System.out.println("Oops...");
                 }
@@ -37,6 +52,12 @@ public class Basics {
         return encrypted.toString();
     }
 
+    /**
+     * Decrypt a message, with the key it has been encrypted with.
+     * @param encrypted Encrypted message
+     * @param key The key, used for encryption
+     * @return The decrypted message
+     */
     private static String decrypt(String encrypted, String key) {
         StringBuilder decrypted = new StringBuilder();
         char[] charArray = encrypted.toCharArray();
@@ -62,7 +83,8 @@ public class Basics {
         }
         return decrypted.toString();
     }
-    public static void main(String... args) {
+
+    private static void checkTheKeys() {
         if (NOT_ENCRYPTED.length() != 26) {
             System.out.printf("Bad length for NOT_ENCRYPTED: %d\n", NOT_ENCRYPTED.length());
         }
@@ -72,11 +94,37 @@ public class Basics {
         if (BERNARD_KEY.length() != 26) {
             System.out.printf("Bad length for BERNARD_KEY: %d\n", BERNARD_KEY.length());
         }
+    }
+
+    public static void main(String... args) {
+
+        boolean checkKeys = false;
+        for (String arg : args) {
+            if ("--check-keys".equals(arg)) {
+                checkKeys = true;
+            }
+        }
+        if (checkKeys) {
+            checkTheKeys();
+        }
 
         String originalMessage = "vois moi a midi";
         String encrypted = encrypt(originalMessage, ALICE_KEY);
+
         System.out.printf("Encrypted with Alice's key [%s]\n", encrypted);
+
         String decrypted = decrypt(encrypted, ALICE_KEY);
-        System.out.printf("Back to original [%s]\n", decrypted);
+        System.out.printf("[%s] => Back to original [%s]\n", originalMessage, decrypted);
+
+        // --------------------
+        originalMessage = "Vois moi à midi, Ducon !";
+
+        // String normalized = StringUtils.stripAccents(originalMessage); // TODO Fix that...
+        encrypted = encrypt(originalMessage, ALICE_KEY);
+
+        System.out.println("-----------------");
+        System.out.printf("Accented message: Encrypted with Alice's key [%s]\n", encrypted);
+
+        System.out.println("----- Done! -----");
     }
 }
